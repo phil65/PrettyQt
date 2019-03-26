23af707a06e97e5f5bfaa4e6b6694b936af0f3bb
phil65 <phil65@kodi.tv>
2019-03-26 04:23:13 +0100

    add napolean extension to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ import prettyqt
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx.ext.napoleon',
+              'sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.coverage',
               'sphinx.ext.inheritance_diagram',
