dea9d333b3756de794a79627b1671692185f90ed
phil65 <phil65@kodi.tv>
2019-03-26 04:23:15 +0100

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/phil65/prettyqt',
-    version='0.8.1',
+    version='0.8.2',
     zip_safe=False,
 )
