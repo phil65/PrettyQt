7b0ed8c352b65c071bbbd860f9069b6bb8a26520
phil65 <phil65@kodi.tv>
2019-03-26 02:29:29 +0100

    add __all__ to root __init__.py

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -2,6 +2,11 @@
 
 """Top-level package for PrettyQt."""
 
+from . import core
+from . import widgets
+
 __author__ = """Philipp Temminghoff"""
 __email__ = 'phil65@kodi.tv'
 __version__ = '0.8.1'
+
+__all__ = ["core", "widgets"]
