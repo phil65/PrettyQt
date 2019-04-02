307694a17bf76168e1b35dd1a5421907fcd59a16
phil65 <phil65@kodi.tv>
2019-04-02 16:45:26 +0200

    textedit work

--- prettyqt/widgets/textedit.py
+++ prettyqt/widgets/textedit.py
@@ -13,3 +13,29 @@ class TextEdit(QtWidgets.QTextEdit):
 
     def set_disabled(self):
         self.setEnabled(False)
+
+    def set_font(self, font):
+        self.setFont(font)
+
+    def append(self, text):
+        self.appendPlainText(text)
+
+    def set_text(self, text):
+        self.setPlainText(text)
+
+    def text(self):
+        return self.toPlainText()
+
+    def set_read_only(self, value):
+        self.setReadOnly(value)
+
+    def scroll_to_end(self):
+        self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    widget = TextEdit("This is a test")
+    widget.show()
+    app.exec_()
