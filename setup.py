e6492bcd5e87c1f182234865f2cc75035bf0fff9
phil65 <phil65@kodi.tv>
2019-04-03 05:19:05 +0200

    long_description_content to markdown

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/x-markdown',
     include_package_data=True,
     keywords='prettyqt',
     name='prettyqt',
