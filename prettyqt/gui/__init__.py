f982254996a35845ec473a7b2f0f4067328f22a5
phil65 <phil65@kodi.tv>
2019-04-05 03:13:02 +0200

    add textcharformat

--- prettyqt/gui/__init__.py
+++ prettyqt/gui/__init__.py
@@ -8,6 +8,7 @@ contains QtGui-based classes
 from .regexpvalidator import RegExpValidator
 from .font import Font
 from .icon import Icon
+from .textcharformat import TextCharFormat
 
 
-__all__ = ["RegExpValidator", "Font", "Icon"]
+__all__ = ["RegExpValidator", "Font", "Icon", "TextCharFormat"]
---
+++ prettyqt/gui/textcharformat.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtGui
+from prettyqt import gui
+
+
+class TextCharFormat(QtGui.QTextCharFormat):
+
+    def set_foreground_color(self, color_name):
+        color = gui.Color()
+        color.setNamedColor(color_name)
+        self.setForeground(color)
+
+    def set_font_weight(self, weight):
+        WEIGHTS = dict(thin=QtGui.QFont.Thin,
+                       light=QtGui.QFont.Light,
+                       medium=QtGui.QFont.Medium,
+                       bold=QtGui.QFont.Bold)
+        if weight not in WEIGHTS:
+            raise ValueError("Invalid font weight")
+        self.setFontWeight(WEIGHTS[weight])
