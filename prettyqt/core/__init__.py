5982b2ccf4340f27b52fcae01a3cfa960785a42f
phil65 <phil65@kodi.tv>
2019-04-05 22:28:34 +0200

    add MimeData

--- prettyqt/core/__init__.py
+++ prettyqt/core/__init__.py
@@ -11,8 +11,16 @@ from .object import Object
 from .settings import Settings
 from .size import Size
 from .rect import Rect
+from .mimedata import MimeData
 from .filesystemmodel import FileSystemModel
 
 from qtpy.QtCore import Slot, Signal
 
-__all__ = ["Object", "Settings", "Size", "Rect", "FileSystemModel", "Slot", "Signal"]
+__all__ = ["Object",
+           "Settings",
+           "Size",
+           "Rect",
+           "MimeData",
+           "FileSystemModel",
+           "Slot",
+           "Signal"]
