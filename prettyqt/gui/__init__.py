02387b0ab8345769699c4fda250b9e15c99fdf54
phil65 <phil65@kodi.tv>
2019-04-05 02:39:54 +0200

    add Icon

--- prettyqt/gui/__init__.py
+++ prettyqt/gui/__init__.py
@@ -7,5 +7,7 @@ contains QtGui-based classes
 
 from .regexpvalidator import RegExpValidator
 from .font import Font
+from .icon import Icon
 
-__all__ = ["RegExpValidator", "Font"]
+
+__all__ = ["RegExpValidator", "Font", "Icon"]
---
+++ prettyqt/gui/icon.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtGui
+
+
+class Icon(QtGui.QIcon):
+
+    def set_size(self, size: int):
+        pass
