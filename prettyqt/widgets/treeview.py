2f534b0177b8820117a07e58099bc992054997ff
phil65 <phil65@kodi.tv>
2019-04-06 02:32:32 +0200

    treeview work

--- prettyqt/widgets/treeview.py
+++ prettyqt/widgets/treeview.py
@@ -8,7 +8,20 @@ from qtpy import QtWidgets
 
 class TreeView(QtWidgets.QTreeView):
 
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # visual settings
+        self.setAnimated(True)
+        self.setRootIsDecorated(False)
+        self.setAllColumnsShowFocus(True)
+
+        # misc
+        self.setUniformRowHeights(True)
+        self.set_selection_mode("extended")
+
+    def h_header(self):
+        return self.header()
 
 
 if __name__ == "__main__":
