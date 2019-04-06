6ca4071a63c333afa6b8a2332e6d93dcaa508759
phil65 <phil65@kodi.tv>
2019-04-06 03:32:58 +0200

    add missing treeview method

--- prettyqt/widgets/treeview.py
+++ prettyqt/widgets/treeview.py
@@ -5,6 +5,11 @@
 
 from qtpy import QtWidgets
 
+SELECTION_MODES = dict(single=QtWidgets.QAbstractItemView.SingleSelection,
+                       extended=QtWidgets.QAbstractItemView.ExtendedSelection,
+                       multi=QtWidgets.QAbstractItemView.MultiSelection,
+                       none=QtWidgets.QAbstractItemView.NoSelection)
+
 
 class TreeView(QtWidgets.QTreeView):
 
@@ -22,6 +27,22 @@ class TreeView(QtWidgets.QTreeView):
 
     def h_header(self):
         return self.header()
+
+    def set_selection_mode(self, mode: str):
+        """set selection mode for given item view
+
+        Allowed values are "single", "extended", "multi" or "none"
+
+        Args:
+            mode: selection mode to use
+
+        Raises:
+            ValueError: mode does not exist
+        """
+        if mode not in SELECTION_MODES:
+            raise ValueError("Format must be either 'single', 'extended',"
+                             "'multi' or 'None'")
+        self.setSelectionMode(SELECTION_MODES[mode])
 
 
 if __name__ == "__main__":
