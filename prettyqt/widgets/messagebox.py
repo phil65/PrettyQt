81fd23ac6fd9841c0c0d94107c511ec343836e9c
phil65 <phil65@kodi.tv>
2019-04-06 02:30:24 +0200

    MessageBox.set_icon

--- prettyqt/widgets/messagebox.py
+++ prettyqt/widgets/messagebox.py
@@ -4,6 +4,7 @@
 """
 
 from qtpy import QtWidgets, QtCore
+import qtawesome as qta
 
 
 class MessageBox(QtWidgets.QMessageBox):
@@ -13,6 +14,12 @@ class MessageBox(QtWidgets.QMessageBox):
 
     def set_vertical(self):
         self.setOrientation(QtCore.Qt.Vertical)
+
+    def set_icon(self, icon):
+        if icon:
+            if isinstance(icon, str):
+                icon = qta.icon(icon, color="lightgray")
+            self.setWindowIcon(icon)
 
 
 if __name__ == "__main__":
