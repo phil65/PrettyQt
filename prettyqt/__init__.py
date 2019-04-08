debf4d199ee8937788df0d0e2389282586e015a9
phil65 <phil65@kodi.tv>
2019-04-08 22:38:22 +0200

    Bump version: 0.13.2 → 0.13.3

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -9,6 +9,6 @@ from . import syntaxhighlighters
 
 __author__ = """Philipp Temminghoff"""
 __email__ = 'phil65@kodi.tv'
-__version__ = '0.13.2'
+__version__ = '0.13.3'
 
 __all__ = ["core", "widgets", "gui", "syntaxhighlighters"]
