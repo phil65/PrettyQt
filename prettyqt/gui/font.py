4064c38b4505e2a03d8493d5bd0d27e48ff611b9
phil65 <phil65@kodi.tv>
2019-04-12 02:10:09 +0200

    Font.mono

--- prettyqt/gui/font.py
+++ prettyqt/gui/font.py
@@ -10,3 +10,9 @@ class Font(QtGui.QFont):
 
     def set_size(self, size: int):
         pass
+
+    @classmethod
+    def mono(cls, size=8):
+        font = cls("Consolas", size)
+        # font.setStyleHint()
+        return font
