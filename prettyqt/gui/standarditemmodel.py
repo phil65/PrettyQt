95f01bb5548e700cf547fa2991e5bffb7e251b8d
phil65 <phil65@kodi.tv>
2019-04-14 09:48:23 +0200

    standarditemmodel iter

--- prettyqt/gui/standarditemmodel.py
+++ prettyqt/gui/standarditemmodel.py
@@ -10,6 +10,10 @@ from prettyqt import gui
 
 class StandardItemModel(QtGui.QStandardItemModel):
 
+    def __iter__(self):
+        items = [self.item(index) for index in range(self.rowCount())]
+        return iter(items)
+
     def add_item(self, label):
         item = gui.StandardItem(label)
         self.appendRow(item)
