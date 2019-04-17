ca2b655ec862d37a1446c48eed26f1b0017a595a
phil65 <phil65@kodi.tv>
2019-04-17 02:14:21 +0200

    gui.Icon: allow pathlib path for class init

--- prettyqt/gui/icon.py
+++ prettyqt/gui/icon.py
@@ -3,10 +3,18 @@
 @author: Philipp Temminghoff
 """
 
+import pathlib
+
 from qtpy import QtGui
 
 
 class Icon(QtGui.QIcon):
 
-    def set_size(self, size: int):
-        pass
+    def __init__(self, icon=None):
+        if isinstance(icon, pathlib.Path):
+            icon = str(icon)
+        super().__init__(icon)
+
+
+if __name__ == "__main__":
+    icon = Icon()
