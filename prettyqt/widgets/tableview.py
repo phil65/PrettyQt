c0cb3104e1aa109d05fcf0dd07438d043f29c34b
phil65 <phil65@kodi.tv>
2019-04-17 20:15:43 +0200

    tableview additions

--- prettyqt/widgets/tableview.py
+++ prettyqt/widgets/tableview.py
@@ -3,13 +3,21 @@
 @author: Philipp Temminghoff
 """
 
-from qtpy import QtWidgets
+from qtpy import QtWidgets, QtCore
 
 from prettyqt import widgets
 
 TRIGGERS = dict(none=QtWidgets.QAbstractItemView.DoubleClicked,
                 double_click=QtWidgets.QAbstractItemView.DoubleClicked,
                 edit_key=QtWidgets.QAbstractItemView.EditKeyPressed)
+
+SCROLLBAR_POLICY = dict(always_on=QtCore.Qt.ScrollBarAlwaysOn,
+                        always_off=QtCore.Qt.ScrollBarAlwaysOff,
+                        as_needed=QtCore.Qt.ScrollBarAsNeeded)
+
+SELECTION_BEHAVIOURS = dict(rows=QtWidgets.QAbstractItemView.SelectRows,
+                            columns=QtWidgets.QAbstractItemView.SelectColumns,
+                            items=QtWidgets.QAbstractItemView.SelectItems)
 
 
 class TableView(QtWidgets.QTableView):
@@ -19,6 +27,12 @@ class TableView(QtWidgets.QTableView):
 
     def h_header(self):
         return self.horizontalHeader()
+
+    def set_horizontal_scrollbar_visibility(self, mode):
+        self.setHorizontalScrollBarPolicy(SCROLLBAR_POLICY[mode])
+
+    def set_selection_behaviour(self, mode):
+        self.setSelectionBehaviour(SELECTION_BEHAVIOURS[mode])
 
 
 if __name__ == "__main__":
