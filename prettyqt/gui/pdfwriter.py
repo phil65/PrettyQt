4cb532c69fa4e60de6ec9ad73742fffc5ce1a54b
phil65 <phil65@kodi.tv>
2019-04-18 01:12:07 +0200

    cleanup

--- prettyqt/gui/pdfwriter.py
+++ prettyqt/gui/pdfwriter.py
@@ -15,19 +15,3 @@ class PdfWriter(QtGui.QPdfWriter):
         self.setResolution(dpi)
         self.setPageMargins(QtCore.QMarginsF(0, 0, 0, 0))
         self.setPageSizeMM(core.SizeF(size.width(), size.height()) / dpi * 25.4)
-
-    def testus(self):
-        self.setup(self.getTargetRect().size())
-        painter = gui.Painter(self)
-        try:
-            dct = dict(antialias=True,
-                       background=self.background,
-                       painter=painter)
-            self.setExportMode(True, dct)
-            painter.use_antialiasing()
-            self.getScene().render(painter,
-                                   core.RectF(self.getTargetRect()),
-                                   core.RectF(self.getSourceRect()))
-        finally:
-            self.setExportMode(False)
-        painter.end()
