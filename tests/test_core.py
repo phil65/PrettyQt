1a55113705f32f11443c16043347e1a631245ca1
phil65 <phil65@kodi.tv>
2019-04-29 22:32:13 +0200

    testing

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,6 +8,88 @@ import pathlib
 from prettyqt import core
 
 
+def test_abstractitemmodel():
+    model = core.AbstractItemModel()
+    assert model.rowCount() == 0
+    assert model.columnCount() == 0
+    with model.change_layout():
+        pass
+    with model.reset_model():
+        pass
+    with model.remove_rows():
+        pass
+    with model.remove_columns():
+        pass
+    with model.insert_rows():
+        pass
+    with model.insert_columns():
+        pass
+    # qtmodeltester.check(model, force_py=True)
+
+
+def test_abstracttablemodel():
+    model = core.AbstractTableModel()
+    assert model.rowCount() == 0
+    assert model.columnCount() == 0
+    with model.change_layout():
+        pass
+    with model.reset_model():
+        pass
+    with model.remove_rows():
+        pass
+    with model.remove_columns():
+        pass
+    with model.insert_rows():
+        pass
+    with model.insert_columns():
+        pass
+    # qtmodeltester.check(model, force_py=True)
+
+
+def test_dir():
+    core.Dir()
+
+
+def test_diriterator():
+    for i in core.DirIterator(__file__):
+        pass
+
+
+def test_mimedata():
+    mime_data = core.MimeData()
+    mime_data.set_data("type a", "data")
+
+
+def test_modelindex():
+    core.ModelIndex()
+
+
+def test_object():
+    core.Object()
+
+
+def test_point():
+    core.Point()
+
+
+def test_rect():
+    core.Rect()
+
+
+def test_rectf():
+    core.RectF()
+
+
+def test_regexp():
+    regex = core.RegExp("[0-9]")
+    a = list(regex.matches_in_text("0a4"))
+    assert len(a) == 2
+
+
+def test_runnable():
+    core.Runnable()
+
+
 def test_settings():
     settings = core.Settings("1", "2")
     settings.set_value("test", "value")
@@ -26,44 +108,20 @@ def test_settings():
     settings.set_path("native", "user", path)
 
 
-def test_abstractitemmodel():
-    model = core.AbstractItemModel()
-    with model.change_layout():
-        pass
-    with model.reset_model():
-        pass
-    with model.remove_rows():
-        pass
-    with model.remove_columns():
-        pass
-    with model.insert_rows():
-        pass
-    with model.insert_columns():
-        pass
-    # qtmodeltester.check(model, force_py=True)
+def test_size():
+    core.Size()
 
 
-def test_abstracttablemodel():
-    model = core.AbstractTableModel()
-    with model.change_layout():
-        pass
-    with model.reset_model():
-        pass
-    with model.remove_rows():
-        pass
-    with model.remove_columns():
-        pass
-    with model.insert_rows():
-        pass
-    with model.insert_columns():
-        pass
-    # qtmodeltester.check(model, force_py=True)
+def test_sizef():
+    core.SizeF()
 
 
-def test_regexp():
-    regex = core.RegExp("[0-9]")
-    a = list(regex.matches_in_text("0a4"))
-    assert len(a) == 2
+def test_sortfilterproxymodel():
+    core.SortFilterProxyModel()
+
+
+def test_thread():
+    core.ThreadPool()
 
 
 def test_threadpool():
@@ -74,3 +132,7 @@ def test_timer():
     def test():
         pass
     core.Timer.single_shot(test)
+
+
+def test_translator():
+    core.Translator()
