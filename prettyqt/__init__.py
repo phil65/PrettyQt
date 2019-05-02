ac95ef45200f846d6f5c3525b92ed9295a61a88e
phil65 <phil65@kodi.tv>
2019-05-02 07:24:19 +0200

    Bump version: 0.20.3 → 0.21.0

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -7,6 +7,6 @@ from qtpy.QtCore import Qt as flags
 
 __author__ = """Philipp Temminghoff"""
 __email__ = "phil65@kodi.tv"
-__version__ = "0.20.3"
+__version__ = "0.21.0"
 
 __all__ = ["core", "widgets", "gui", "syntaxhighlighters", "flags"]
