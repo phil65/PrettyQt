416427a5095e8ea25f13b3d305b3e48ab4a3dbd7
phil65 <phil65@kodi.tv>
2019-05-03 00:52:33 +0200

    improve colordiaog.get_color

--- prettyqt/widgets/colordialog.py
+++ prettyqt/widgets/colordialog.py
@@ -11,10 +11,13 @@ from prettyqt import widgets, gui
 class ColorDialog(QtWidgets.QColorDialog):
 
     @classmethod
-    def get_color(cls, preset, parent=None):
+    def get_color(cls, preset=None, allow_alpha=False, parent=None):
         if isinstance(preset, str):
             preset = gui.Color(preset)
-        return cls.getColor(preset, parent)
+        if preset is None:
+            preset = gui.Color()
+        kwargs = dict(options=cls.ShowAlphaChannel) if allow_alpha else dict()
+        return gui.Color(cls.getColor(preset, parent, **kwargs))
 
     def current_color(self):
         return gui.Color(self.currentColor())
@@ -22,6 +25,5 @@ class ColorDialog(QtWidgets.QColorDialog):
 
 if __name__ == "__main__":
     app = widgets.Application.create_default_app()
-    widget = ColorDialog()
-    widget.show()
+    ColorDialog.get_color()
     app.exec_()
