a000661a80272f6eac340d33e1c5d790f554c152
phil65 <phil65@kodi.tv>
2019-05-11 16:39:46 +0200

    groupbox pickling + fix

--- prettyqt/widgets/groupbox.py
+++ prettyqt/widgets/groupbox.py
@@ -14,12 +14,34 @@ H_ALIGNMENTS = dict(left=QtCore.Qt.AlignLeft,
 
 class GroupBox(QtWidgets.QGroupBox):
 
-    def set_alignment(self):
-        self.setAlignment(QtCore.Qt.Vertical)
+    def __repr__(self):
+        return f"GroupBox: {self.__getstate__()}"
+
+    def __getstate__(self):
+        return dict(checkable=self.isCheckable(),
+                    checked=self.isChecked(),
+                    layout=self.layout(),
+                    flat=self.isFlat(),
+                    alignment=self.alignment(),
+                    title=self.title())
+
+    def __setstate__(self, state):
+        self.__init__(state["title"])
+        self.setLayout(state["layout"])
+        self.setCheckable(state["checkable"])
+        self.setChecked(state["checked"])
+        self.setFlat(state["flat"])
+        self.setAlignment(state["alignment"])
+
+    def set_alignment(self, alignment):
+        self.setAlignment(H_ALIGNMENTS[alignment])
 
 
 if __name__ == "__main__":
     app = widgets.Application.create_default_app()
     widget = GroupBox()
+    ly = widgets.BoxLayout()
+    ly.addWidget(widgets.RadioButton("test"))
+    widget.setLayout(ly)
     widget.show()
     app.exec_()
