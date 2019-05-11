07c0b267d1bd2c9bb588b8a4655f8af1c6be15f2
phil65 <phil65@kodi.tv>
2019-05-11 21:44:43 +0200

    add notemptyvalidator

---
+++ prettyqt/custom_validators/notemptyvalidator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtGui
+
+
+class NotEmptyValidator(QtGui.QValidator):
+
+    def validate(self, text, pos=0):
+        if text == "":
+            return (self.Intermediate, text, pos)
+        return (self.Acceptable, text, pos)
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+    val = NotEmptyValidator()
+    app = widgets.Application.create_default_app()
+    widget = widgets.LineEdit("This is a test")
+    widget.setValidator(val)
+    widget.show()
+    app.exec_()
