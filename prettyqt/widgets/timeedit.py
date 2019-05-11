b226420a5524881a971ea3ef73419b4d91f478cc
phil65 <phil65@kodi.tv>
2019-05-11 04:10:59 +0200

    same for timeedit

--- prettyqt/widgets/timeedit.py
+++ prettyqt/widgets/timeedit.py
@@ -8,6 +8,27 @@ from qtpy import QtWidgets
 
 class TimeEdit(QtWidgets.QTimeEdit):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setCalendarPopup(True)
+
+    def __getstate__(self):
+        return dict(calendar_popup=self.calendarPopup(),
+                    time=self.get_time(),
+                    display_format=self.displayFormat(),
+                    enabled=self.isEnabled())
+
+    def __setstate__(self, state):
+        super().__init__(state["time"])
+        self.setEnabled(state["enabled"])
+        self.setDisplayFormat(state["display_format"])
+
+    def get_time(self):
+        try:
+            return self.time().toPython()
+        except (TypeError, AttributeError):
+            return self.time().toPyTime()
+
     def set_enabled(self):
         self.setEnabled(True)
 
