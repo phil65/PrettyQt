acb62f6f15bc7cb3c7d69be9c0737dc163495710
phil65 <phil65@kodi.tv>
2019-05-11 13:44:31 +0200

    action work

--- prettyqt/widgets/action.py
+++ prettyqt/widgets/action.py
@@ -4,7 +4,17 @@
 """
 
 import qtawesome as qta
-from qtpy import QtWidgets
+from qtpy import QtWidgets, QtCore
+
+
+PRIORITIES = dict(low=QtWidgets.QAction.LowPriority,
+                  normal=QtWidgets.QAction.NormalPriority,
+                  high=QtWidgets.QAction.HighPriority)
+
+CONTEXTS = dict(widget=QtCore.Qt.WidgetShortcut,
+                widget_with_children=QtCore.Qt.WidgetWithChildrenShortcut,
+                window=QtCore.Qt.WindowShortcut,
+                application=QtCore.Qt.ApplicationShortcut)
 
 
 class Action(QtWidgets.QAction):
@@ -13,7 +23,12 @@ class Action(QtWidgets.QAction):
         return dict(text=self.text(),
                     enabled=self.isEnabled(),
                     shortcut=self.shortcut(),
-                    tooltip=self.toolTip())
+                    tooltip=self.toolTip(),
+                    checkable=self.isCheckable(),
+                    checked=self.isChecked(),
+                    priority=self.priority(),
+                    shortcut_context=self.shortcutContext(),
+                    statustip=self.statusTip())
 
     def __setstate__(self, state):
         self.__init__()
@@ -21,6 +36,11 @@ class Action(QtWidgets.QAction):
         self.setEnabled(state["enabled"])
         self.set_shortcut(state["shortcut"])
         self.setToolTip(state["tooltip"])
+        self.setStatusTip(state["statustip"])
+        self.setChecked(state["checked"])
+        self.setPriority(state["priority"])
+        self.setShortcutContext(state["shortcut_context"])
+        self.setCheckable(state["checkable"])
 
     def set_enabled(self):
         self.setEnabled(True)
@@ -28,7 +48,7 @@ class Action(QtWidgets.QAction):
     def set_disabled(self):
         self.setEnabled(False)
 
-    def set_tooltip(self, text):
+    def set_tooltip(self, text: str):
         self.setToolTip(text)
 
     def set_icon(self, icon):
@@ -40,3 +60,21 @@ class Action(QtWidgets.QAction):
     def set_shortcut(self, shortcut):
         if shortcut:
             self.setShortcut(shortcut)
+
+    def set_priority(self, priority: str):
+        if priority not in PRIORITIES:
+            raise ValueError(f"{priority} not a valid priority.")
+        self.setPriority(PRIORITIES[priority])
+
+    def set_shortcut_context(self, context: str):
+        if context not in CONTEXTS:
+            raise ValueError(f"{context} not a valid shortcut context.")
+        self.setShortcutContext(CONTEXTS[context])
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+    app = widgets.Application.create_default_app()
+    action = Action("This is a test")
+    print(action.__getstate__())
+    app.exec_()
