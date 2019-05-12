9610ad7119b4f7c76a89f17e1c2663d9c27359f0
phil65 <phil65@kodi.tv>
2019-05-12 10:13:26 +0200

    add constants

---
+++ prettyqt/constants/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""charts module
+"""
+
+from qtpy import QtCore
+
+DISPLAY_ROLE = QtCore.Qt.DisplayRole
+USER_ROLE = QtCore.Qt.UserRole
+EDIT_ROLE = QtCore.Qt.EditRole
+BACKGROUND_ROLE = QtCore.Qt.BackgroundRole
+TOOLTIP_ROLE = QtCore.Qt.ToolTipRole
+STATUSTIP_ROLE = QtCore.Qt.StatusTipRole
+DECORATION_ROLE = QtCore.Qt.DecorationRole
+CHECKSTATE_ROLE = QtCore.Qt.CheckStateRole
+ALIGNMENT_ROLE = QtCore.Qt.TextAlignmentRole
+
+ALIGN_RIGHT = QtCore.Qt.AlignRight
+ALIGN_CENTER = QtCore.Qt.AlignCenter
+ALIGN_BOTTOM = QtCore.Qt.AlignBottom
+HORIZONTAL = QtCore.Qt.Horizontal
+
+ASCENDING = QtCore.Qt.AscendingOrder
+
+DROP_ENABLED = QtCore.Qt.ItemIsDropEnabled
+DRAG_ENABLED = QtCore.Qt.ItemIsDragEnabled
+IS_ENABLED = QtCore.Qt.ItemIsEnabled
+IS_SELECTABLE = QtCore.Qt.ItemIsSelectable
+IS_EDITABLE = QtCore.Qt.ItemIsEditable
+IS_CHECKABLE = QtCore.Qt.ItemIsUserCheckable
+NO_FLAGS = QtCore.Qt.NoItemFlags
+NO_CHILDREN = QtCore.Qt.NoItemFlags
+
+MOVE_ACTION = QtCore.Qt.MoveAction
+COPY_ACTION = QtCore.Qt.CopyAction
+
+
+CTRL_MOD = QtCore.Qt.ControlModifier
+
+KEY_F11 = QtCore.Qt.Key_F11
