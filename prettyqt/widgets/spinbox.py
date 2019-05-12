26dfa622f6f9f67f02bb8fc51537a99ca4c66a4f
phil65 <phil65@kodi.tv>
2019-05-12 17:02:47 +0200

    spinbox pickling

--- prettyqt/widgets/spinbox.py
+++ prettyqt/widgets/spinbox.py
@@ -9,12 +9,12 @@ from qtpy import QtWidgets
 
 from prettyqt import widgets, gui
 
-CORRECTION_MODES = dict(to_previous=QtWidgets.QSpinBox.CorrectToPreviousValue,
-                        to_nearest=QtWidgets.QSpinBox.CorrectToNearestValue)
+CORRECTION_MODES = bidict(dict(to_previous=QtWidgets.QSpinBox.CorrectToPreviousValue,
+                               to_nearest=QtWidgets.QSpinBox.CorrectToNearestValue))
 
-SYMBOLS = dict(up_down=QtWidgets.QSpinBox.UpDownArrows,
-               plus_minus=QtWidgets.QSpinBox.PlusMinus,
-               none=QtWidgets.QSpinBox.NoButtons)
+SYMBOLS = bidict(dict(up_down=QtWidgets.QSpinBox.UpDownArrows,
+                      plus_minus=QtWidgets.QSpinBox.PlusMinus,
+                      none=QtWidgets.QSpinBox.NoButtons))
 
 STEP_TYPES = bidict(dict(default=QtWidgets.QSpinBox.DefaultStepType,
                          adaptive=QtWidgets.QSpinBox.AdaptiveDecimalStepType))
@@ -34,10 +34,12 @@ class SpinBox(QtWidgets.QSpinBox):
         return dict(range=(self.minimum(), self.maximum()),
                     value=self.value(),
                     enabled=self.isEnabled(),
-                    step_type=self.get_step_type(),
                     prefix=self.prefix(),
-                    int_base=self.displayIntegerBase(),
                     suffix=self.suffix(),
+                    int_base=self.displayIntegerBase(),
+                    step_type=self.get_step_type(),
+                    button_symbols=self.get_button_symbols(),
+                    correction_mode=self.get_correction_mode(),
                     single_step=self.singleStep())
 
     def __setstate__(self, state):
@@ -48,6 +50,8 @@ class SpinBox(QtWidgets.QSpinBox):
         self.setSingleStep(state["single_step"])
         self.setPrefix(state["prefix"])
         self.setSuffix(state["suffix"])
+        self.set_button_symbols(state["button_symbols"])
+        self.set_correction_mode(state["correction_mode"])
         self.setDisplayIntegerBase(state["int_base"])
         self.set_step_type(state["step_type"])
 
@@ -63,11 +67,17 @@ class SpinBox(QtWidgets.QSpinBox):
     def set_validator(self, validator: gui.Validator):
         self.lineEdit().setValidator(validator)
 
+    def get_button_symbols(self):
+        return SYMBOLS.inv[self.buttonSymbols()]
+
     def set_button_symbols(self, mode: str):
         self.setButtonSymbols(SYMBOLS[mode])
 
     def set_correction_mode(self, mode: str):
         self.setCorrectionMode(CORRECTION_MODES[mode])
+
+    def get_correction_mode(self):
+        return CORRECTION_MODES.inv[self.correctionMode()]
 
     def set_step_type(self, mode: str):
         self.setStepType(STEP_TYPES[mode])
