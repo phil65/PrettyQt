68749f07cf7e16ef434b56a53f836aedf7133869
phil65 <phil65@kodi.tv>
2019-05-17 20:14:13 +0200

    label pickling

--- prettyqt/widgets/label.py
+++ prettyqt/widgets/label.py
@@ -25,6 +25,10 @@ TEXT_INTERACTION = bidict(dict(none=QtCore.Qt.NoTextInteraction,
                                by_mouse=QtCore.Qt.TextSelectableByMouse,
                                by_keyboard=QtCore.Qt.TextSelectableByKeyboard))
 
+TEXT_FORMATS = bidict(dict(rich=QtCore.Qt.RichText,
+                           plain=QtCore.Qt.PlainText,
+                           auto=QtCore.Qt.AutoText))
+
 
 class Label(QtWidgets.QLabel):
 
@@ -38,11 +42,24 @@ class Label(QtWidgets.QLabel):
     def __getstate__(self):
         return dict(text=self.text(),
                     scaled_contents=self.hasScaledContents(),
+                    indent=self.indent(),
+                    margin=self.margin(),
+                    text_format=self.get_text_format(),
+                    # pixmap=self.pixmap(),
+                    open_external_links=self.openExternalLinks(),
+                    alignment=int(self.alignment()),
                     word_wrap=self.wordWrap())
 
     def __setstate__(self, state):
         self.__init__()
         self.setText(state.get("text", ""))
+        self.setIndent(state.get("indent", -1))
+        self.setMargin(state.get("margin", 0))
+        self.setWordWrap(state.get("word_wrap", 0))
+        self.set_text_format(state.get("text_format", 0))
+        # self.setPixmap(state.get("pixmap"))
+        self.setOpenExternalLinks(state.get("open_external_links", False))
+        self.setAlignment(QtCore.Qt.Alignment(state.get("alignment")))
         self.setScaledContents(state["scaled_contents"])
         self.setWordWrap(state["word_wrap"])
 
@@ -58,6 +75,12 @@ class Label(QtWidgets.QLabel):
         else:
             return
         self.setAlignment(flag)
+
+    def set_text_format(self, text_format: str):
+        self.setTextFormat(TEXT_FORMATS[text_format])
+
+    def get_text_format(self) -> str:
+        return TEXT_FORMATS.inv[self.textFormat()]
 
     def set_text_interaction(self, interaction_type: str):
         self.setTextInteractionFlags(TEXT_INTERACTION[interaction_type])
