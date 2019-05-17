0b4358e72b872ba5d6e9b9048ab00460053045fd
phil65 <phil65@kodi.tv>
2019-05-18 00:16:46 +0200

    tabbar pickling

--- prettyqt/widgets/tabbar.py
+++ prettyqt/widgets/tabbar.py
@@ -13,9 +13,9 @@ ELIDE_MODES = bidict(dict(left=QtCore.Qt.ElideLeft,
                           middle=QtCore.Qt.ElideMiddle,
                           none=QtCore.Qt.ElideNone))
 
-SELECTION_MODES = bidict(dict(left_tab=QtWidgets.QTabBar.SelectLeftTab,
-                              right_tab=QtWidgets.QTabBar.SelectRightTab,
-                              previous_tab=QtWidgets.QTabBar.SelectPreviousTab))
+REMOVE_BEHAVIOURS = bidict(dict(left_tab=QtWidgets.QTabBar.SelectLeftTab,
+                                right_tab=QtWidgets.QTabBar.SelectRightTab,
+                                previous_tab=QtWidgets.QTabBar.SelectPreviousTab))
 
 SHAPES = bidict(dict(rounded_north=QtWidgets.QTabBar.RoundedNorth,
                      rounded_south=QtWidgets.QTabBar.RoundedSouth,
@@ -47,6 +47,25 @@ class TabBar(QtWidgets.QTabBar):
     def __setitem__(self, index, value):
         self.set_tab(index[0], index[1], value)
 
+    def __getstate__(self):
+        return dict(movable=self.isMovable(),
+                    document_mode=self.documentMode(),
+                    current_index=self.currentIndex(),
+                    # shape=self.shape(),
+                    draw_base=self.drawBase(),
+                    elide_mode=self.get_elide_mode(),
+                    icon_size=self.iconSize())
+
+    def __setstate__(self, state):
+        self.__init__()
+        self.setDocumentMode(state.get("document_mode", False))
+        self.setMovable(state.get("movable", False))
+        # self.setShape(state.get("shape", "rounded"))
+        self.setIconSize(state["icon_size"])
+        self.setDrawBase(state.get("draw_base"))
+        self.set_elide_mode(state.get("elide_mode"))
+        self.setCurrentIndex(state.get("index", 0))
+
     #  Send the on_detach when a tab is double clicked
     def mouseDoubleClickEvent(self, event):
         event.accept()
@@ -67,12 +86,12 @@ class TabBar(QtWidgets.QTabBar):
         Args:
             mode: new remove behaviour
         """
-        if mode not in SELECTION_MODES:
+        if mode not in REMOVE_BEHAVIOURS:
             raise ValueError("Mode not available")
-        self.setSelectionBehaviorOnRemove(SELECTION_MODES[mode])
+        self.setSelectionBehaviorOnRemove(REMOVE_BEHAVIOURS[mode])
 
     def get_remove_behaviour(self) -> str:
-        return SELECTION_MODES.inv[self.selectionBehaviorOnRemove()]
+        return REMOVE_BEHAVIOURS.inv[self.selectionBehaviorOnRemove()]
 
     def set_elide_mode(self, mode: str):
         if mode not in ELIDE_MODES:
