6d97da366ce36a951494a766bbaeee2e1c379597
phil65 <phil65@kodi.tv>
2019-05-19 16:27:39 +0200

    tableview: custom header for verticalHeader

--- prettyqt/widgets/tableview.py
+++ prettyqt/widgets/tableview.py
@@ -14,6 +14,7 @@ class TableView(QtWidgets.QTableView):
         super().__init__(*args, **kwargs)
         self.setObjectName(self.__class__.__name__)
         self.setHorizontalHeader(widgets.HeaderView(parent=self))
+        self.setVerticalHeader(widgets.HeaderView(parent=self))
         self.setAlternatingRowColors(True)
         self.setWordWrap(False)
 
