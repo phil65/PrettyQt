c4c24b4d8d9fc9120ed6749e31aa461d1834ca65
phil65 <phil65@kodi.tv>
2019-05-21 23:15:16 +0200

    messagebox work

--- prettyqt/widgets/messagebox.py
+++ prettyqt/widgets/messagebox.py
@@ -3,13 +3,27 @@
 @author: Philipp Temminghoff
 """
 
+from bidict import bidict
+
 from qtpy import QtWidgets
 
 from prettyqt import widgets
 
 
+ICONS = bidict(dict(none=QtWidgets.QMessageBox.NoIcon,
+                    information=QtWidgets.QMessageBox.Information,
+                    warning=QtWidgets.QMessageBox.Warning,
+                    critical=QtWidgets.QMessageBox.Critical,
+                    question=QtWidgets.QMessageBox.Question))
+
+
 class MessageBox(QtWidgets.QMessageBox):
-    pass
+
+    @classmethod
+    def message(cls, msg, title=None, msg_type=None):
+        m = cls(cls.NoIcon, title, msg)
+        m.setIcon(ICONS[msg_type])
+        m.exec_()
 
 
 MessageBox.__bases__[0].__bases__ = (widgets.BaseDialog,)
@@ -17,6 +31,6 @@ MessageBox.__bases__[0].__bases__ = (widgets.BaseDialog,)
 
 if __name__ == "__main__":
     app = widgets.app()
-    widget = MessageBox()
+    widget = MessageBox.message("Test", "header", "warning")
     widget.show()
     app.exec_()
