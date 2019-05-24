5c823a1aed6aba4426e75312df7d726fbfe7dfce
phil65 <phil65@kodi.tv>
2019-05-24 18:08:37 +0200

    add listview.set_view_mode

--- prettyqt/widgets/listview.py
+++ prettyqt/widgets/listview.py
@@ -3,12 +3,42 @@
 @author: Philipp Temminghoff
 """
 
+from bidict import bidict
+
 from qtpy import QtWidgets
 from prettyqt import widgets
 
 
+VIEW_MODES = bidict(dict(list=QtWidgets.QListView.ListMode,
+                         icon=QtWidgets.QListView.IconMode))
+
+
 class ListView(QtWidgets.QListView):
-    pass
+
+    def set_view_mode(self, mode: str):
+        """set view mode
+
+        possible values are "list", "icon"
+
+        Args:
+            mode: view mode to use
+
+        Raises:
+            ValueError: invalid view mode
+        """
+        if mode not in VIEW_MODES:
+            raise ValueError(f"Invalid value. Valid values: {VIEW_MODES.keys()}")
+        self.setViewMode(VIEW_MODES[mode])
+
+    def get_view_mode(self) -> str:
+        """returns view mode
+
+        possible values are "list", "icon"
+
+        Returns:
+            view mode
+        """
+        return VIEW_MODES.inv[self.viewMode()]
 
 
 ListView.__bases__[0].__bases__ = (widgets.AbstractItemView,)
@@ -16,7 +46,6 @@ ListView.__bases__[0].__bases__ = (widgets.AbstractItemView,)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    dlg = QtWidgets.QMainWindow()
-    status_bar = ListView()
+    dlg = ListView()
     dlg.show()
     app.exec_()
