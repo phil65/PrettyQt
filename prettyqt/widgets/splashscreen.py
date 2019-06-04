382d9bc1e2e6d2b0e14cb7964c08a41410b3f49e
phil65 <phil65@kodi.tv>
2019-06-04 15:36:42 +0200

    splashscreen.set_text

--- prettyqt/widgets/splashscreen.py
+++ prettyqt/widgets/splashscreen.py
@@ -3,9 +3,22 @@
 @author: Philipp Temminghoff
 """
 
+from bidict import bidict
+
 from qtpy import QtCore, QtWidgets
 
 from prettyqt import gui, widgets
+
+
+H_ALIGNMENTS = bidict(dict(left=QtCore.Qt.AlignLeft,
+                           right=QtCore.Qt.AlignRight,
+                           center=QtCore.Qt.AlignHCenter,
+                           justify=QtCore.Qt.AlignJustify))
+
+V_ALIGNMENTS = bidict(dict(top=QtCore.Qt.AlignTop,
+                           bottom=QtCore.Qt.AlignBottom,
+                           center=QtCore.Qt.AlignVCenter,
+                           baseline=QtCore.Qt.AlignBaseline))
 
 
 class SplashScreen(QtWidgets.QSplashScreen):
@@ -25,6 +38,11 @@ class SplashScreen(QtWidgets.QSplashScreen):
 
     def __exit__(self, typ, value, traceback):
         self.hide()
+
+    def set_text(self, text, color="black", h_align="center", v_align="bottom"):
+        self.showMessage(text,
+                         color=gui.Color(color),
+                         alignment=H_ALIGNMENTS[h_align] | V_ALIGNMENTS[v_align])
 
 
 SplashScreen.__bases__[0].__bases__ = (widgets.Widget,)
