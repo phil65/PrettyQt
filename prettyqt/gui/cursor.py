9b30d358c559275c854a0d36f5505f66ad08645c
phil65 <phil65@kodi.tv>
2019-07-10 01:25:11 +0200

    gui.cursor fixes + docs

--- prettyqt/gui/cursor.py
+++ prettyqt/gui/cursor.py
@@ -5,14 +5,39 @@
 
 from qtpy import QtGui, QtCore
 
-SHAPES = dict(arrow=QtCore.Qt.ArrowCursor,
-              uparrow=QtCore.Qt.ArrowCursor,
-              cross=QtCore.Qt.CrossCursor,
-              wait=QtCore.Qt.WaitCursor,
-              caret=QtCore.Qt.IBeamCursor)
+from prettyqt.utils import bidict
+
+
+SHAPES = bidict(arrow=QtCore.Qt.ArrowCursor,
+                uparrow=QtCore.Qt.UpArrowCursor,
+                cross=QtCore.Qt.CrossCursor,
+                wait=QtCore.Qt.WaitCursor,
+                caret=QtCore.Qt.IBeamCursor)
 
 
 class Cursor(QtGui.QCursor):
 
     def set_shape(self, shape: str):
+        """sets cursor shape
+
+        Allowed values are "arrow", "uparrow", "cross", "wait", "caret"
+
+        Args:
+            shape: shape to use
+
+        Raises:
+            ValueError: shape does not exist
+        """
+        if shape not in SHAPES:
+            raise ValueError("Invalid shape type.")
         self.setShape(SHAPES[shape])
+
+    def get_shape(self):
+        """returns current cursor shape
+
+        Possible values: "arrow", "uparrow", "cross", "wait", "caret"
+
+        Returns:
+            cursor shape
+        """
+        return SHAPES.inv[self.shape()]
