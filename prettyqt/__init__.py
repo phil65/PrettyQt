49ccdab135d7e017f1cbc01e64b23c29008cfb1a
phil65 <phil65@kodi.tv>
2019-07-23 04:15:33 +0200

    Bump version: 0.48.0 → 0.49.0

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Philipp Temminghoff"""
 __email__ = "phil65@kodi.tv"
-__version__ = "0.48.0"
+__version__ = "0.49.0"
 
 
 __all__ = ["core",
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/phil65/prettyqt",
-    version="0.48.0",
+    version="0.49.0",
     zip_safe=False,
     cmdclass={"build_sphinx": BuildDoc}
 )
