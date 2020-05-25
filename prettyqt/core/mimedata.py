48c84d3c4e861b7bcb0dbf4efe64c2f24a11033a
phil65 <phil65@kodi.tv>
2020-05-25 16:32:35 +0200

    mimedata typing

--- prettyqt/core/mimedata.py
+++ prettyqt/core/mimedata.py
@@ -7,14 +7,13 @@ from qtpy import QtCore
 
 from prettyqt import core
 
-
 QtCore.QMimeData.__bases__ = (core.Object,)
 
 
 class MimeData(QtCore.QMimeData):
 
-    def set_data(self, mime_type, data):
+    def set_data(self, mime_type: str, data: str):
         self.setData(mime_type, QtCore.QByteArray(data.encode()))
 
-    def get_data(self, mime_type):
+    def get_data(self, mime_type: str):
         return bytes(self.data(mime_type)).decode()
