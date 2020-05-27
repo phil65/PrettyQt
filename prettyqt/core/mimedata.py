e186eb15d3ecd2984121ecf34ddff4637afcb212
phil65 <phil65@kodi.tv>
2020-05-27 06:36:47 +0200

    mimedata.get/set_json_data

--- prettyqt/core/mimedata.py
+++ prettyqt/core/mimedata.py
@@ -3,6 +3,8 @@
 @author: Philipp Temminghoff
 """
 
+import json
+
 from qtpy import QtCore
 
 from prettyqt import core
@@ -15,5 +17,12 @@ class MimeData(QtCore.QMimeData):
     def set_data(self, mime_type: str, data: str):
         self.setData(mime_type, QtCore.QByteArray(data.encode()))
 
-    def get_data(self, mime_type: str):
+    def set_json_data(self, mime_type: str, data):
+        self.set_data(mime_type, json.dumps(data))
+
+    def get_data(self, mime_type: str) -> str:
         return bytes(self.data(mime_type)).decode()
+
+    def get_json_data(self, mime_type: str):
+        data = self.get_data(mime_type)
+        return json.loads(data)
