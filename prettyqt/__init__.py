382e3c3c97c17d4814648239c6019421f1040976
phil65 <philipptemminghoff@googlemail.com>
2020-05-31 18:39:42 +0200

    Bump version: 0.51.3 → 0.52.0

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Philipp Temminghoff"""
 __email__ = "phil65@kodi.tv"
-__version__ = "0.51.3"
+__version__ = "0.52.0"
 
 
 __all__ = ["core",
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/phil65/prettyqt",
-    version="0.51.3",
+    version="0.52.0",
     zip_safe=False,
     cmdclass={"build_sphinx": BuildDoc}
 )
