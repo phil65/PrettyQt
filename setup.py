1ecd001a021cf6e2278d294a7b8ef20b0e6c1a80
phil65 <philipptemminghoff@googlemail.com>
2020-06-17 13:14:52 +0200

    Bump version: 0.57.1 → 0.58.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/phil65/prettyqt",
-    version="0.57.1",
+    version="0.58.0",
     zip_safe=False,
     cmdclass={"build_sphinx": BuildDoc}
 )
