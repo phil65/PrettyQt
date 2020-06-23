353df75348dcb27e8bde6c1852bebbef4e9150d8
phil65 <philipptemminghoff@googlemail.com>
2020-06-24 00:18:55 +0200

    multiline match highlighter

--- prettyqt/custom_widgets/regexeditor/match_highlighter.py
+++ prettyqt/custom_widgets/regexeditor/match_highlighter.py
@@ -13,13 +13,41 @@ class MatchHighlighter(gui.SyntaxHighlighter):
         self.prog = None
         self._format = gui.TextCharFormat()
         self._format.set_background_color("lightgreen")
+        self.matches = []
 
     def set_prog(self, prog):
         self.prog = prog
+        text = self.document().toPlainText()
+        if self.prog is not None:
+            self.matches = [m.span() for m in self.prog.finditer(text)]
+            print(self.matches)
         self.rehighlight()
 
     def highlightBlock(self, text):
-        if self.prog and text:
-            for m in self.prog.finditer(text):
-                start, end = m.span()
-                self.setFormat(start, end - start, self._format)
+        block = self.currentBlock()
+        # line_no = block.blockNumber()
+        # if line_no == 0:
+        #     self.setCurrentBlockState(-1)
+        start_char = block.position()
+        end_char = start_char + block.length()
+        # print(f"\nline {line_no} ({start_char} - {end_char})")
+        # print(f"prev block state: {self.previousBlockState()}")
+        if not self.matches or not text:
+            return None
+        for start, end in self.matches:
+            match_len = end - start
+            starts_in_line = start_char <= start <= end_char
+            ends_in_line = start_char <= end <= end_char
+            line_pos = start - start_char
+            if starts_in_line and ends_in_line:
+                # print(f"in line: {line_pos} - {line_pos + match_len}")
+                self.setFormat(line_pos, line_pos + match_len, self._format)
+            elif ends_in_line:
+                # if self.previousBlockState() == 1:
+                # print(f"ends: {end}")
+                self.setFormat(0, end, self._format)
+                self.setCurrentBlockState(-1)
+            elif starts_in_line:
+                # print(f"starts: {line_pos}")
+                self.setCurrentBlockState(1)
+                self.setFormat(line_pos, end_char, self._format)
