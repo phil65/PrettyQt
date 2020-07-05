329ee6584f252e21f48ae2a4049b424268a8091c
phil65 <philipptemminghoff@googlemail.com>
2020-07-05 22:29:29 +0200

    feat: add checkboxdelegate

---
+++ prettyqt/custom_widgets/checkboxdelegate.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import logging
+
+from prettyqt import core, widgets
+
+logger = logging.getLogger(__name__)
+
+
+class CheckBoxDelegate(widgets.ItemDelegate):
+    """
+    A delegate that places a fully functioning CheckBox in every
+    cell of the column to which it's applied
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.selected = []
+
+    def createEditor(self, parent, option, index):
+        """
+        override
+        instanciate the editor widget and initialize it
+        also connect currentIndexChanged signal
+        """
+        cb = widgets.CheckBox(parent)
+        cb.currentIndexChanged.connect(self.currentIndexChanged)
+        return cb
+
+    def setEditorData(self, cb, index):
+        """
+        override
+        set correct initial value for editor widget
+        """
+        current_selection = index.data()
+        with cb.block_signals():
+            cb.setCurrentText(current_selection)
+
+    def setModelData(self, combo, model, index):
+        """
+        override, gets called on self.commitData (?)
+        apply the newly selected dtype to the column if possible
+        """
+        dtype = self.dtypes[combo.currentText()]
+        # s = model.data(index, model.DATA_ROLE)
+        try:
+            model.setData(index, dtype, model.DTYPE_ROLE)
+        except ValueError as e:
+            logger.error(e)
+
+    @core.Slot()
+    def currentIndexChanged(self):
+        self.commitData.emit(self.sender())
