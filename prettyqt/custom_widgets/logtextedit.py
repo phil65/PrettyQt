21a0c4ba504c2f9d13fc0d9fce972db19119b418
phil65 <philipptemminghoff@googlemail.com>
2020-07-08 13:59:16 +0200

    fix: remove Qt logger on Application exit

--- prettyqt/custom_widgets/logtextedit.py
+++ prettyqt/custom_widgets/logtextedit.py
@@ -202,6 +202,7 @@ class LogTextEdit(widgets.PlainTextEdit):
         # self.handler.log_line.connect(self.append_text)
         self.handler = signallogger.RecordSignalLogger()
         self.handler.log_record.connect(self.append_record)
+        widgets.Application.call_on_exit(lambda: logger.removeHandler(self.handler))
         self.handler.setLevel(logging.INFO)
         logger.addHandler(self.handler)
         fmt = logging.Formatter("%(asctime)s  %(levelname)i  %(message)s")
