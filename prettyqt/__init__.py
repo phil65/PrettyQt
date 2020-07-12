3c47cae1d7c44d7fd59d220d78567c72e556af20
phil65 <philipptemminghoff@googlemail.com>
2020-07-12 16:23:59 +0200

    bump: version 0.86.1 → 0.86.2

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Philipp Temminghoff"""
 __email__ = "phil65@kodi.tv"
-__version__ = "0.86.1"
+__version__ = "0.86.2"
 
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages(),
     test_suite="tests",
     url="https://github.com/phil65/prettyqt",
-    version="0.86.1",
+    version="0.86.2",
     zip_safe=False,
 )
