f2ec5a029ed9de58bda598355ef9ac48d76ebced
phil65 <philipptemminghoff@googlemail.com>
2020-07-13 15:21:44 +0200

    chore(SidebarWidget): prevent context menu

--- prettyqt/custom_widgets/sidebarwidget.py
+++ prettyqt/custom_widgets/sidebarwidget.py
@@ -19,6 +19,7 @@ class SidebarWidget(widgets.MainWindow):
         self.sidebar.set_id("SidebarWidget")
         self.sidebar.set_title("Sidebar")
         self.sidebar.set_style("text_below_icon")
+        self.sidebar.set_contextmenu_policy("prevent")
         self.sidebar.setFloatable(False)
         self.sidebar.set_allowed_areas("all")
         self.sidebar.set_icon_size(60)
