a6d8e919ee08f197f88ff380931ee1f968b49f6d
phil65 <philipptemminghoff@googlemail.com>
2020-07-14 03:06:50 +0200

    chore: test fix

--- tests/test_winextras.py
+++ tests/test_winextras.py
@@ -14,14 +14,18 @@ except (ImportError, ModuleNotFoundError):
     pass
 
 
-@pytest.mark.skipif(sys.patform != "win32", reason="Only supported on windows")
+@pytest.mark.skipif(sys.platform != "win32", reason="Only supported on windows")
 def test_winjumplistcategory(qapp):
     cat = winextras.WinJumpListCategory()
     assert len(cat) == 0
     assert cat.get_type() == "custom"
+    cat.set_title("test")
+    path = pathlib.Path.home()
+    cat.add_destination(path)
+    cat.add_link("test", path)
 
 
-@pytest.mark.skipif(sys.patform != "win32", reason="Only supported on windows")
+@pytest.mark.skipif(sys.platform != "win32", reason="Only supported on windows")
 def test_winjumplistitem(qapp):
     item = winextras.WinJumpListItem("destination")
     item.set_title("test")
@@ -35,7 +39,7 @@ def test_winjumplistitem(qapp):
     assert item.get_type() == "destination"
 
 
-@pytest.mark.skipif(sys.patform != "win32", reason="Only supported on windows")
+@pytest.mark.skipif(sys.platform != "win32", reason="Only supported on windows")
 def test_winjumplist(qapp):
     jumplist = winextras.WinJumpList()
     jumplist.add_category("Test")
