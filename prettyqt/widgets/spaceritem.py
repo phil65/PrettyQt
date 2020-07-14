9dc447a06ffc08661a9911e6c58febafca41fb3f
phil65 <philipptemminghoff@googlemail.com>
2020-07-14 20:38:18 +0200

    feat(SpacerItem): allow strings for size policy in ctor

--- prettyqt/widgets/spaceritem.py
+++ prettyqt/widgets/spaceritem.py
@@ -9,6 +9,20 @@ from prettyqt import widgets
 
 QtWidgets.QSpacerItem.__bases__ = (widgets.LayoutItem,)
 
+SIZE_POLICIES = widgets.sizepolicy.SIZE_POLICIES
+
 
 class SpacerItem(QtWidgets.QSpacerItem):
-    pass
+    def __init__(self, w, h, h_policy, v_policy):
+        if h_policy in SIZE_POLICIES:
+            h_policy = SIZE_POLICIES[h_policy]
+        if v_policy in SIZE_POLICIES:
+            v_policy = SIZE_POLICIES[v_policy]
+        super().__init__(w, h, h_policy, v_policy)
+
+    def change_size(self, w, h, h_policy="minimum", v_policy="minimum"):
+        if h_policy in SIZE_POLICIES:
+            h_policy = SIZE_POLICIES[h_policy]
+        if v_policy in SIZE_POLICIES:
+            v_policy = SIZE_POLICIES[v_policy]
+        self.changeSize(w, h, h_policy, v_policy)
