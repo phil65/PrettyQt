7afde8c5ce2c05e7b712093b5d6b7753c1e99e9f
phil65 <philipptemminghoff@googlemail.com>
2020-07-20 03:20:09 +0200

    feat(ToolTip): add show_text method

--- prettyqt/widgets/tooltip.py
+++ prettyqt/widgets/tooltip.py
@@ -2,9 +2,27 @@
 """
 """
 
-from qtpy import QtWidgets
+from typing import Optional
+from qtpy import QtWidgets, QtGui, QtCore
 
 
 class ToolTip(QtWidgets.QToolTip):
-    def set_formatted_text(self, text, linebreak_px=400):
-        self.setText(f'<div style="max-width: {linebreak_px}px">{text}</div>')
+    @classmethod
+    def show_text(
+        cls,
+        position: Optional[QtCore.QPoint] = None,
+        text: str = "",
+        linebreak_px: int = 400,
+    ):
+        if position is None:
+            cursor = QtGui.QCursor()
+            position = cursor.pos()
+        cls.showText(position, f'<div style="max-width: {linebreak_px}px">{text}</div>')
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    app = widgets.app()
+    ToolTip.show_text(text="test")
+    app.exec_()
