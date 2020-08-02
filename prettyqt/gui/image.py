746ecb47186e1ab927d26e379f8f3ad712ea6ee8
phil65 <philipptemminghoff@googlemail.com>
2020-08-02 19:58:31 +0200

    feat(Image): allow pickling

--- prettyqt/gui/image.py
+++ prettyqt/gui/image.py
@@ -2,14 +2,19 @@
 
 from qtpy import QtGui
 
-from prettyqt import gui
+from prettyqt import gui, core
 
 
 QtGui.QImage.__bases__ = (gui.PaintDevice,)
 
 
 class Image(QtGui.QImage):
-    pass
+    def __getstate__(self):
+        return core.DataStream.create_bytearray(self)
+
+    def __setstate__(self, ba):
+        self.__init__()
+        core.DataStream.write_bytearray(ba, self)
 
 
 if __name__ == "__main__":
