06f9259f44a287e5cf21b736443eb8bb72af3ffc
phil65 <philipptemminghoff@googlemail.com>
2020-08-08 19:02:08 +0200

    feat(gui): some magic methods + get_matches for KeySequence

--- prettyqt/gui/keysequence.py
+++ prettyqt/gui/keysequence.py
@@ -2,20 +2,44 @@
 
 from qtpy import QtCore, QtGui
 
+from prettyqt.utils import bidict
+
+
+SEQUENCE_MATCHES = bidict(
+    none=QtGui.QKeySequence.NoMatch,
+    partial=QtGui.QKeySequence.PartialMatch,
+    exact=QtGui.QKeySequence.ExactMatch,
+)
+
+MODS = {
+    QtCore.Qt.ShiftModifier: QtCore.Qt.SHIFT,
+    QtCore.Qt.ControlModifier: QtCore.Qt.CTRL,
+    QtCore.Qt.AltModifier: QtCore.Qt.ALT,
+    QtCore.Qt.MetaModifier: QtCore.Qt.META,
+}
+
 
 class KeySequence(QtGui.QKeySequence):
     def __str__(self):
         return self.toString()
 
+    def __repr__(self):
+        return f"KeySequence({self.toString()!r})"
+
+    def __bool__(self):
+        return not self.isEmpty()
+
+    def __reduce__(self):
+        return (self.__class__, (self.toString()))
+
+    def get_matches(self, seq):
+        if isinstance(seq, str):
+            seq = KeySequence(seq)
+        return SEQUENCE_MATCHES.inv[self.matches(seq)]
+
     @classmethod
     def to_shortcut_str(cls, key, mod=0):
-        mods = {
-            QtCore.Qt.ShiftModifier: QtCore.Qt.SHIFT,
-            QtCore.Qt.ControlModifier: QtCore.Qt.CTRL,
-            QtCore.Qt.AltModifier: QtCore.Qt.ALT,
-            QtCore.Qt.MetaModifier: QtCore.Qt.META,
-        }
-        for k, v in mods.items():
+        for k, v in MODS.items():
             if mod & k:
                 key += v
         return str(cls(key))
