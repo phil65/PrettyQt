3d6f91766adce92df82a93183db13ba573a80aee
phil65 <philipptemminghoff@googlemail.com>
2020-08-13 14:43:24 +0200

    feat(Brush): allow pickling + add get_texture_image method

--- prettyqt/gui/brush.py
+++ prettyqt/gui/brush.py
@@ -2,6 +2,16 @@
 
 from qtpy import QtGui
 
+from prettyqt import core, gui
+
 
 class Brush(QtGui.QBrush):
-    pass
+    def __getstate__(self):
+        return core.DataStream.create_bytearray(self)
+
+    def __setstate__(self, ba):
+        self.__init__()
+        core.DataStream.write_bytearray(ba, self)
+
+    def get_texture_image(self):
+        return gui.Image(self.textureImage())
