94d064d411aa00e06704e1e5601360b6357dd76a
phil65 <philipptemminghoff@googlemail.com>
2020-08-22 13:23:29 +0200

    feat(StylePainter): add draw_complex_control method

--- prettyqt/widgets/stylepainter.py
+++ prettyqt/widgets/stylepainter.py
@@ -2,11 +2,13 @@
 
 from qtpy import QtWidgets
 
-from prettyqt import gui
+from prettyqt import gui, widgets
 
+COMPLEX_CONTROLS = widgets.style.COMPLEX_CONTROLS
 
 QtWidgets.QStylePainter.__bases__ = (gui.Painter,)
 
 
 class StylePainter(QtWidgets.QStylePainter):
-    pass
+    def draw_complex_control(self, control: str, option: QtWidgets.QStyleOptionComplex):
+        self.drawComplexControl(COMPLEX_CONTROLS[control], option)
