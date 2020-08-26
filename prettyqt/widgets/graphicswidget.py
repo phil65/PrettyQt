0d671251bf43409f84585336afb5d412e21aed98
phil65 <philipptemminghoff@googlemail.com>
2020-08-27 01:37:45 +0200

    feat(GraphicsWidget): add set_layout method

--- prettyqt/widgets/graphicswidget.py
+++ prettyqt/widgets/graphicswidget.py
@@ -1,13 +1,32 @@
-# -*- coding: utf-8 -*-
+from typing import Union
 
 from qtpy import QtWidgets
 
-from prettyqt import widgets
+from prettyqt import widgets, gui
 
 
 QtWidgets.QGraphicsWidget.__bases__ = (widgets.GraphicsObject, widgets.GraphicsLayoutItem)
 
 
 class GraphicsWidget(QtWidgets.QGraphicsWidget):
+    def serialize_fields(self):
+        return dict(
+            autofill_background=self.autoFillBackground(),
+            font=gui.Font(self.font()),
+            window_title=self.windowTitle(),
+        )
 
-    pass
+    def set_layout(self, layout: Union[str, QtWidgets.QGraphicsLayout, None]) -> None:
+        if layout is None:
+            return None
+        if layout == "grid":
+            self.box = widgets.GraphicsGridLayout()
+        elif layout in ["horizontal", "vertical"]:
+            self.box = widgets.GraphicsLinearLayout(layout)
+        elif layout == "anchor":
+            self.box = widgets.GraphicsAnchorLayout()
+        elif isinstance(layout, QtWidgets.QGraphicsLayout):
+            self.box = layout
+        else:
+            raise ValueError("Invalid Layout")
+        self.setLayout(self.box)
