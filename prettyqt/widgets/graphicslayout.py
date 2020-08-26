4e9801e3ef27baaa7cad4beb641e3440b5581b1d
phil65 <philipptemminghoff@googlemail.com>
2020-08-27 01:36:27 +0200

    feat(GraphicsLayout): add some magic methods

--- prettyqt/widgets/graphicslayout.py
+++ prettyqt/widgets/graphicslayout.py
@@ -6,4 +6,26 @@ QtWidgets.QGraphicsLayout.__bases__ = (widgets.GraphicsLayoutItem,)
 
 
 class GraphicsLayout(QtWidgets.QGraphicsLayout):
-    pass
+    def __getitem__(self, index: int) -> QtWidgets.QGraphicsItem:
+        layoutitem = self.itemAt(index)
+        return layoutitem.graphicsItem()
+
+    def __setitem__(self, index: int, value: QtWidgets.QGraphicsItem):
+        layoutitem = self.itemAt(index)
+        layoutitem.setGraphicsItem(value)
+
+    def __delitem__(self, index: int):
+        self.removeAt(index)
+
+    def __iter__(self):
+        return iter(self[i] for i in range(self.count()))
+
+    def __contains__(self, item):
+        return item in self.get_children()
+
+    def get_children(self) -> list:
+        return list(self)
+
+
+if __name__ == "__main__":
+    layout = GraphicsLayout()
