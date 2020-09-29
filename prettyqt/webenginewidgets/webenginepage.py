3106db854c7c94f56f101869b9535d04f245f9c6
phil65 <philipptemminghoff@googlemail.com>
2020-09-29 04:35:31 +0200

    chore: webenginepage serializing

--- prettyqt/webenginewidgets/webenginepage.py
+++ prettyqt/webenginewidgets/webenginepage.py
@@ -128,6 +128,18 @@ WEB_WINDOW_TYPES = bidict(
 class WebEnginePage(QtWebEngineWidgets.QWebEnginePage):
     """A web engine page holds the HTML document contents, link history + actions."""
 
+    def serialize_fields(self):
+        return dict(
+            audio_muted=self.isAudioMuted(),
+            background_color=self.backgroundColor(),
+            # has_selection=self.hasSelection(),
+            lifecycle_state=self.get_lifecycle_state(),
+            # scroll_position=self.scrollPosition(),
+            url=core.Url(self.url()),
+            visible=self.isVisible(),
+            zoom_factor=self.zoomFactor(),
+        )
+
     def set_url(self, url: Union[str, pathlib.Path]):
         """Set the url of the WebEnginePage.
 
