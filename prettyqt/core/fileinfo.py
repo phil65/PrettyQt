c5477f1e18716c3e2867ddddb01367614ddc7cc7
phil65 <philipptemminghoff@googlemail.com>
2020-10-02 04:47:04 +0200

    feat(FileInfo): support pathlib + add proper repr

--- prettyqt/core/fileinfo.py
+++ prettyqt/core/fileinfo.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import pathlib
+
 from qtpy import QtCore
 
 
 class FileInfo(QtCore.QFileInfo):
-    pass
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], pathlib.Path):
+            super().__init__(str(args[0]))
+        else:
+            super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return f"FileInfo({self.absoluteFilePath()!r})"
+
+
+if __name__ == "__main__":
+    p = pathlib.Path.home()
+    f = FileInfo(p)
