78572cd85a7a9fe210b3d4ddb1ea93d7183332a6
phil65 <philipptemminghoff@googlemail.com>
2020-10-07 00:25:21 +0200

    chore: use from_user_input for DesktopServices

--- prettyqt/gui/desktopservices.py
+++ prettyqt/gui/desktopservices.py
@@ -11,4 +11,4 @@ from prettyqt import core
 class DesktopServices(QtGui.QDesktopServices):
     @classmethod
     def open_url(cls, location: Union[str, pathlib.Path, QtCore.QUrl]) -> bool:
-        return cls.openUrl(core.Url(location))
+        return cls.openUrl(core.Url.from_user_input(str(location)))
