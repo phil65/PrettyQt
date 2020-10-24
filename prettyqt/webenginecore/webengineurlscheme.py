f1e4ab5eafb8f15b6b1475b9df956c00a848a033
phil65 <philipptemminghoff@googlemail.com>
2020-10-25 00:07:05 +0200

    chore: explicit qt importing

--- prettyqt/webenginecore/webengineurlscheme.py
+++ prettyqt/webenginecore/webengineurlscheme.py
@@ -2,9 +2,11 @@
 
 from qtpy import QtCore
 
-try:
+from qtpy import PYQT5, PYSIDE2
+
+if PYQT5:
     from PyQt5 import QtWebEngineCore  # type: ignore
-except ImportError:
+elif PYSIDE2:
     from PySide2 import QtWebEngineCore
 
 from prettyqt.utils import bidict, InvalidParamError
