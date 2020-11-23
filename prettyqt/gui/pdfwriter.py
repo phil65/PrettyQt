5ff07039385a9e44ba92356d709a4abcaea32132
phil65 <philipptemminghoff@googlemail.com>
2020-11-23 23:19:52 +0100

    chore: PdfWriter work

--- prettyqt/gui/pdfwriter.py
+++ prettyqt/gui/pdfwriter.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from qtpy import QtCore, QtGui
+from qtpy import QtCore, QtGui, QtWidgets
 
-from prettyqt import core, widgets
+from prettyqt import core
 
 
 class PdfWriter(QtGui.QPdfWriter):
-    def setup(self, size):
-        dpi = widgets.DesktopWidget().logicalDpiX()
+    def setup(self, size: QtCore.QSize):
+        dpi = QtWidgets.QDesktopWidget().logicalDpiX()
         self.setResolution(dpi)
         self.setPageMargins(QtCore.QMarginsF(0, 0, 0, 0))
         self.setPageSizeMM(core.SizeF(size.width(), size.height()) / dpi * 25.4)
