7893013e138526a44564072ed9b30a1d32ba01b6
phil65 <philipptemminghoff@googlemail.com>
2020-11-26 00:43:32 +0100

    feat(WebEngineHistoryItem): add get_icon_url method

--- prettyqt/webenginewidgets/webenginehistoryitem.py
+++ prettyqt/webenginewidgets/webenginehistoryitem.py
@@ -14,5 +14,8 @@ class WebEngineHistoryItem(QtWebEngineWidgets.QWebEngineHistoryItem):
     def get_url(self) -> core.Url:
         return core.Url(self.url())
 
+    def get_icon_url(self) -> core.Url:
+        return core.Url(self.iconUrl())
+
     def get_last_visited(self) -> core.DateTime:
         return core.DateTime(self.lastVisited())
