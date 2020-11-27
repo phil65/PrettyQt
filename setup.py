2354ebfa0341e41df0fd26924b03aba2dde3673d
phil65 <philipptemminghoff@googlemail.com>
2020-11-27 17:46:08 +0100

    bump: version 0.137.0 → 0.137.1

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     packages=find_packages(),
     test_suite="tests",
     url="https://github.com/phil65/prettyqt",
-    version="0.137.0",
+    version="0.137.1",
     zip_safe=False,
 )
