b571d019459544d80ac476a3544e72b5d8efe3d2
phil65 <philipptemminghoff@googlemail.com>
2020-11-27 04:48:36 +0100

    chore: fix DesktopWidget inheritance

--- prettyqt/widgets/desktopwidget.py
+++ prettyqt/widgets/desktopwidget.py
@@ -2,6 +2,11 @@
 
 from qtpy import QtWidgets
 
+from prettyqt import widgets
+
+
+QtWidgets.QDesktopWidget.__bases__ = (widgets.Widget,)
+
 
 class DesktopWidget(QtWidgets.QDesktopWidget):
     pass
