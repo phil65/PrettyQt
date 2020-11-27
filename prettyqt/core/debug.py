e4ed118a5be650c0bc634e54b780753f88bed2d8
phil65 <philipptemminghoff@googlemail.com>
2020-11-27 17:45:28 +0100

    fix: doc build fix

--- prettyqt/core/debug.py
+++ prettyqt/core/debug.py
@@ -1,43 +1,43 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-from qtpy import QtCore
+# from qtpy import QtCore
 
-from prettyqt.utils import bidict, InvalidParamError
+# from prettyqt.utils import bidict, InvalidParamError
 
-VERBOSITY_LEVEL = bidict(
-    minimum=QtCore.QDebug.MinimumVerbosity,
-    default=QtCore.QDebug.DefaultVerbosity,
-    maximum=QtCore.QDebug.MaximumVerbosity,
-)
-
-
-class Debug(QtCore.QDebug):
-    def set_verbosity(self, verbosity: str):
-        """Set the debug verbosity.
-
-        Valid values are "minimum", "default", "maximum"
-
-        Args:
-            verbosity: debug verbosity
-
-        Raises:
-            InvalidParamError: invalid debug verbosity
-        """
-        if verbosity not in VERBOSITY_LEVEL:
-            raise InvalidParamError(verbosity, VERBOSITY_LEVEL)
-        self.setVerbosity(VERBOSITY_LEVEL[verbosity])
-
-    def get_verbosity(self) -> str:
-        """Get current debug verbosity.
-
-        Possible values are "minimum", "default", "maximum"
-
-        Returns:
-            current debug verbosity
-        """
-        return VERBOSITY_LEVEL.inv[self.verbosity()]
+# VERBOSITY_LEVEL = bidict(
+#     minimum=QtCore.QDebug.MinimumVerbosity,
+#     default=QtCore.QDebug.DefaultVerbosity,
+#     maximum=QtCore.QDebug.MaximumVerbosity,
+# )
 
 
-if __name__ == "__main__":
-    matcher = Debug("Test")
-    print(repr(matcher))
+# class Debug(QtCore.QDebug):
+#     def set_verbosity(self, verbosity: str):
+#         """Set the debug verbosity.
+
+#         Valid values are "minimum", "default", "maximum"
+
+#         Args:
+#             verbosity: debug verbosity
+
+#         Raises:
+#             InvalidParamError: invalid debug verbosity
+#         """
+#         if verbosity not in VERBOSITY_LEVEL:
+#             raise InvalidParamError(verbosity, VERBOSITY_LEVEL)
+#         self.setVerbosity(VERBOSITY_LEVEL[verbosity])
+
+#     def get_verbosity(self) -> str:
+#         """Get current debug verbosity.
+
+#         Possible values are "minimum", "default", "maximum"
+
+#         Returns:
+#             current debug verbosity
+#         """
+#         return VERBOSITY_LEVEL.inv[self.verbosity()]
+
+
+# if __name__ == "__main__":
+#     matcher = Debug("Test")
+#     print(repr(matcher))
