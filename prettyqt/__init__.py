48de09d98883c8f7c263fe0ba8c74338659acb58
phil65 <philipptemminghoff@googlemail.com>
2020-12-02 22:35:46 +0100

    bump: version 0.140.2 → 0.140.3

--- prettyqt/__init__.py
+++ prettyqt/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Philipp Temminghoff"""
 __email__ = "phil65@kodi.tv"
-__version__ = "0.140.2"
+__version__ = "0.140.3"
 
 
 def debug():
--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     packages=find_packages(),
     test_suite="tests",
     url="https://github.com/phil65/prettyqt",
-    version="0.140.2",
+    version="0.140.3",
     zip_safe=False,
 )
