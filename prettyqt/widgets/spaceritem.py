507429cba9d4c4ab932bf4e0951f7d2a4d902ab6
phil65 <philipptemminghoff@googlemail.com>
2020-12-02 11:03:23 +0100

    chore: cleanup

--- prettyqt/widgets/spaceritem.py
+++ prettyqt/widgets/spaceritem.py
@@ -7,20 +7,18 @@ from prettyqt import widgets
 
 QtWidgets.QSpacerItem.__bases__ = (widgets.LayoutItem,)
 
-SIZE_POLICIES = widgets.sizepolicy.SIZE_POLICIES
-
 
 class SpacerItem(QtWidgets.QSpacerItem):
     def __init__(self, w, h, h_policy, v_policy):
-        if h_policy in SIZE_POLICIES:
-            h_policy = SIZE_POLICIES[h_policy]
-        if v_policy in SIZE_POLICIES:
-            v_policy = SIZE_POLICIES[v_policy]
+        if h_policy in widgets.sizepolicy.SIZE_POLICIES:
+            h_policy = widgets.sizepolicy.SIZE_POLICIES[h_policy]
+        if v_policy in widgets.sizepolicy.SIZE_POLICIES:
+            v_policy = widgets.sizepolicy.SIZE_POLICIES[v_policy]
         super().__init__(w, h, h_policy, v_policy)
 
     def change_size(self, w, h, h_policy="minimum", v_policy="minimum"):
-        if h_policy in SIZE_POLICIES:
-            h_policy = SIZE_POLICIES[h_policy]
-        if v_policy in SIZE_POLICIES:
-            v_policy = SIZE_POLICIES[v_policy]
+        if h_policy in widgets.sizepolicy.SIZE_POLICIES:
+            h_policy = widgets.sizepolicy.SIZE_POLICIES[h_policy]
+        if v_policy in widgets.sizepolicy.SIZE_POLICIES:
+            v_policy = widgets.sizepolicy.SIZE_POLICIES[v_policy]
         self.changeSize(w, h, h_policy, v_policy)
