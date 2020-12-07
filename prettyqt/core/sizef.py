8d6b0c3bc027b79526b21f77ad093ebc4073a091
phil65 <philipptemminghoff@googlemail.com>
2020-12-07 18:00:50 +0100

    feat(SizeF): add some methods to be on par with Size

--- prettyqt/core/sizef.py
+++ prettyqt/core/sizef.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import annotations
+
 from qtpy import QtCore
 
 
 class SizeF(QtCore.QSizeF):
     def __repr__(self):
         return f"SizeF({self.width()}, {self.height()})"
+
+    def __getitem__(self, index) -> float:
+        return (self.width(), self.height())[index]
+
+    def __reduce__(self):
+        return (self.__class__, (self.width(), self.height()))
+
+    def expanded_to(self, size: QtCore.QSizeF) -> SizeF:
+        return SizeF(self.expandedTo(size))
+
+
+if __name__ == "__main__":
+    size = SizeF(10, 20)
+    print(tuple(size))
+    size = size.expanded_to(QtCore.QSizeF(100, 100))
+    print(type(size))
