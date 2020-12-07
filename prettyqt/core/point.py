60d784f4af1a2f6e2c1a25c23d7359ff6394e9fd
phil65 <philipptemminghoff@googlemail.com>
2020-12-07 18:20:17 +0100

    chore(Point/PointF): add __getitem__ method to allow tuple()

--- prettyqt/core/point.py
+++ prettyqt/core/point.py
@@ -6,3 +6,6 @@ from qtpy import QtCore
 class Point(QtCore.QPoint):
     def __repr__(self):
         return f"Point(x={self.x()}, y={self.y()})"
+
+    def __getitem__(self, index) -> int:
+        return (self.x(), self.y())[index]
--- prettyqt/core/pointf.py
+++ prettyqt/core/pointf.py
@@ -6,3 +6,6 @@ from qtpy import QtCore
 class PointF(QtCore.QPointF):
     def __repr__(self):
         return f"PointF(x={self.x()}, y={self.y()})"
+
+    def __getitem__(self, index) -> float:
+        return (self.x(), self.y())[index]
