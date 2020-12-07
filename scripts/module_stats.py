ace714e7b46c17d3fdd03011ce0eb147c0b8f03c
phil65 <philipptemminghoff@googlemail.com>
2020-12-07 03:46:42 +0100

    chore: dont show obselete classes in module_stats

--- scripts/module_stats.py
+++ scripts/module_stats.py
@@ -3,6 +3,7 @@
 from prettyqt import gui, widgets, core
 from qtpy import QtGui, QtWidgets, QtCore
 
+OBSELETE = ["QPictureIO", "QDesktopWidget", "QDirModel"]
 
 mapper = {gui: QtGui, widgets: QtWidgets, core: QtCore}
 
@@ -12,5 +13,5 @@ if __name__ == "__main__":
         ours = dir(k)
         print(f"Missing modules for {k.__name__}")
         for i in theirs:
-            if i[1:] not in ours and i[0] == "Q":
+            if i[1:] not in ours and i[0] == "Q" and i not in OBSELETE:
                 print(f"{v.__name__}.{i}")
