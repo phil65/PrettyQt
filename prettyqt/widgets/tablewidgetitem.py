76114289ddc5f17e8c89248639cb27f20161910a
phil65 <philipptemminghoff@googlemail.com>
2020-12-14 11:31:12 +0100

    feat(TableWidgetItem): add set_text_alignment method

--- prettyqt/widgets/tablewidgetitem.py
+++ prettyqt/widgets/tablewidgetitem.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from qtpy import QtWidgets, QtCore
 
@@ -12,6 +12,34 @@ STATE = bidict(
 )
 
 StateStr = Literal["unchecked", "partial", "checked"]
+
+H_ALIGNMENTS = bidict(
+    left=QtCore.Qt.AlignLeft,
+    right=QtCore.Qt.AlignRight,
+    center=QtCore.Qt.AlignHCenter,
+    justify=QtCore.Qt.AlignJustify,
+)
+
+HorizontalAlignmentStr = Literal[
+    "left",
+    "right",
+    "center",
+    "justify",
+]
+
+V_ALIGNMENTS = bidict(
+    top=QtCore.Qt.AlignTop,
+    bottom=QtCore.Qt.AlignBottom,
+    center=QtCore.Qt.AlignVCenter,
+    baseline=QtCore.Qt.AlignBaseline,
+)
+
+VerticalAlignmentStr = Literal[
+    "top",
+    "bottom",
+    "center",
+    "baseline",
+]
 
 
 class TableWidgetItem(QtWidgets.QTableWidgetItem):
@@ -60,6 +88,29 @@ class TableWidgetItem(QtWidgets.QTableWidgetItem):
             checkstate
         """
         return STATE.inverse[self.checkState()]
+
+    def set_text_alignment(
+        self,
+        horizontal: Optional[HorizontalAlignmentStr] = None,
+        vertical: Optional[VerticalAlignmentStr] = None,
+    ):
+        """Set text alignment of the checkbox.
+
+        Args:
+            alignment: text alignment to use
+
+        Raises:
+            InvalidParamError: invalid text alignment
+        """
+        if horizontal is None and vertical is not None:
+            flag = V_ALIGNMENTS[vertical]
+        elif vertical is None and horizontal is not None:
+            flag = H_ALIGNMENTS[horizontal]
+        elif vertical is not None and horizontal is not None:
+            flag = V_ALIGNMENTS[vertical] | H_ALIGNMENTS[horizontal]
+        else:
+            return
+        self.setTextAlignment(flag)
 
     def get_background(self) -> gui.Brush:
         return gui.Brush(self.background())
