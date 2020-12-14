9eb5d1c4c5460b605df12b62b9eaadcf63b53b29
phil65 <philipptemminghoff@googlemail.com>
2020-12-14 12:25:21 +0100

    feat(Region): add some additional methods

--- prettyqt/gui/region.py
+++ prettyqt/gui/region.py
@@ -1,9 +1,22 @@
+from typing import Literal
+
 from qtpy import QtGui
 
 from prettyqt import core
+from prettyqt.utils import bidict
+
+REGION_TYPE = bidict(
+    rectangle=QtGui.QRegion.Rectangle,
+    ellipse=QtGui.QRegion.Ellipse,
+)
+
+RegionTypeStr = Literal["rectangle", "ellipse"]
 
 
 class Region(QtGui.QRegion):
+    def __repr__(self):
+        return f"{type(self).__name__}({self.get_bounding_rect()}, {self.get_shape()!r})"
+
     def __getstate__(self):
         return bytes(self)
 
@@ -16,3 +29,14 @@ class Region(QtGui.QRegion):
     def __bytes__(self):
         ba = core.DataStream.create_bytearray(self)
         return bytes(ba)
+
+    def get_bounding_rect(self) -> core.Rect:
+        return core.Rect(self.boundingRect())
+
+    def get_shape(self) -> RegionTypeStr:  # workaround for not being able to get shape
+        return "rectangle" if self == Region(self.get_bounding_rect()) else "ellipse"
+
+
+if __name__ == "__main__":
+    region = Region(0, 0, 10, 10)
+    print(repr(region))
