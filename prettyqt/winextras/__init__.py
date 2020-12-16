5eab56749f57ccd77640967076dc6e74324c8d08
phil65 <philipptemminghoff@googlemail.com>
2020-12-16 10:44:17 +0100

    feat(winextras): add WinThumbnailToolButton class

--- prettyqt/winextras/__init__.py
+++ prettyqt/winextras/__init__.py
@@ -7,7 +7,13 @@ try:
     from .winjumplistcategory import WinJumpListCategory
     from .winjumplistitem import WinJumpListItem
     from .winjumplist import WinJumpList
+    from .winthumbnailtoolbutton import WinThumbnailToolButton
 
-    __all__ = ["WinJumpListCategory", "WinJumpListItem", "WinJumpList"]
+    __all__ = [
+        "WinJumpListCategory",
+        "WinJumpListItem",
+        "WinJumpList",
+        "WinThumbnailToolButton",
+    ]
 except (ImportError, ModuleNotFoundError):
     pass
---
+++ prettyqt/winextras/winthumbnailtoolbutton.py
@@ -0,0 +1,23 @@
+from qtpy import PYQT5, PYSIDE2
+
+if PYQT5:
+    from PyQt5 import QtWinExtras  # type: ignore
+elif PYSIDE2:
+    from PySide2 import QtWinExtras
+
+from prettyqt import core
+
+
+QtWinExtras.QWinThumbnailToolButton.__bases__ = (core.Object,)
+
+
+class WinThumbnailToolButton(QtWinExtras.QWinThumbnailToolButton):
+    pass
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    app = widgets.app()
+    btn = WinThumbnailToolButton()
+    # app.main_loop()
