e4441f2a0fcf2333f0cce15d38745b05e36482c6
phil65 <philipptemminghoff@googlemail.com>
2020-12-20 22:10:45 +0100

    chore: qt update

--- prettyqt/qt/__init__.py
+++ prettyqt/qt/__init__.py
@@ -1,9 +1,20 @@
-"""Provides QtGui classes and functions."""
+"""Provides Qt init stuff."""
+
+import os
 
 
 class PythonQtError(ImportError):
     pass
 
 
-PYQT5 = True
-PYSIDE2 = True
+os.environ.setdefault("QT_API", "pyqt5")
+
+API = os.environ["QT_API"].lower()
+
+PYQT5 = API == "pyqt5"
+PYSIDE2 = API == "pyside2"
+
+if API == "pyqt5":
+    from PyQt5.QtCore import QT_VERSION_STR as QT_VERSION
+else:
+    from PySide2.QtCore import __version__ as QT_VERSION  # type: ignore
