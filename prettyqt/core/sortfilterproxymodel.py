074a0adcb094e11167c471d2fc5113b2af0b4f93
phil65 <philipptemminghoff@googlemail.com>
2020-12-20 17:04:34 +0100

    feat(SortFilterProxyModel): add is/set_filter_case_sensitive methods

--- prettyqt/core/sortfilterproxymodel.py
+++ prettyqt/core/sortfilterproxymodel.py
@@ -11,3 +11,34 @@ QtCore.QSortFilterProxyModel.__bases__ = (core.AbstractProxyModel,)
 class SortFilterProxyModel(QtCore.QSortFilterProxyModel):
 
     HEADER: List[str] = []
+
+    # def set_filter_case_sensitivity(self, sensitivity: constants.CaseSensitivityStr):
+    #     """Set the filter case sensitivity.
+
+    #     Args:
+    #         sensitivity: filter case sensitivity
+
+    #     Raises:
+    #         InvalidParamError: filter case sensitivity does not exist
+    #     """
+    #     if sensitivity not in constants.CASE_SENSITIVITY:
+    #         raise InvalidParamError(sensitivity, constants.CASE_SENSITIVITY)
+    #     self.setFilterCaseSensitivity(constants.CASE_SENSITIVITY[sensitivity])
+
+    # def get_filter_case_sensitivity(self) -> constants.CaseSensitivityStr:
+    #     """Return current filter case sensitivity.
+
+    #     Returns:
+    #         filter case sensitivity
+    #     """
+    #     return constants.CASE_SENSITIVITY.inverse[self.filterCaseSensitivity()]
+
+    def set_filter_case_sensitive(self, state: bool):
+        if state:
+            sensitivity = QtCore.Qt.CaseSensitive
+        else:
+            sensitivity = QtCore.Qt.CaseInsensitive
+        self.setFilterCaseSensitivity(sensitivity)
+
+    def is_filter_case_sensitive(self) -> bool:
+        return self.filterCaseSensitivity() == QtCore.Qt.CaseSensitive
