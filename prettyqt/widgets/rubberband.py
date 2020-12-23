9c93523cbbfea46506d8d233b341bbab58151ecd
phil65 <philipptemminghoff@googlemail.com>
2020-12-23 14:20:11 +0100

    chore: Literal

--- prettyqt/widgets/rubberband.py
+++ prettyqt/widgets/rubberband.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Literal, Optional, Union
 
 from qtpy import QtWidgets
 
@@ -8,6 +8,8 @@ from prettyqt.utils import bidict
 
 SHAPE = bidict(line=QtWidgets.QRubberBand.Line, rectangle=QtWidgets.QRubberBand.Rectangle)
 
+ShapeStr = Literal["line", "rectangle"]
+
 
 QtWidgets.QRubberBand.__bases__ = (widgets.Widget,)
 
@@ -15,11 +17,11 @@ QtWidgets.QRubberBand.__bases__ = (widgets.Widget,)
 class RubberBand(QtWidgets.QRubberBand):
     def __init__(
         self,
-        shape: Union[str, QtWidgets.QRubberBand.Shape],
+        shape: Union[ShapeStr, QtWidgets.QRubberBand.Shape],
         parent: Optional[QtWidgets.QWidget] = None,
     ):
         shape = SHAPE[shape] if isinstance(shape, str) else shape
         super().__init__(shape, parent)
 
-    def get_shape(self) -> str:
+    def get_shape(self) -> ShapeStr:
         return SHAPE.inverse[self.shape()]
