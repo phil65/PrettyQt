17d6bdbd4b7abaeb9073edca0ee54070fa19a87e
phil65 <philipptemminghoff@googlemail.com>
2020-12-28 20:46:48 +0100

    chore: serialize

--- prettyqt/widgets/buttongroup.py
+++ prettyqt/widgets/buttongroup.py
@@ -2,5 +2,8 @@ from prettyqt.qt import QtWidgets
 
 
 class ButtonGroup(QtWidgets.QButtonGroup):
+    def serialize_fields(self):
+        return dict(exclusive=self.exclusive())
+
     def __getitem__(self, index: int) -> QtWidgets.QAbstractButton:
         return self.button(index)
--- prettyqt/widgets/mdisubwindow.py
+++ prettyqt/widgets/mdisubwindow.py
@@ -1,12 +1,23 @@
 from prettyqt import widgets
 from prettyqt.qt import QtWidgets
+from prettyqt.utils import bidict
 
 
 QtWidgets.QMdiSubWindow.__bases__ = (widgets.Widget,)
 
 
+SUB_WINDOW_OPTION = bidict(
+    rubber_band_resize=QtWidgets.QMdiSubWindow.RubberBandResize,
+    rubber_band_move=QtWidgets.QMdiSubWindow.RubberBandMove,
+)
+
+
 class MdiSubWindow(QtWidgets.QMdiSubWindow):
-    pass
+    def serialize_fields(self):
+        return dict(
+            keyboard_single_step=self.keyboardSingleStep(),
+            keyboard_page_step=self.keyboardPageStep(),
+        )
 
 
 if __name__ == "__main__":
