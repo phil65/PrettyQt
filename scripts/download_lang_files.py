68aac9cfedbf2bd44af54ade012f0258ba6e1c4b
phil65 <philipptemminghoff@googlemail.com>
2020-12-30 15:58:08 +0100

    build: add script to download lang files

---
+++ scripts/download_lang_files.py
@@ -0,0 +1,24 @@
+import pathlib
+
+from bs4 import BeautifulSoup
+import requests
+
+
+BASE_URL = "http://l10n-files.qt.io/l10n-files/"
+
+r = requests.get(BASE_URL)
+data = r.text
+soup = BeautifulSoup(data)
+target_dir = pathlib.Path(__file__).parent.parent / "prettyqt" / "localization"
+
+for link in soup.find_all("a"):
+    url = link.get("href")
+    if url.startswith("qt5-stable"):
+        response = requests.get(f"{BASE_URL}/{url}")
+        response.encoding = "utf-8"
+        filename = url.rsplit("/", 1)[1]
+        file = target_dir / filename
+        with file.open("w", encoding="utf-8") as f:
+            f.write(response.text)
+        # print(response.text)
+        # break
