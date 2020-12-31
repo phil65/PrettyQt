175747a155cdc971c612646a8e882a68b921cdec
phil65 <philipptemminghoff@googlemail.com>
2020-12-31 16:12:13 +0100

    build: language script update

--- scripts/download_lang_files.py
+++ scripts/download_lang_files.py
@@ -13,10 +13,10 @@ target_dir = pathlib.Path(__file__).parent.parent / "prettyqt" / "localization"
 
 for link in soup.find_all("a"):
     url = link.get("href")
-    if url.startswith("qt5-stable"):
+    if url.startswith("qt5-stable") and "untranslated" not in url:
         response = requests.get(f"{BASE_URL}/{url}")
         response.encoding = "utf-8"
-        filename = url.rsplit("/", 1)[1]
+        filename = url.rsplit("/", 1)[1].replace("qt_help", "qthelp")
         file = target_dir / filename
         with file.open("w", encoding="utf-8") as f:
             f.write(response.text)
