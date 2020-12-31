762e9700bcfd06bed46ccd2ab2eb6c888de0e8ca
phil65 <philipptemminghoff@googlemail.com>
2020-12-31 02:58:39 +0100

    chore: add some command cmds to QtUiTools

--- prettyqt/qt/QtUiTools/__init__.py
+++ prettyqt/qt/QtUiTools/__init__.py
@@ -10,10 +10,20 @@ if PYQT5:
         def load(self, path: str):
             return uic.loadUi(path)
 
-
+    RCC_CMD = "pyrcc5 -no-compress"
+    LUPDATE_CMD = "pylupdate5 -verbose"
+    UIC_CMD = "pyuic5 --debug"
 elif PYSIDE2:
-    from PySide2.QtUiTools import QUiLoader
+    from PySide2.QtUiTools import QUiLoader, loadUiType
+
+    RCC_CMD = "pyside2-rcc --no-compress --verbose"
+    LUPDATE_CMD = "pyside2-lupdate -verbose"
+    UIC_CMD = "pyside2-uic"
 elif PYSIDE6:
-    from PySide6.QtUiTools import QUiLoader  # type: ignore
+    from PySide6.QtUiTools import QUiLoader, loadUiType  # type: ignore
+
+    RCC_CMD = "pyside6-rcc --no-compress --verbose"
+    LUPDATE_CMD = "pyside6-lupdate -verbose"
+    UIC_CMD = "pyside6-uic"
 else:
     raise PythonQtError("No Qt bindings could be found")
