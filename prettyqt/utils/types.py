f9c85fa24e6d8afa88fc39106b768c39c8320991
phil65 <philipptemminghoff@googlemail.com>
2021-01-05 00:59:43 +0100

    feat(types): add Variant(Type)

--- prettyqt/utils/types.py
+++ prettyqt/utils/types.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import Union
+from typing import Dict, List, Union
 
 from prettyqt.qt import QtCore, QtGui
 
@@ -8,3 +8,57 @@ IconType = Union[QtGui.QIcon, str, pathlib.Path, None]
 
 ColorType = Union[str, int, QtCore.Qt.GlobalColor, QtGui.QColor, tuple, None]
 ColorAndBrushType = Union[ColorType, QtGui.QBrush]
+
+VariantType = Union[
+    QtCore.QPersistentModelIndex,
+    QtCore.QModelIndex,
+    QtCore.QJsonDocument,
+    # QtCore.QJsonArray,
+    # QtCore.QJsonObject,
+    QtCore.QJsonValue,
+    QtCore.QUrl,
+    QtCore.QUuid,
+    QtCore.QEasingCurve,
+    QtCore.QRegularExpression,
+    QtCore.QLocale,
+    QtCore.QRectF,
+    QtCore.QRect,
+    QtCore.QLineF,
+    QtCore.QLine,
+    QtCore.QPointF,
+    QtCore.QPoint,
+    QtCore.QSizeF,
+    QtCore.QSize,
+    QtCore.QDateTime,
+    QtCore.QTime,
+    QtCore.QDate,
+    QtCore.QByteArray,
+    QtGui.QColor,
+    QtGui.QImage,
+    QtGui.QBrush,
+    QtGui.QBitmap,
+    QtGui.QCursor,
+    QtGui.QFont,
+    QtGui.QKeySequence,
+    QtGui.QIcon,
+    QtGui.QTransform,
+    QtGui.QPalette,
+    QtGui.QMatrix4x4,
+    QtGui.QPen,
+    QtGui.QPixmap,
+    QtGui.QPolygon,
+    QtGui.QPolygonF,
+    QtGui.QRegion,
+    QtGui.QTextFormat,
+    QtGui.QTextLength,
+    QtGui.QVector2D,
+    QtGui.QVector3D,
+    QtGui.QVector4D,
+    bool,
+    str,
+    int,
+    float,
+    bytes,
+]
+
+Variant = Union[VariantType, List[VariantType], Dict[str, VariantType]]
