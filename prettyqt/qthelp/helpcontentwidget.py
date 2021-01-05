fd3b8cb6ed3c4cd59705a36e3cd83abd2960ab55
phil65 <philipptemminghoff@googlemail.com>
2021-01-05 04:55:05 +0100

    feat(HelpContentWidget): add index_of method

--- prettyqt/qthelp/helpcontentwidget.py
+++ prettyqt/qthelp/helpcontentwidget.py
@@ -1,9 +1,17 @@
+from typing import Optional, Union
+
 from prettyqt import widgets
-from prettyqt.qt import QtHelp
+from prettyqt.qt import QtCore, QtHelp
 
 
 QtHelp.QHelpContentWidget.__bases__ = (widgets.TreeView,)
 
 
 class HelpContentWidget(QtHelp.QHelpContentWidget):
-    pass
+    def index_of(self, url: Union[QtCore.QUrl, str]) -> Optional[QtCore.QModelIndex]:
+        if isinstance(url, str):
+            url = QtCore.QUrl(url)
+        idx = self.indexOf(url)
+        if not idx.isValid():
+            return None
+        return idx
