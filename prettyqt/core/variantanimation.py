26eb0f13f1d51d4fa94be96239ae12df220277e7
phil65 <philipptemminghoff@googlemail.com>
2021-01-06 15:32:38 +0100

    chore: some stuff for VariantAnimation

--- prettyqt/core/variantanimation.py
+++ prettyqt/core/variantanimation.py
@@ -1,20 +1,31 @@
 from __future__ import annotations
 
-from typing import Callable, Union
+from typing import Callable, Iterator, Tuple, Union
 
 from prettyqt import core
 from prettyqt.qt import QtCore
+from prettyqt.utils import types
 
 
 QtCore.QVariantAnimation.__bases__ = (core.AbstractAnimation,)
 
 
 class VariantAnimation(QtCore.QVariantAnimation):
-    def __getitem__(self, value: float):
+    def __getitem__(self, value: float) -> types.Variant:
         return self.keyValueAt(value)
 
-    def __setitem__(self, key: float, value):
+    def __setitem__(self, key: float, value: types.Variant):
         self.setKeyValueAt(key, value)
+
+    def __iter__(self) -> Iterator[Tuple[float, types.Variant]]:
+        return iter(self.keyValues())
+
+    def serialize_fields(self):
+        return dict(
+            duration=self.duration(),
+            easing_curve=self.get_easing(),
+            key_values=self.keyValues(),
+        )
 
     def set_easing(
         self, easing_type: Union[core.easingcurve.TypeStr, Callable[[float], float]]
