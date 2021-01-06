d590a997d024cc33d105d708bf5f77993b62e441
phil65 <philipptemminghoff@googlemail.com>
2021-01-06 22:52:17 +0100

    feat(LayoutItem): add get/set_alignment methods

--- prettyqt/widgets/layoutitem.py
+++ prettyqt/widgets/layoutitem.py
@@ -1,7 +1,31 @@
 from __future__ import annotations
 
+from prettyqt import constants
 from prettyqt.qt import QtWidgets
+from prettyqt.utils import InvalidParamError
 
 
 class LayoutItem(QtWidgets.QLayoutItem):
-    pass
+    # def __bool__(self):
+    #     return not self.isEmpty()
+
+    def set_alignment(self, alignment: constants.AlignmentStr):
+        """Set the alignment of the layout.
+
+        Args:
+            alignment: alignment for the layout
+
+        Raises:
+            InvalidParamError: alignment does not exist
+        """
+        if alignment not in constants.ALIGNMENTS:
+            raise InvalidParamError(alignment, constants.ALIGNMENTS)
+        self.setAlignment(constants.ALIGNMENTS[alignment])
+
+    def get_alignment(self) -> constants.AlignmentStr:
+        """Return current alignment.
+
+        Returns:
+            alignment
+        """
+        return constants.ALIGNMENTS.inverse[self.alignment()]
