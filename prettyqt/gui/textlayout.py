4c0c10d31eb7c8829ae88e23ec11709f57ceaa98
phil65 <philipptemminghoff@googlemail.com>
2021-01-10 08:56:30 +0100

    chore: TextLayout

--- prettyqt/gui/textlayout.py
+++ prettyqt/gui/textlayout.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
+import contextlib
+
 from prettyqt.qt import QtGui
 
 
 class TextLayout(QtGui.QTextLayout):
     def __repr__(self):
         return f"{type(self).__name__}({self.text()!r})"
+
+    @contextlib.contextmanager
+    def process_layout(self):
+        self.beginLayout()
+        yield self
+        self.endLayout()
 
 
 if __name__ == "__main__":
