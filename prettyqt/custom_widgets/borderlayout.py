a5039f9ba7a5ff5c55f7c9b75035bc7ed581f4f1
phil65 <philipptemminghoff@googlemail.com>
2021-01-18 17:23:06 +0100

    chore: borderlayout cleanup

--- prettyqt/custom_widgets/borderlayout.py
+++ prettyqt/custom_widgets/borderlayout.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
-from typing import List, Optional
+import enum
+from typing import List, Literal, Optional
 
 from prettyqt import constants, core, widgets
 from prettyqt.qt import QtCore, QtWidgets
@@ -8,12 +11,20 @@ from prettyqt.qt import QtCore, QtWidgets
 @dataclass
 class ItemWrapper:
     item: QtWidgets.QWidgetItem
-    position: int
+    position: BorderLayout.Position
 
 
 class BorderLayout(widgets.Layout):
-    West, North, South, East, Center = range(5)
-    MinimumSize, SizeHint = range(2)
+    class Position(enum.IntEnum):
+        """Item position."""
+
+        West = 0
+        North = 1
+        South = 2
+        East = 3
+        Center = 4
+
+    QtCore.QEnum(Position)
 
     def __init__(
         self,
@@ -32,9 +43,13 @@ class BorderLayout(widgets.Layout):
             item = self.takeAt(0)
 
     def addItem(self, item: QtWidgets.QWidgetItem):
-        self.add_widgetitem(item, BorderLayout.West)
+        self.add_widgetitem(item, BorderLayout.Position.West)
 
-    def addWidget(self, widget: QtWidgets.QWidget, position: int = 0):
+    def addWidget(
+        self,
+        widget: QtWidgets.QWidget,
+        position: Position = Position.West,
+    ):
         self.add_widgetitem(widgets.WidgetItem(widget), position)
 
     def expandingDirections(self):
@@ -52,7 +67,7 @@ class BorderLayout(widgets.Layout):
         return None
 
     def minimumSize(self):
-        return self.calculate_size(BorderLayout.MinimumSize)
+        return self.calculate_size("minimum")
 
     def setGeometry(self, rect: QtCore.QRect):
         center = None
@@ -67,7 +82,7 @@ class BorderLayout(widgets.Layout):
             item = wrapper.item
             position = wrapper.position
 
-            if position == BorderLayout.North:
+            if position == BorderLayout.Position.North:
                 geom = core.Rect(
                     rect.x(), north_height, rect.width(), item.sizeHint().height()
                 )
@@ -75,7 +90,7 @@ class BorderLayout(widgets.Layout):
 
                 north_height += item.geometry().height() + self.spacing()
 
-            elif position == BorderLayout.South:
+            elif position == BorderLayout.Position.South:
                 geom = core.Rect(
                     item.geometry().x(),
                     item.geometry().y(),
@@ -93,7 +108,7 @@ class BorderLayout(widgets.Layout):
                 )
                 item.setGeometry(geom)
 
-            elif position == BorderLayout.Center:
+            elif position == BorderLayout.Position.Center:
                 center = wrapper
 
         center_height = rect.height() - north_height - south_height
@@ -102,7 +117,7 @@ class BorderLayout(widgets.Layout):
             item = wrapper.item
             position = wrapper.position
 
-            if position == BorderLayout.West:
+            if position == BorderLayout.Position.West:
                 geom = core.Rect(
                     rect.x() + west_width,
                     north_height,
@@ -113,7 +128,7 @@ class BorderLayout(widgets.Layout):
 
                 west_width += item.geometry().width() + self.spacing()
 
-            elif position == BorderLayout.East:
+            elif position == BorderLayout.Position.East:
                 geom = core.Rect(
                     item.geometry().x(),
                     item.geometry().y(),
@@ -142,7 +157,7 @@ class BorderLayout(widgets.Layout):
             center.item.setGeometry(rect)
 
     def sizeHint(self) -> core.Size:
-        return self.calculate_size(BorderLayout.SizeHint)
+        return self.calculate_size("size_hint")
 
     def takeAt(self, index: int) -> Optional[QtWidgets.QWidgetItem]:  # type: ignore
         if 0 <= index < len(self.items):
@@ -151,73 +166,78 @@ class BorderLayout(widgets.Layout):
 
         return None
 
-    def add_widgetitem(self, item: QtWidgets.QWidgetItem, position: int):
+    def add_widgetitem(self, item: QtWidgets.QWidgetItem, position: Position):
         self.items.append(ItemWrapper(item, position))
 
-    def calculate_size(self, size_type: int) -> core.Size:
+    def calculate_size(self, size_type: Literal["minimum", "size_hint"]) -> core.Size:
         total_size = core.Size()
 
         for wrapper in self.items:
             position = wrapper.position
-            if size_type == BorderLayout.MinimumSize:
+            if size_type == "minimum":
                 item_size = wrapper.item.minimumSize()
-            else:  # size_type == BorderLayout.SizeHint
+            else:  # size_type == "size_hint"
                 item_size = wrapper.item.sizeHint()
 
-            if position in (BorderLayout.North, BorderLayout.South, BorderLayout.Center):
+            if position in (
+                BorderLayout.Position.North,
+                BorderLayout.Position.South,
+                BorderLayout.Position.Center,
+            ):
                 total_size.setHeight(total_size.height() + item_size.height())
 
-            if position in (BorderLayout.West, BorderLayout.East, BorderLayout.Center):
+            if position in (
+                BorderLayout.Position.West,
+                BorderLayout.Position.East,
+                BorderLayout.Position.Center,
+            ):
                 total_size.setWidth(total_size.width() + item_size.width())
 
         return total_size
 
 
-class Window(widgets.Widget):
-    def __init__(self):
-        super().__init__()
-
-        central_widget = widgets.TextBrowser()
-        central_widget.setPlainText("Central widget")
-
-        layout = BorderLayout()
-        layout.addWidget(central_widget, BorderLayout.Center)
-
-        # Because BorderLayout doesn't call its super-class addWidget() it
-        # doesn't take ownership of the widgets until setLayout() is called.
-        # Therefore we keep a local reference to each label to prevent it being
-        # garbage collected too soon.
-        label_n = self.create_label("North")
-        layout.addWidget(label_n, BorderLayout.North)
-
-        label_w = self.create_label("West")
-        layout.addWidget(label_w, BorderLayout.West)
-
-        label_e1 = self.create_label("East 1")
-        layout.addWidget(label_e1, BorderLayout.East)
-
-        label_e2 = self.create_label("East 2")
-        layout.addWidget(label_e2, BorderLayout.East)
-
-        label_s = self.create_label("South")
-        layout.addWidget(label_s, BorderLayout.South)
-
-        self.setLayout(layout)
-
-        self.setWindowTitle("Border Layout")
-
-    @staticmethod
-    def create_label(text: str):
-        label = widgets.Label(text)
-        label.setFrameStyle(widgets.Frame.Box | widgets.Frame.Raised)
-        return label
-
-
 if __name__ == "__main__":
 
-    import sys
+    class Window(widgets.Widget):
+        def __init__(self):
+            super().__init__()
+
+            central_widget = widgets.TextBrowser()
+            central_widget.setPlainText("Central widget")
+
+            layout = BorderLayout()
+            layout.addWidget(central_widget, BorderLayout.Position.Center)
+
+            # Because BorderLayout doesn't call its super-class addWidget() it
+            # doesn't take ownership of the widgets until setLayout() is called.
+            # Therefore we keep a local reference to each label to prevent it being
+            # garbage collected too soon.
+            label_n = self.create_label("North")
+            layout.addWidget(label_n, BorderLayout.Position.North)
+
+            label_w = self.create_label("West")
+            layout.addWidget(label_w, BorderLayout.Position.West)
+
+            label_e1 = self.create_label("East 1")
+            layout.addWidget(label_e1, BorderLayout.Position.East)
+
+            label_e2 = self.create_label("East 2")
+            layout.addWidget(label_e2, BorderLayout.Position.East)
+
+            label_s = self.create_label("South")
+            layout.addWidget(label_s, BorderLayout.Position.South)
+
+            self.setLayout(layout)
+
+            self.setWindowTitle("Border Layout")
+
+        @staticmethod
+        def create_label(text: str):
+            label = widgets.Label(text)
+            label.setFrameStyle(widgets.Frame.Box | widgets.Frame.Raised)
+            return label
 
     app = widgets.app()
     window = Window()
     window.show()
-    sys.exit(app.main_loop())
+    app.main_loop()
