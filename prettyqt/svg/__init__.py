93f990d7c6de9456599fd71b1df008bd8f8afac1
phil65 <philipptemminghoff@googlemail.com>
2021-01-25 05:06:54 +0100

    feat(svg): add SvgRenderer class

--- prettyqt/svg/__init__.py
+++ prettyqt/svg/__init__.py
@@ -5,6 +5,7 @@ contains QtSvg-based classes
 
 from .graphicssvgitem import GraphicsSvgItem
 from .svggenerator import SvgGenerator
+from .svgrenderer import SvgRenderer
 
 
-__all__ = ["GraphicsSvgItem", "SvgGenerator"]
+__all__ = ["GraphicsSvgItem", "SvgGenerator", "SvgRenderer"]
---
+++ prettyqt/svg/svgrenderer.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import os
+from typing import Union
+
+from prettyqt import constants, core
+from prettyqt.qt import QtSvg
+from prettyqt.utils import InvalidParamError
+
+
+QtSvg.QSvgRenderer.__bases__ = (core.Object,)
+
+
+class SvgRenderer(QtSvg.QSvgRenderer):
+    def load_file(self, path: Union[str, os.PathLike]):
+        result = self.load(os.fspath(path))
+        if not result:
+            raise ValueError("invalid path")
+
+    def set_aspect_ratio_mode(self, mode: constants.AspectRatioModeStr):
+        """Set the aspect ratio mode.
+
+        Args:
+            mode: aspect ratio mode
+
+        Raises:
+            InvalidParamError: aspect ratio mode does not exist
+        """
+        if mode not in constants.ASPECT_RATIO_MODE:
+            raise InvalidParamError(mode, constants.ASPECT_RATIO_MODE)
+        self.setAspectRatioMode(constants.ASPECT_RATIO_MODE[mode])
+
+    def get_aspect_ratio_mode(self) -> constants.AspectRatioModeStr:
+        """Return current aspect ratio mode.
+
+        Returns:
+            aspect ratio mode
+        """
+        return constants.ASPECT_RATIO_MODE.inverse[self.aspectRatioMode()]
