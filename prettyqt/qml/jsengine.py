84f5030996ad8d3990e0548dfefb43fe4cf3a071
phil65 <philipptemminghoff@googlemail.com>
2021-02-05 11:13:17 +0100

    feat(JSEngine): add eval method

--- prettyqt/qml/jsengine.py
+++ prettyqt/qml/jsengine.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import Optional
-
 from prettyqt import core, qml
 from prettyqt.qt import QtQml
 from prettyqt.utils import bidict
@@ -21,21 +19,22 @@ class JSEngine(QtQml.QJSEngine):
     def serialize_fields(self):
         return dict(ui_language=self.uiLanguage())
 
-    def install_extensions(self, extension: str, obj: Optional[QtQml.QJSValue] = None):
+    def install_extensions(self, extension: str, obj: QtQml.QJSValue | None = None):
         if obj is None:
             obj = QtQml.QJSValue()
         self.installExtensions(EXTENSIONS[extension], obj)
 
-    def new_array(self, length=0) -> qml.JSValue:
+    def new_array(self, length: int = 0) -> qml.JSValue:
         return qml.JSValue(self.newArray(length))
+
+    def eval(self, program: str):
+        result = self.evaluate(program)
+        return qml.JSValue(result)
 
 
 if __name__ == "__main__":
     app = core.CoreApplication([])
-    val = JSEngine()
-    arr = val.new_array()
-    arr["test"] = 1
-    arr["tes2t"] = 1
-    assert arr["test"] == 1
-    for name, val in arr:
-        print(name, val)
+    engine = JSEngine()
+    fun = engine.eval("(function(a, b) { return a + b; })")
+    result = fun(1, 2)
+    print(result)
