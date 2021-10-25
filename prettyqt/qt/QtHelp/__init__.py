f1522fc7db6a8e5a8fce2cd0791cda5cf44c8123
phil65 <philipptemminghoff@googlemail.com>
2021-10-25 23:59:43 +0200

    chore: Qt6 compat

--- prettyqt/qt/QtHelp/__init__.py
+++ prettyqt/qt/QtHelp/__init__.py
@@ -1,11 +1,15 @@
 """Provides QtHelp classes and functions."""
 
-from prettyqt.qt import PYQT5, PYSIDE2, PythonQtError
+from prettyqt.qt import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError
 
 
 if PYQT5:
     from PyQt5.QtHelp import *
+elif PYQT6:
+    from PyQt6.QtHelp import *
 elif PYSIDE2:
     from PySide2.QtHelp import *
+elif PYSIDE6:
+    from PySide6.QtHelp import *
 else:
     raise PythonQtError("No Qt bindings could be found")
--- prettyqt/svg/__init__.py
+++ prettyqt/svg/__init__.py
@@ -3,10 +3,14 @@
 contains QtSvg-based classes
 """
 
-from .graphicssvgitem import GraphicsSvgItem
+from prettyqt import core
+
 from .svggenerator import SvgGenerator
 from .svgrenderer import SvgRenderer
-from .svgwidget import SvgWidget
+
+if core.VersionNumber.get_qt_version() < (6, 0, 0):
+    from .graphicssvgitem import GraphicsSvgItem
+    from .svgwidget import SvgWidget
 
 
 __all__ = ["GraphicsSvgItem", "SvgGenerator", "SvgRenderer", "SvgWidget"]
