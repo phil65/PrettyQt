08ffabdf97d1e76e82e2d01676920fe1d121a32a
phil65 <philipptemminghoff@googlemail.com>
2021-10-25 22:44:31 +0200

    chore: update lang download script

--- scripts/download_lang_files.py
+++ scripts/download_lang_files.py
@@ -10,13 +10,13 @@ BASE_URL = "http://l10n-files.qt.io/l10n-files/"
 
 r = requests.get(BASE_URL)
 data = r.text
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, features="lxml")
 TARGET_DIR = pathlib.Path(__file__).parent.parent / "prettyqt" / "localization"
 
 langs = collections.defaultdict(list)
 for link in soup.find_all("a"):
     url = link.get("href")
-    if url.startswith("qt5-stable") and "untranslated" not in url:
+    if url.startswith("qt-old515") and "untranslated" not in url:
         # merging seems to create an invalid qm file, so just qtbase for now
         if "qtbase" not in url:
             continue
