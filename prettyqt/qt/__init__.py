96242287ebce22d7008db03d1372aa375fe7e448
phil65 <philipptemminghoff@googlemail.com>
2021-10-30 04:31:16 +0200

    feat: fallback to installed qt package in case environment var QT_API is invalid / empty

--- prettyqt/qt/__init__.py
+++ prettyqt/qt/__init__.py
@@ -1,15 +1,45 @@
 """Provides Qt init stuff."""
 
 import os
+import importlib
 
 
 class PythonQtError(ImportError):
     pass
 
 
-os.environ.setdefault("QT_API", "pyqt5")
+# the order in the tuple represents the search order when importing the package
+packages = {
+    "pyside6": "PySide6",
+    "pyqt6": "PyQt6",
+    "pyside2": "PySide2",
+    "pyqt5": "PyQt5",
+}
 
-API = os.environ["QT_API"].lower()
+qt_api = os.getenv("QT_API")
+
+# one can create a QT_API environment variable to select which Qt API to use
+if qt_api:
+    qt_api = qt_api.lower()
+    if qt_api not in packages.keys():
+        raise ValueError(f"Invalid QT_API environment variable {qt_api!r}")
+else:
+    for name, pkg_name in packages.items():
+        try:
+            importlib.import_module(pkg_name)
+        except ImportError:
+            continue
+        else:
+            qt_api = name
+            break
+
+if not qt_api:
+    pkg_str = ", ".join(packages)
+    raise ImportError(f"One of the following Qt packages must be installed: {pkg_str}")
+
+# os.environ.setdefault("QT_API", "pyqt6")
+
+API = qt_api
 
 PYQT5 = API == "pyqt5"
 PYQT6 = API == "pyqt6"
