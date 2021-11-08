b7cc00338d38c1cdd831c54120ab690fec0f3245
phil65 <philipptemminghoff@googlemail.com>
2021-11-08 23:38:49 +0100

    feat(qml): add clear_type_registrations method

--- prettyqt/qml/__init__.py
+++ prettyqt/qml/__init__.py
@@ -3,7 +3,10 @@
 Contains QtQml-based classes
 """
 
-from prettyqt.qt.QtQml import qmlRegisterType as register_qml_type
+from prettyqt.qt.QtQml import (
+    qmlRegisterType as register_qml_type,
+    qmlClearTypeRegistrations as clear_type_registrations,
+)
 from .qmlparserstatus import QmlParserStatus
 from .jsvalue import JSValue
 from .jsvalueiterator import JSValueIterator
@@ -15,6 +18,7 @@ from .qmlimageproviderbase import QmlImageProviderBase
 
 __all__ = [
     "register_qml_type",
+    "clear_type_registrations",
     "QmlParserStatus",
     "JSValue",
     "JSValueIterator",
