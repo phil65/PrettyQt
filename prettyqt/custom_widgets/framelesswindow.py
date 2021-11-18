51cc8de1d2d374fb00bebcd6ae7b3cc7147be2b5
phil65 <philipptemminghoff@googlemail.com>
2021-11-18 03:35:09 +0100

    chore: rework FramelessWindow

--- prettyqt/custom_widgets/framelesswindow.py
+++ prettyqt/custom_widgets/framelesswindow.py
@@ -2,138 +2,144 @@ from __future__ import annotations
 
 from typing import Literal
 
-from prettyqt import core, widgets
-from prettyqt.qt import QtCore, QtWidgets
+from prettyqt import widgets
+from prettyqt.qt import QtCore, QtGui, QtWidgets
 
 
 SideStr = Literal["left", "top", "right", "bottom"]
 
 
-class SideGrip(widgets.Widget):
-    def __init__(
-        self,
-        parent: QtWidgets.QWidget | None,
-        edge: SideStr,
-    ):
-        super().__init__(parent=parent)
-        if edge == "left":
-            self.set_cursor("size_horizontal")
-            self.resize_fn = self.resize_left
-        elif edge == "top":
-            self.set_cursor("size_vertical")
-            self.resize_fn = self.resize_top
-        elif edge == "right":
-            self.set_cursor("size_horizontal")
-            self.resize_fn = self.resize_right
-        else:
-            self.set_cursor("size_vertical")
-            self.resize_fn = self.resize_bottom
-        self.mouse_pos = None
-
-    def resize_left(self, delta: QtCore.QPoint):
-        window = self.window()
-        width = max(window.minimumWidth(), window.width() - delta.x())
-        geo = window.geometry()
-        geo.setLeft(geo.right() - width)
-        window.setGeometry(geo)
-
-    def resize_top(self, delta: QtCore.QPoint):
-        window = self.window()
-        height = max(window.minimumHeight(), window.height() - delta.y())
-        geo = window.geometry()
-        geo.setTop(geo.bottom() - height)
-        window.setGeometry(geo)
-
-    def resize_right(self, delta: QtCore.QPoint):
-        window = self.window()
-        width = max(window.minimumWidth(), window.width() + delta.x())
-        window.resize(width, window.height())
-
-    def resize_bottom(self, delta: QtCore.QPoint):
-        window = self.window()
-        height = max(window.minimumHeight(), window.height() + delta.y())
-        window.resize(window.width(), height)
-
-    def mousePressEvent(self, event):
-        if event.button() == QtCore.Qt.MouseButton.LeftButton:
-            self.mouse_pos = event.position()
-
-    def mouseMoveEvent(self, event):
-        if self.mouse_pos is not None:
-            delta = event.position() - self.mouse_pos
-            self.resize_fn(delta)
-
-    def mouseReleaseEvent(self, event):
-        self.mouse_pos = None
+# Aero snap still doesn't work https://bugreports.qt.io/browse/QTBUG-84466
 
 
-class FramelessWindow(widgets.MainWindow):
-    _grip_size = 4
+class TitleBarIcon(widgets.PushButton):
+    def __init__(self, text, parent=None):
+        super().__init__(text, parent)
+
+        self.setStyleSheet(
+            "margin: 0;" "padding: 0px;" "font-size: 16px;" "width: 44px;" "height: 30px;"
+        )
+
+        self.set_margin(0)
+
+
+class CustomTitleBar(widgets.Frame):
+    def __init__(self, window_widget: QtWidgets.QWidget):
+        super().__init__(window_widget)
+
+        self.window_widget = window_widget
+        self.minimize_button = TitleBarIcon("🗕")
+        self.maximize_button = TitleBarIcon("🗖")
+        self.exit_button = TitleBarIcon("✕")
+
+        self.minimize_button.clicked.connect(
+            lambda: window_widget.setWindowState(QtCore.Qt.WindowMinimized)
+        )
+        self.maximize_button.clicked.connect(
+            lambda: (
+                window_widget.showNormal()
+                if window_widget.isMaximized()
+                else window_widget.showMaximized()
+            )
+        )
+        self.exit_button.clicked.connect(window_widget.close)
+
+        self.set_layout("horizontal")
+        spacer_item = QtWidgets.QSpacerItem(0, 0, QtWidgets.QSizePolicy.Expanding)
+        self.box.addSpacerItem(spacer_item)
+        for widget in [
+            self.minimize_button,
+            self.maximize_button,
+            self.exit_button,
+        ]:
+            self.box.addWidget(widget)
+
+        self.setStyleSheet("width: 100%;" "padding: 0;" "margin: 0;")
+        self.setContentsMargins(0, 0, 0, 0)
+        self.box.setContentsMargins(0, 0, 0, 0)
+        self.box.setSpacing(0)
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.window_widget.windowHandle().startSystemMove()
+
+
+class FramelessWindow(widgets.Widget):
+    gripSize = 6
 
     def __init__(self, parent: QtWidgets.QWidget | None = None):
-        super().__init__(parent=parent)
-        self.set_flags(frameless=True)
-        sides: list[SideStr] = ["left", "top", "right", "bottom"]
-        self.side_grips = [SideGrip(self, side) for side in sides]
-        # corner grips should be "on top" of everything, otherwise the side grips
-        # will take precedence on mouse events, so we are adding them *after*;
-        # alternatively, widget.raise_() can be used
-        self.corner_grips = [widgets.SizeGrip(self) for i in range(4)]
-        self.set_margin(self.grip_size)
+        super().__init__(parent)
 
-    @property
-    def grip_size(self) -> int:
-        return self._grip_size
+        # Remove window title bar and frame
+        self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.FramelessWindowHint)
 
-    def set_grip_size(self, size: int):
-        if size == self._grip_size:
-            return
-        self._grip_size = max(2, size)
-        self.set_margin(self.grip_size)
-        self.update_grips()
+        self.title_bar = CustomTitleBar(self)
+        self.main_widget = widgets.MainWindow()
 
-    def update_grips(self):
-        outer = self.rect()
-        # an "inner" rect used for reference to set the geometries of size grips
-        inner = outer.adjusted(
-            self.grip_size, self.grip_size, -self.grip_size, -self.grip_size
+        # Set up layout
+        self.main_layout = widgets.BoxLayout("vertical")
+        self.main_layout.addWidget(self.title_bar)
+        self.main_layout.addWidget(self.main_widget)
+
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(0)
+
+        self.grip_layout = widgets.GridLayout()
+
+        self.grip_layout.addLayout(self.main_layout, 1, 1)
+        self.grip_layout.addWidget(EdgeGrip(QtCore.Qt.TopEdge), 0, 1)
+        self.grip_layout.addWidget(EdgeGrip(QtCore.Qt.RightEdge), 1, 2)
+        self.grip_layout.addWidget(EdgeGrip(QtCore.Qt.BottomEdge), 2, 1)
+        self.grip_layout.addWidget(EdgeGrip(QtCore.Qt.LeftEdge), 1, 0)
+        self.grip_layout.addWidget(EdgeGrip(QtCore.Qt.TopEdge | QtCore.Qt.LeftEdge), 0, 0)
+        self.grip_layout.addWidget(
+            EdgeGrip(QtCore.Qt.TopEdge | QtCore.Qt.RightEdge), 0, 2
+        )
+        self.grip_layout.addWidget(
+            EdgeGrip(QtCore.Qt.BottomEdge | QtCore.Qt.LeftEdge), 2, 0
+        )
+        self.grip_layout.addWidget(
+            EdgeGrip(QtCore.Qt.BottomEdge | QtCore.Qt.RightEdge), 2, 2
         )
 
-        # top left
-        rect = core.Rect(outer.topLeft(), inner.topLeft())
-        self.corner_grips[0].setGeometry(rect)
-        # top right
-        rect = core.Rect(outer.topRight(), inner.topRight()).normalized()
-        self.corner_grips[1].setGeometry(rect)
-        # bottom right
-        rect = core.Rect(inner.bottomRight(), outer.bottomRight())
-        self.corner_grips[2].setGeometry(rect)
-        # bottom left
-        rect = core.Rect(outer.bottomLeft(), inner.bottomLeft()).normalized()
-        self.corner_grips[3].setGeometry(rect)
+        self.grip_layout.setContentsMargins(0, 0, 0, 0)
+        self.grip_layout.setSpacing(0)
+        self.setLayout(self.grip_layout)
 
-        # left edge
-        self.side_grips[0].setGeometry(0, inner.top(), self.grip_size, inner.height())
-        # top edge
-        self.side_grips[1].setGeometry(inner.left(), 0, inner.width(), self.grip_size)
-        # right edge
-        self.side_grips[2].setGeometry(
-            inner.right(),
-            inner.top(),
-            self.grip_size,
-            inner.height(),
-        )
-        # bottom edge
-        self.side_grips[3].setGeometry(
-            self.grip_size,
-            inner.bottom(),
-            inner.width(),
-            self.grip_size,
-        )
+    def __getattr__(self, attr: str):
+        return getattr(self.main_widget, attr)
 
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
-        self.update_grips()
+
+class EdgeGrip(widgets.Widget):
+    def __init__(self, edges: QtCore.Qt.Edges | QtCore.Qt.Edge, grip_size=6, parent=None):
+        super().__init__(parent)
+        self.edges = edges
+        self.grip_size = grip_size
+        # Sides
+        if edges == QtCore.Qt.TopEdge:
+            self.setCursor(QtCore.Qt.SizeVerCursor)
+            self.setFixedHeight(self.grip_size)
+        elif edges == QtCore.Qt.RightEdge:
+            self.setCursor(QtCore.Qt.SizeHorCursor)
+            self.setFixedWidth(self.grip_size)
+        elif edges == QtCore.Qt.BottomEdge:
+            self.setCursor(QtCore.Qt.SizeVerCursor)
+            self.setFixedHeight(self.grip_size)
+        elif edges == QtCore.Qt.LeftEdge:
+            self.setCursor(QtCore.Qt.SizeHorCursor)
+            self.setFixedWidth(self.grip_size)
+        # Corners
+        elif edges == QtCore.Qt.TopEdge | QtCore.Qt.LeftEdge:
+            self.setCursor(QtCore.Qt.SizeFDiagCursor)
+        elif edges == QtCore.Qt.TopEdge | QtCore.Qt.RightEdge:
+            self.setCursor(QtCore.Qt.SizeBDiagCursor)
+        elif edges == QtCore.Qt.BottomEdge | QtCore.Qt.LeftEdge:
+            self.setCursor(QtCore.Qt.SizeBDiagCursor)
+        elif edges == QtCore.Qt.BottomEdge | QtCore.Qt.RightEdge:
+            self.setCursor(QtCore.Qt.SizeFDiagCursor)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.parent().windowHandle().startSystemResize(self.edges)
 
 
 if __name__ == "__main__":
