fe247d48dbdbc39fffc2509247a8f5fa131e1b5a
phil65 <philipptemminghoff@googlemail.com>
2021-11-19 04:38:58 +0100

    feat(custom_validators): add IntegerValidator class

--- prettyqt/custom_validators/__init__.py
+++ prettyqt/custom_validators/__init__.py
@@ -8,12 +8,14 @@ from .compositevalidator import CompositeValidator
 from .notemptyvalidator import NotEmptyValidator
 from .notzerovalidator import NotZeroValidator
 from .pathvalidator import PathValidator
+from .integervalidator import IntegerValidator
 from .regexvalidators import FloatListValidator, IntListValidator
 from .regexpatternvalidator import RegexPatternValidator
 
 __all__ = [
     "PathValidator",
     "NotEmptyValidator",
+    "IntegerValidator",
     "IntListValidator",
     "FloatListValidator",
     "NotZeroValidator",
---
+++ prettyqt/custom_validators/integervalidator.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from prettyqt import gui
+
+
+class IntegerValidator(gui.Validator):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.range: tuple[int | None, int | None] = (None, None)
+
+    def set_range(self, lower: int | None, upper: int | None):
+        self.range = (lower, upper)
+
+    def validate(self, text: str, pos: int):
+        if not text.lstrip("-"):
+            return self.State.Intermediate, text, len(text)
+        if text.lstrip("-").isnumeric():
+            left = float("-inf") if self.range[0] is None else self.range[0]
+            right = float("inf") if self.range[1] is None else self.range[1]
+            if left <= int(text) <= right:
+                return self.State.Acceptable, text, len(text)
+        return self.State.Invalid, text, len(text)
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    val = IntegerValidator()
+    app = widgets.app()
+    widget = widgets.LineEdit("This is a test")
+    widget.setValidator(val)
+    widget.show()
+    app.main_loop()
