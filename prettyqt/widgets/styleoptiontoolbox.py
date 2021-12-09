c83e331693ef2a3516fef45fd40ec7adf2db2f14
phil65 <philipptemminghoff@googlemail.com>
2021-12-09 01:59:05 +0100

    feat(StyleOptionToolBox): add get/set_selected_position methods

--- prettyqt/widgets/styleoptiontoolbox.py
+++ prettyqt/widgets/styleoptiontoolbox.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Literal
+
 from prettyqt import widgets
 from prettyqt.qt import QtWidgets
 from prettyqt.utils import bidict
@@ -13,6 +15,8 @@ SELECTED_POSITION = bidict(
     previous_is_selected=mod.SelectedPosition.PreviousIsSelected,
 )
 
+SelectedPositionStr = Literal["not_adjacent", "next_is_selected", "previous_is_selected"]
+
 TAB_POSITION = bidict(
     beginning=mod.TabPosition.Beginning,
     middle=mod.TabPosition.Middle,
@@ -20,9 +24,20 @@ TAB_POSITION = bidict(
     only_one_tab=mod.TabPosition.OnlyOneTab,
 )
 
+TabPositionStr = Literal["beginning", "middle", "end" "only_one_tab"]
+
 
 QtWidgets.QStyleOptionToolBox.__bases__ = (widgets.StyleOption,)
 
 
 class StyleOptionToolBox(QtWidgets.QStyleOptionToolBox):
-    pass
+    def set_selected_position(self, position: SelectedPositionStr):
+        self.selectedPosition = SELECTED_POSITION[position]
+
+    def get_selected_position(self) -> SelectedPositionStr:
+        return SELECTED_POSITION.inv[self.selectedPosition]
+
+
+if __name__ == "__main__":
+    opt = StyleOptionToolBox()
+    print(opt.get_selected_position())
