8912af953514c6de41f191b281218f7f621ceb92
phil65 <philipptemminghoff@googlemail.com>
2021-12-31 17:14:31 +0100

    feat(Rect): add margins_added / margins_removed methods

--- prettyqt/core/rect.py
+++ prettyqt/core/rect.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from prettyqt.qt import QtCore
+from prettyqt.utils import types
 
 
 class Rect(QtCore.QRect):
@@ -12,6 +13,16 @@ class Rect(QtCore.QRect):
 
     def __reduce__(self):
         return type(self), (self.x(), self.y(), self.width(), self.height())
+
+    def margins_added(self, margins: types.MarginsType) -> Rect:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMargins(*margins)
+        return Rect(self.marginsAdded(margins))
+
+    def margins_removed(self, margins: types.MarginsType) -> Rect:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMargins(*margins)
+        return Rect(self.marginsRemoved(margins))
 
 
 if __name__ == "__main__":
