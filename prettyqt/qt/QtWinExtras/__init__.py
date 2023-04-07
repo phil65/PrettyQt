5d7ccef7947dbb01d43902f27ef94341ca24474f
phil65 <philipptemminghoff@googlemail.com>
2023-04-07 03:27:22 +0200

    chore: remove winextras module

--- prettyqt/qt/QtWinExtras/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Provides QtWinExtras classes and functions."""
-
-from prettyqt.qt import PYQT5, PythonQtError
-
-
-if PYQT5:
-    from PyQt5.QtWinExtras import *
-else:
-    raise PythonQtError("No Qt bindings could be found")
