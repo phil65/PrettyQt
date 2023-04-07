a3bd38d9851a3bb6a62d95ffa2c272e8bf48d8a4
phil65 <philipptemminghoff@googlemail.com>
2023-04-07 04:10:21 +0200

    feat: MultimediaWidgets for Qt6

--- prettyqt/qt/QtMultimediaWidgets/__init__.py
+++ prettyqt/qt/QtMultimediaWidgets/__init__.py
@@ -1,9 +1,13 @@
 """Provides QtMultimediaWidgets classes and functions."""
 
-from prettyqt.qt import PYQT5, PythonQtError
+from prettyqt.qt import PYQT5, PYQT6, PYSIDE6, PythonQtError
 
 
 if PYQT5:
     from PyQt5.QtMultimediaWidgets import *
+elif PYQT6:
+    from PyQt6.QtMultimediaWidgets import *
+elif PYSIDE6:
+    from PySide6.QtMultimediaWidgets import *
 else:
     raise PythonQtError("No Qt bindings could be found")
