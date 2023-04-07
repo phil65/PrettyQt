2aeb7686beaaa29d792be6fea0b92dd9950f7ca3
phil65 <philipptemminghoff@googlemail.com>
2023-04-07 03:38:04 +0200

    chore: rem VideoWidgetControl

--- prettyqt/multimediawidgets/__init__.py
+++ prettyqt/multimediawidgets/__init__.py
@@ -1,7 +1,6 @@
 """Multimediawidgets module."""
 
-from .videowidgetcontrol import VideoWidgetControl
 from .videowidget import VideoWidget
 from .graphicsvideoitem import GraphicsVideoItem
 
-__all__ = ["VideoWidgetControl", "VideoWidget", "GraphicsVideoItem"]
+__all__ = ["VideoWidget", "GraphicsVideoItem"]
