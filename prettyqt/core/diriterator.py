037a58c802f048bd512e11d2a4ac8bb6c1c3b9ea
phil65 <philipptemminghoff@googlemail.com>
2023-04-13 03:15:14 +0200

    feat(DirIterator): add get_file_path / get_file_info methods

--- prettyqt/core/diriterator.py
+++ prettyqt/core/diriterator.py
@@ -1,6 +1,24 @@
 from __future__ import annotations
 
+import pathlib
+from typing import Literal
+
+from prettyqt import core
 from prettyqt.qt import QtCore
+from prettyqt.utils import bidict
+
+
+ITERATOR_FLAG = bidict(
+    none=QtCore.QDirIterator.IteratorFlag.NoIteratorFlags,
+    subdirectories=QtCore.QDirIterator.IteratorFlag.Subdirectories,
+    follow_symlinks=QtCore.QDirIterator.IteratorFlag.FollowSymlinks,
+)
+
+IteratorFlagStr = Literal[
+    "none",
+    "subdirectories",
+    "follow_symlinks",
+]
 
 
 class DirIterator(QtCore.QDirIterator):
@@ -11,3 +29,14 @@ class DirIterator(QtCore.QDirIterator):
         if self.hasNext():
             return self.next()
         raise StopIteration
+
+    def get_file_path(self) -> pathlib.Path:
+        return pathlib.Path(self.filePath())
+
+    def get_file_info(self) -> core.FileInfo:
+        return core.FileInfo(self.fileInfo())
+
+
+if __name__ == "__main__":
+    it = core.DirIterator("C:/Intel")
+    print(list(it))
