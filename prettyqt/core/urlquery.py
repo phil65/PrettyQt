954fdbcb84ec37b52a5e1e8f0724db473602eb6f
phil65 <philipptemminghoff@googlemail.com>
2023-04-22 23:13:44 +0200

    feat(UrlQuery): add __getitem__ and __setitem__ methods

--- prettyqt/core/urlquery.py
+++ prettyqt/core/urlquery.py
@@ -20,6 +20,15 @@ class UrlQuery(QtCore.QUrlQuery):
             self.addQueryItem(k, str(v))
         return self
 
+    def __setitem__(self, key, value):
+        items = dict(self.queryItems())
+        items[key] = value
+        items = list(items.items())
+        self.setQueryItems(items)
+
+    def __getitem__(self, key: str):
+        return self.queryItemValue(key)
+
     def serialize_fields(self) -> dict[str, Any]:
         return dict(path=self.toString())
 
