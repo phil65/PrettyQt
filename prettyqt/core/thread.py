8179503530cd6290546b28edee6045b46d2d3237
phil65 <philipptemminghoff@googlemail.com>
2023-04-22 18:58:41 +0200

    feat: add some methods for Thread/ThreadPool

--- prettyqt/core/thread.py
+++ prettyqt/core/thread.py
@@ -1,7 +1,27 @@
 from __future__ import annotations
 
+from typing import Literal
+
 from prettyqt.qt import QtCore
+from prettyqt.utils import bidict
+
+
+PRIORITY = bidict(
+    idle=QtCore.QThread.Priority.IdlePriority,
+    lowest=QtCore.QThread.Priority.LowestPriority,
+    low=QtCore.QThread.Priority.LowPriority,
+    normal=QtCore.QThread.Priority.NormalPriority,
+    high=QtCore.QThread.Priority.HighPriority,
+    highest=QtCore.QThread.Priority.HighestPriority,
+    time_critical=QtCore.QThread.Priority.TimeCriticalPriority,
+    inherit=QtCore.QThread.Priority.InheritPriority,
+)
+
+PriorityStr = Literal[
+    "idle", "lowest", "low", "normal", "high", "highest", "time_critical", "inherit"
+]
 
 
 class Thread(QtCore.QThread):
-    pass
+    def get_priority(self) -> PriorityStr:
+        return PRIORITY.inverse[self.priority()]
--- prettyqt/core/threadpool.py
+++ prettyqt/core/threadpool.py
@@ -5,4 +5,16 @@ from prettyqt.qt import QtCore
 
 
 class ThreadPool(core.ObjectMixin, QtCore.QThreadPool):
-    pass
+    def __contains__(self, other: QtCore.QThread):
+        return self.contains(other)
+
+    def get_thread_priority(self) -> core.thread.PriorityStr:
+        return core.thread.PRIORITY.inverse[self.priority()]
+
+    def set_thread_priority(self, priority: core.thread.PriorityStr):
+        prio = core.thread.PRIORITY[self.threadPriority()]
+        self.setThreadPriority(prio)
+
+
+if __name__ == "__main__":
+    pool = ThreadPool()
