7749a8f1db423709df3bc8e5e8ffffac173372cf
phil65 <philipptemminghoff@googlemail.com>
2023-04-25 02:30:27 +0200

    test: enable model tests

--- tests/test_custom_models.py
+++ tests/test_custom_models.py
@@ -1,20 +1,22 @@
 """Tests for `prettyqt` package."""
 
-# import regex as re
+from importlib import metadata
+
+import regex as re
 
 from prettyqt import custom_models
 
 
 def test_regexmatchesmodel(qtmodeltester):
-    # comp = re.compile("[0-9]")
-    # text = "aa356aa356"
-    matches = []  # list(comp.finditer(text))
+    comp = re.compile("[0-9]")
+    text = "aa356aa356"
+    matches = list(comp.finditer(text))
     model = custom_models.RegexMatchesModel(matches)
     qtmodeltester.check(model, force_py=True)
 
 
 def test_importlibdistributionmodel(qtmodeltester):
-    model = custom_models.ImportlibDistributionModel([])
+    model = custom_models.ImportlibDistributionModel([metadata.distribution("prettyqt")])
     qtmodeltester.check(model, force_py=True)
     custom_models.ImportlibDistributionModel.from_package("prettyqt")
     custom_models.ImportlibDistributionModel.from_system()
