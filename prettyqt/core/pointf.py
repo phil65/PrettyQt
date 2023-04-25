d3a327cc805bc9cb22e7b549ad11651a1295773d
phil65 <philipptemminghoff@googlemail.com>
2023-04-25 06:32:58 +0200

    chore: some work for Point(F)

--- prettyqt/core/pointf.py
+++ prettyqt/core/pointf.py
@@ -6,10 +6,25 @@ from prettyqt.utils import get_repr
 
 class PointF(QtCore.QPointF):
     def __repr__(self):
-        return get_repr(self, x=self.x(), y=self.y())
+        return get_repr(self, self.x(), self.y())
 
     def __getitem__(self, index) -> float:
-        return (self.x(), self.y())[index]
+        match index:
+            case 0:
+                return self.x()
+            case 1:
+                return self.y()
+            case _:
+                raise IndexError(f"Invalid index {index} for Point")
+
+    def __setitem__(self, i, x: float):
+        match i:
+            case 0:
+                return self.setX(x)
+            case 1:
+                return self.setY(x)
+            case _:
+                raise IndexError(f"Invalid index {i} for Point")
 
     def __reduce__(self):
         return type(self), (self.x(), self.y())
