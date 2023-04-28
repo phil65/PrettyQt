642e578e5a210c98d3e822157c944bd40a8c4cac
phil65 <philipptemminghoff@googlemail.com>
2023-04-28 06:39:58 +0200

    feat(Date): add add_days, add_months, add_years and get_current_date methods

--- prettyqt/core/date.py
+++ prettyqt/core/date.py
@@ -14,7 +14,26 @@ class Date(QtCore.QDate):
     def __reduce__(self):
         return type(self), (self.year(), self.month(), self.day())
 
+    def add_days(self, days: int) -> Date:
+        return Date(self.addDays(days))
+
+    def add_months(self, months: int, calendar: QtCore.QCalendar | None = None) -> Date:
+        if calendar:
+            return Date(self.addMonths(months, calendar))
+        else:
+            return Date(self.addMonths(months))
+
+    def add_years(self, years: int, calendar: QtCore.QCalendar | None = None) -> Date:
+        if calendar:
+            return Date(self.addYears(years, calendar))
+        else:
+            return Date(self.addYears(years))
+
+    @classmethod
+    def get_current_date(cls) -> Date:
+        return Date(cls.currentDate())
+
 
 if __name__ == "__main__":
     dt = Date(2000, 11, 11)
-    print(dt)
+    dt.add_years(30)
