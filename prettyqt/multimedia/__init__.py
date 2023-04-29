eb4cb79617edeb47c592b3c5173a1e79be43e9ae
phil65 <philipptemminghoff@googlemail.com>
2023-04-29 06:44:08 +0200

    feat(multimedia): add AudioDevice class

--- prettyqt/multimedia/__init__.py
+++ prettyqt/multimedia/__init__.py
@@ -4,6 +4,7 @@ contains QtMultimedia-based classes
 """
 
 from .audioformat import AudioFormat
+from .audiodevice import AudioDevice
 from .cameraformat import CameraFormat
 from .cameradevice import CameraDevice
 from .camera import Camera
@@ -12,6 +13,7 @@ from .mediaplayer import MediaPlayer
 
 __all__ = [
     "AudioFormat",
+    "AudioDevice",
     "Camera",
     "CameraDevice",
     "CameraFormat",
---
+++ prettyqt/multimedia/audiodevice.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from prettyqt import multimedia
+from prettyqt.qt import QtMultimedia
+from prettyqt.utils import bidict
+
+
+MODE = bidict(
+    null=QtMultimedia.QAudioDevice.Mode.Null,
+    input=QtMultimedia.QAudioDevice.Mode.Input,
+    output=QtMultimedia.QAudioDevice.Mode.Output,
+)
+
+ModeStr = Literal["null", "input", "output"]
+
+
+class AudioDevice(QtMultimedia.QAudioDevice):
+    def get_mode(self) -> ModeStr:
+        return MODE.inverse[self.mode()]
+
+    def get_supported_sample_formats(
+        self,
+    ) -> list[multimedia.audioformat.SampleFormatStr]:
+        return multimedia.audioformat.SAMPLE_FORMAT.get_list(
+            self.supportedSampleFormats()
+        )
+
+    def get_channel_config(self) -> multimedia.audioformat.ChannelConfigStr:
+        return multimedia.audioformat.ChannelConfigStr.inverse[
+            self.channelConfiguration()
+        ]
+
+    def get_id(self) -> str:
+        return self.id().data().decode()
+
+
+if __name__ == "__main__":
+    fmt = AudioDevice()
+    fmt.get_id()
