afb8dec97035229e141b0ed53daeaae4edad100c
phil65 <philipptemminghoff@googlemail.com>
2023-04-29 06:40:07 +0200

    feat(PdfWriter): add get/set_version methods

--- prettyqt/gui/pdfwriter.py
+++ prettyqt/gui/pdfwriter.py
@@ -24,6 +24,22 @@ class PdfWriter(core.ObjectMixin, gui.PagedPaintDeviceMixin, QtGui.QPdfWriter):
             raise InvalidParamError(unit, gui.pagelayout.UNITS)
         return self.setPageMargins(margins, gui.pagelayout.UNITS[unit])
 
+    def get_pdf_version(self) -> gui.pagedpaintdevice.PdfVersionStr:
+        return gui.pagedpaintdevice.PDF_VERSION.inverse[self.pdfVersion()]
+
+    def set_pdf_version(self, version: gui.pagedpaintdevice.PdfVersionStr):
+        """Set pdf version.
+
+        Args:
+            version: pdf version
+
+        Raises:
+            InvalidParamError: pdf version does not exist
+        """
+        if version not in gui.pagedpaintdevice.PDF_VERSION:
+            raise InvalidParamError(version, gui.pagedpaintdevice.PDF_VERSION)
+        self.setPdfVersion(gui.pagedpaintdevice.PDF_VERSION[version])
+
 
 if __name__ == "__main__":
     app = gui.app()
