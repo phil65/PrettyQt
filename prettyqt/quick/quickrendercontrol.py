5903330c49e40ec66ac7c13c533e99ab1e98d42e
phil65 <philipptemminghoff@googlemail.com>
2023-04-29 06:41:59 +0200

    feat(QuickRenderControl): add edit_frame context manager

--- prettyqt/quick/quickrendercontrol.py
+++ prettyqt/quick/quickrendercontrol.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+import contextlib
+
 from prettyqt import core
 from prettyqt.qt import QtQuick
 
 
 class QuickRenderControl(core.ObjectMixin, QtQuick.QQuickRenderControl):
-    pass
+    @contextlib.contextmanager
+    def edit_frame(self):
+        self.beginFrame()
+        yield None
+        self.endFrame()
 
 
 if __name__ == "__main__":
