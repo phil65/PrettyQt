5241380a641548ebc2bf4b0fe6937b35c838a3c8
phil65 <philipptemminghoff@googlemail.com>
2023-04-29 20:14:50 +0200

    chore: typing

--- prettyqt/gui/syntaxhighlighter.py
+++ prettyqt/gui/syntaxhighlighter.py
@@ -16,7 +16,7 @@ class SyntaxHighlighterMixin(core.ObjectMixin):
     def get_current_block(self) -> gui.TextBlock:
         return gui.TextBlock(self.currentBlock())
 
-    def get_format(self, position: int) -> gui.TextBlock:
+    def get_format(self, position: int) -> gui.TextCharFormat:
         return gui.TextCharFormat(self.format(position))
 
     @classmethod
