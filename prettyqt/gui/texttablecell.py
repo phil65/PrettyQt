4371ac153ef09276c958ba0812eadeda4eebd2bf
phil65 <philipptemminghoff@googlemail.com>
2023-05-02 06:37:25 +0200

    feat(TtextTableCell): add get_first/last_cursor_position methods

--- prettyqt/gui/texttablecell.py
+++ prettyqt/gui/texttablecell.py
@@ -6,7 +6,14 @@ from prettyqt.qt import QtGui
 
 class TextTableCell(QtGui.QTextTableCell):
     def get_format(self) -> gui.TextCharFormat:
+        #  .format() seems to crash both bindings?
         return gui.TextCharFormat(self.format())
+
+    def get_first_cursor_position(self) -> gui.TextCursor:
+        return gui.TextCursor(self.firstCursorPosition())
+
+    def get_last_cursor_position(self) -> gui.TextCursor:
+        return gui.TextCursor(self.lastCursorPosition())
 
 
 if __name__ == "__main__":
