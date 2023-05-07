6af72c182651ddd2f540980726bae62b8059f1c6
phil65 <philipptemminghoff@googlemail.com>
2023-05-07 16:33:37 +0200

    feat(ItemSelectionModel): add set_current_index method

--- prettyqt/core/itemselectionmodel.py
+++ prettyqt/core/itemselectionmodel.py
@@ -1,8 +1,41 @@
 from __future__ import annotations
 
+from typing import Literal
+
 from prettyqt import core
 from prettyqt.qt import QtCore
+from prettyqt.utils import bidict
+
+
+SELECTION_FLAG = bidict(
+    none=QtCore.QItemSelectionModel.SelectionFlag.NoUpdate,
+    clear=QtCore.QItemSelectionModel.SelectionFlag.Clear,
+    select=QtCore.QItemSelectionModel.SelectionFlag.Select,
+    deselect=QtCore.QItemSelectionModel.SelectionFlag.Deselect,
+    toggle=QtCore.QItemSelectionModel.SelectionFlag.Toggle,
+    current=QtCore.QItemSelectionModel.SelectionFlag.Current,
+    rows=QtCore.QItemSelectionModel.SelectionFlag.Rows,
+    columns=QtCore.QItemSelectionModel.SelectionFlag.Columns,
+    select_current=QtCore.QItemSelectionModel.SelectionFlag.SelectCurrent,
+    toggle_current=QtCore.QItemSelectionModel.SelectionFlag.ToggleCurrent,
+    clear_and_select=QtCore.QItemSelectionModel.SelectionFlag.ClearAndSelect,
+)
+
+SelectionFlagStr = Literal[
+    "none",
+    "clear",
+    "select",
+    "deselect",
+    "toggle",
+    "current",
+    "rows",
+    "columns",
+    "select_current",
+    "toggle_current",
+    "clear_and_select",
+]
 
 
 class ItemSelectionModel(core.ObjectMixin, QtCore.QItemSelectionModel):
-    pass
+    def set_current_index(self, index, flag: SelectionFlagStr):
+        self.setCurrentIndex(index, SELECTION_FLAG[flag])
