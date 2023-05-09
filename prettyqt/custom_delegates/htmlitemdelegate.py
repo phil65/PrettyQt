8fbe5460ed7c92fed90c8c0a4ef86c610adf4557
phil65 <philipptemminghoff@googlemail.com>
2023-05-09 22:04:34 +0200

    fix(HtmlItemDelegate): qt6 porting bug

--- prettyqt/custom_delegates/htmlitemdelegate.py
+++ prettyqt/custom_delegates/htmlitemdelegate.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from prettyqt import constants, core, gui, widgets
-from prettyqt.qt import QtCore, QtGui, QtWidgets
+from prettyqt import gui, widgets
+from prettyqt.qt import QtCore, QtWidgets
 
 
 class HtmlItemDelegate(widgets.StyledItemDelegate):
@@ -49,4 +49,21 @@ class HtmlItemDelegate(widgets.StyledItemDelegate):
         doc.setHtml(options.text)
         doc.setTextWidth(options.rect.width())
 
-        return QtCore.QSize(doc.idealWidth(), doc.size().height())
+        return QtCore.QSize(int(doc.idealWidth()), int(doc.size().height()))
+
+
+if __name__ == "__main__":
+    from prettyqt import iconprovider
+
+    app = widgets.app()
+    model = gui.StandardItemModel()
+    model.add("test")
+    app = widgets.app()
+    w = widgets.ListView()
+    w.set_delegate(HtmlItemDelegate())
+    w.set_model(model)
+    item = gui.StandardItem("<b>test</b>")
+    item.set_data("user", iconprovider.get_icon("mdi.folder"))
+    model += item
+    w.show()
+    app.main_loop()
