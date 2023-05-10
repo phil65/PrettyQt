f05cdf89f24fb53982c7509dbdb8343de3389b67
phil65 <philipptemminghoff@googlemail.com>
2023-05-10 13:24:01 +0200

    build: add qtpy as dep for choosing API

--- prettyqt/qt/__init__.py
+++ prettyqt/qt/__init__.py
@@ -3,7 +3,7 @@
 from typing import Literal
 
 import os
-import importlib
+import qtpy
 
 
 class PythonQtError(ImportError):
@@ -16,36 +16,10 @@ def set_env_vars(qt_binding: Literal["PyQt6", "PySide6"]):
         os.environ[var] = qt_binding
 
 
-# the order in the tuple represents the search order when importing the package
-packages = {
-    "pyside6": "PySide6",
-    "pyqt6": "PyQt6",
-}
+API = qtpy.API
+API_NAME = "PyQt6" if API == "pyqt6" else "PySide6"
+set_env_vars(API_NAME)
 
-qt_api = os.getenv("QT_API")
 
-# one can create a QT_API environment variable to select which Qt API to use
-if qt_api:
-    qt_api = qt_api.lower()
-    if qt_api not in packages.keys():
-        raise ValueError(f"Invalid QT_API environment variable {qt_api!r}")
-else:
-    for name, pkg_name in packages.items():
-        try:
-            importlib.import_module(pkg_name)
-        except ImportError:
-            continue
-        else:
-            qt_api = name
-            break
-
-if not qt_api:
-    pkg_str = ", ".join(packages)
-    raise ImportError(f"One of the following Qt packages must be installed: {pkg_str}")
-
-# os.environ.setdefault("QT_API", "pyqt6")
-
-API = qt_api
-
-PYQT6 = API == "pyqt6"
-PYSIDE6 = API == "pyside6"
+PYQT6 = API.lower() == "pyqt6"
+PYSIDE6 = API.lower() == "pyside6"
