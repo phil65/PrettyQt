de40d7839b375c0e3e0ce9be459885b97b3cef0a
phil65 <philipptemminghoff@googlemail.com>
2023-05-11 03:09:54 +0200

    chore: cleanup WidgetAction, PySide inheritance workaround shouldnt be needed anymore

--- prettyqt/widgets/widgetaction.py
+++ prettyqt/widgets/widgetaction.py
@@ -1,18 +1,11 @@
 from __future__ import annotations
 
 from prettyqt import gui
-from prettyqt.qt import QtCore, QtWidgets
+from prettyqt.qt import QtWidgets
 
 
 class WidgetAction(gui.ActionMixin, QtWidgets.QWidgetAction):
-    def __init__(self, parent: QtCore.QObject | None = None):
-        super().__init__(parent)  # type: ignore
-        self._menu = None  # bc of PySide inheritance also defined here
-
-    #     self.set_text(text)
-    #     self.set_icon(icon)
-    #     self.set_shortcut(shortcut)
-    #     self.set_tooltip(tooltip)
+    pass
 
 
 if __name__ == "__main__":
