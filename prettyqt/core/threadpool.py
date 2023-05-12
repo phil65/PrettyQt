ad54caf2e34b328d332cf0e50d2d0d8c044f453b
phil65 <philipptemminghoff@googlemail.com>
2023-05-12 02:48:24 +0200

    feat: some helper stuff for ThreadPool

--- prettyqt/core/threadpool.py
+++ prettyqt/core/threadpool.py
@@ -1,7 +1,78 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+import functools
+import logging
+
 from prettyqt import core
 from prettyqt.qt import QtCore
+
+
+logger = logging.getLogger(__name__)
+
+
+class Signals(core.Object):
+    finished = core.Signal()
+    error = core.Signal(Exception)
+    result = core.Signal(object)
+    progress = core.Signal(int)
+
+
+class Worker(core.Runnable):
+    """Worker thread.
+
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    Arguments:
+        fn:     The function callback to run on this worker thread.
+                Supplied args and kwargs will be passed through to the runner.
+        args:   Arguments to pass to the callback function
+        kwargs: Keywords to pass to the callback function
+
+    """
+
+    def __init__(self, fn: Callable, *args, **kwargs):
+        self.fn = fn
+        self.args = args
+        self.keyword_args = kwargs
+        self.signals = Signals()
+        super().__init__()
+
+        # Add the callback to our kwargs
+        # self.keyword_args["progress_callback"] = self.signals.progress
+
+    @core.Slot()
+    def run(self):
+        """Initialise the runner function with passed args, kwargs."""
+        try:
+            result = self.fn(*self.args, **self.keyword_args)
+            self.signals.result.emit(result)
+            self.signals.finished.emit()
+        except Exception as e:
+            logger.exception(e)
+            self.signals.error.emit(e)
+
+
+class GeneratorWorker(Worker):
+    @core.Slot()
+    def run(self):
+        try:
+            for result in self.fn(*self.args, **self.keyword_args):
+                self.signals.result.emit(result)
+            self.signals.finished.emit()
+        except Exception as e:
+            logger.exception(e)
+            self.signals.error.emit(e)
+
+
+def run_async(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def async_func(*args, **kwargs):
+        worker = Worker(func, *args, **kwargs)
+        pool = ThreadPool.globalInstance()
+        pool.start(worker)
+
+    return async_func
 
 
 class ThreadPool(core.ObjectMixin, QtCore.QThreadPool):
