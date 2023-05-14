606ce58e799d4e3bd6773f567130dc220390f9a3
phil65 <philipptemminghoff@googlemail.com>
2023-05-14 18:31:33 +0200

    fix(ElidedLabel): qt6 fix

--- prettyqt/custom_widgets/elidedlabel.py
+++ prettyqt/custom_widgets/elidedlabel.py
@@ -32,35 +32,37 @@ class ElidedLabel(widgets.Frame):
 
     def paintEvent(self, event):
         super().paintEvent(event)
-        painter = gui.Painter(self)
-        metrics = painter.get_font_metrics()
-        did_elide = False
-        line_spacing = metrics.lineSpacing()
-        y = 0
-        layout = gui.TextLayout(self._text, painter.font())
-        with layout.process_layout():
-            while True:
-                line = layout.createLine()
-
-                if not line.isValid():
-                    break
-
-                line.setLineWidth(self.width())
-                next_line_y = y + line_spacing
-
-                if self.height() >= next_line_y + line_spacing:
-                    line.draw(painter, core.Point(0, y))
-                    y = next_line_y
-                else:
-                    last_line = self._text[line.textStart() :]
-                    elided_line = metrics.elided_text(last_line, "right", self.width())
-                    painter.drawText(0, y + metrics.ascent(), elided_line)
+        with gui.Painter(self) as painter:
+            metrics = painter.get_font_metrics()
+            did_elide = False
+            line_spacing = metrics.lineSpacing()
+            y = 0
+            layout = gui.TextLayout(self._text, painter.font())
+            with layout.process_layout():
+                while True:
                     line = layout.createLine()
-                    did_elide = line.isValid()
-                    break
-        if did_elide != self.elided:
-            self.elided = did_elide
-            self.elision_changed.emit(did_elide)
+
+                    if not line.isValid():
+                        break
+
+                    line.setLineWidth(self.width())
+                    next_line_y = y + line_spacing
+
+                    if self.height() >= next_line_y + line_spacing:
+                        line.draw(painter, core.PointF(0, y))
+                        y = next_line_y
+                    else:
+                        last_line = self._text[line.textStart() :]
+                        elided_line = metrics.elided_text(
+                            last_line, "right", self.width()
+                        )
+                        painter.drawText(0, y + metrics.ascent(), elided_line)
+                        line = layout.createLine()
+                        did_elide = line.isValid()
+                        break
+            if did_elide != self.elided:
+                self.elided = did_elide
+                self.elision_changed.emit(did_elide)
 
     # def paintEvent(self, event):
     #     painter = gui.Painter(self)
