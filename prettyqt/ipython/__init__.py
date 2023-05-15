5a35a1b07cf43e978bbf53042efc07524d39139a
phil65 <philipptemminghoff@googlemail.com>
2023-05-15 09:17:53 +0200

    chore: __init__.py for ipython module

---
+++ prettyqt/ipython/__init__.py
@@ -0,0 +1,10 @@
+"""ipython module.
+
+contains IPython-based classes
+"""
+
+from .inprocessipythonwidget import InProcessIPythonWidget
+from .outofprocessipythonwidget import OutOfProcessIPythonWidget
+
+
+__all__ = ["InProcessIPythonWidget", "OutOfProcessIPythonWidget"]
