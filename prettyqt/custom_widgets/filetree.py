1f8108dd24a2efd46eb39d3635631a4315a3860e
phil65 <philipptemminghoff@googlemail.com>
2023-05-15 14:39:45 +0200

    chore: some filetree prep

--- prettyqt/custom_widgets/filetree.py
+++ prettyqt/custom_widgets/filetree.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 
+import logging
+import os
 import pathlib
 
-from prettyqt import widgets
+from prettyqt import core, widgets
+
+
+logger = logging.getLogger(__name__)
 
 
 class FileTree(widgets.TreeView):
-    def __init__(self, filters: list[str] | None = None, *args, **kwargs):
+    def __init__(self, *args, filters: list[str] | None = None, **kwargs):
         super().__init__(*args, **kwargs)
         self.setRootIsDecorated(True)
         self.h_header.set_resize_mode("resize_to_contents")
@@ -35,28 +40,86 @@ class FileTree(widgets.TreeView):
     def _save_expanded_on_level(self, index):
         if not self.isExpanded(index):
             return None
+        model = self.model()
         if index.isValid():
-            path = self.model().data(index, self.model().FilePathRole)
+            path = model.data(index, model.FilePathRole)
             self.expanded_ids.append(path)
-        for i in range(self.model().rowCount(index)):
-            val = self.model().index(i, 0, index)
+        for i in range(model.rowCount(index)):
+            val = model.index(i, 0, index)
             self._save_expanded_on_level(val)
 
     def _restore_expanded_on_level(self, index):
-        path = self.model().data(index, self.model().FilePathRole)
+        model = self.model()
+        path = model.data(index, model.FilePathRole)
         if path not in self.expanded_ids:
             return None
         self.setExpanded(index, True)
-        if not self.model().hasChildren(index):
+        if not model.hasChildren(index):
             return None
         path = pathlib.Path(path)
         for it in path.iterdir():
-            child_index = self.model().index(str(path / it))
+            child_index = model.index(str(path / it))
             self._restore_expanded_on_level(child_index)
+
+
+class BreadCrumbsToolBar(widgets.ToolBar):
+    path_clicked = core.Signal(pathlib.Path)
+
+    def set_breadcrumbs(self, path: os.PathLike):
+        self.clear()
+        path = pathlib.Path(path)
+        logger.info(f"Setting breadrumbs to {path}")
+        if path.parents:
+            action = self.add_action(
+                text=str(path.parents[-1]), triggered=self._on_button_clicked
+            )
+            action.setData(path.parents[-1])
+        for p in reversed(path.parents[:-1]):
+            action = self.add_action(text=p.name, triggered=self._on_button_clicked)
+            action.setData(p)
+        if path.is_dir():
+            action = self.add_action(text=path.name, triggered=self._on_button_clicked)
+            action.setData(path)
+
+    def _on_button_clicked(self, button):
+        action = self.sender()
+        path = action.data()
+        self.path_clicked.emit(path)
+
+
+class FileExplorer(widgets.Widget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._file_edit = widgets.LineEdit()
+        self._file_tree = FileTree(self)
+        self.tool_bar = BreadCrumbsToolBar(self)
+
+        self._file_tree.selectionModel().currentChanged.connect(self._on_current_changed)
+        self.tool_bar.path_clicked.connect(self._on_breadcrumbs_clicked)
+        self._file_edit.value_changed.connect(self._update_root)
+
+        with widgets.VBoxLayout(self) as layout:
+            layout.addWidget(self.tool_bar)
+            # layout.addWidget(self._file_edit)
+            layout.addWidget(self._file_tree)
+        # self._update_root(curpath)
+
+    def _on_breadcrumbs_clicked(self, path: pathlib.Path):
+        index = self._file_tree.model().index(str(path))
+        self._file_tree.set_current_index(index)
+
+    def _on_current_changed(self, new, _):
+        if (model := new.model()) is not None:
+            path = new.data(model.Roles.FilePathRole)
+            self.tool_bar.set_breadcrumbs(path)
+
+    def _update_root(self, path: os.PathLike):
+        if pathlib.Path(path).exists():
+            self._file_tree.setRoot(os.fspath(path))
 
 
 if __name__ == "__main__":
     app = widgets.app()
-    w = FileTree()
+    w = FileExplorer()
     w.show()
     app.main_loop()
