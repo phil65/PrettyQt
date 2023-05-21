d5008c6a32243151b96499b1a41cb2f833641b8c
phil65 <philipptemminghoff@googlemail.com>
2023-05-21 22:43:13 +0200

    feat: add PainterPath.add

--- prettyqt/gui/painterpath.py
+++ prettyqt/gui/painterpath.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Iterator
+import math
 from typing import Literal
 
 from prettyqt import constants, core
@@ -35,6 +36,28 @@ class PainterPath(QtGui.QPainterPath):
 
     def __setitem__(self, index: int, value: tuple[int, int]):
         self.setElementPositionAt(index, *value)
+
+    def __iadd__(self, other):
+        if isinstance(
+            other, QtCore.QPoint | QtCore.QRect | QtGui.QPainterPath | QtGui.QRegion
+        ):
+            self.add(other)
+            return self
+        else:
+            raise ValueError(other)
+
+    def add(
+        self, other: QtCore.QPoint | QtCore.QRect | QtGui.QPainterPath | QtGui.QRegion
+    ):
+        match other:
+            case QtGui.QPolygonF():
+                self.addPolygon(other)
+            case QtGui.QPainterPath():
+                self.addPath(other)
+            case QtCore.QRect():
+                self.addRect(other)
+            case QtGui.QRegion():
+                self.addRegion(other)
 
     def __bool__(self):
         return not self.isEmpty()
@@ -79,7 +102,50 @@ class PainterPath(QtGui.QPainterPath):
     def to_reversed(self) -> PainterPath:
         return PainterPath(self.toReversed())
 
+    @classmethod
+    def create_star(cls, size: int):
+        path = cls()
+        star_center_x = size / 2
+        star_center_y = size / 2
+        radius_outer = size * 0.35
+        golden_ratio = (1 + math.sqrt(5)) / 2
+        radius_inner = radius_outer / (1 + golden_ratio)
+        theta_start = math.pi / 2
+        theta_inc = (2 * math.pi) / 10
+        for n in range(11):
+            theta = theta_start + (n * theta_inc)
+            theta = theta % (2 * math.pi)
+            if n % 2 == 0:
+                x = radius_outer * math.cos(theta)
+                y = radius_outer * math.sin(theta)
+
+            else:
+                x = radius_inner * math.cos(theta)
+                y = radius_inner * math.sin(theta)
+
+            x_adj = star_center_x - x
+            y_adj = star_center_y - y + 3
+            if n == 0:
+                path.moveTo(x_adj, y_adj)
+            else:
+                path.lineTo(x_adj, y_adj)
+
+        return path
+
 
 if __name__ == "__main__":
-    p = PainterPath(QtCore.QPoint(1, 1))
-    print(type(p[0]))
+    from prettyqt import gui, widgets
+
+    app = widgets.app()
+
+    class Test(widgets.Widget):
+        def paintEvent(self, event):
+            with gui.Painter(self) as painter:
+                painter.drawPath(gui.PainterPath.create_star(400, 200))
+
+    p = PainterPath.create_star(400, 200)
+    p += QtCore.QPoint(1, 1)
+
+    t = Test()
+    t.show()
+    app.main_loop()
