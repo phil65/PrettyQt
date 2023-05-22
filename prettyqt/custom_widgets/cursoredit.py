7e3555ad73451c96dc6ec72c1a7ca9331f02fe20
phil65 <philipptemminghoff@googlemail.com>
2023-05-22 05:38:51 +0200

    chore: proper CursorEdit

--- prettyqt/custom_widgets/cursoredit.py
+++ prettyqt/custom_widgets/cursoredit.py
@@ -1,28 +1,34 @@
 from __future__ import annotations
 
-from prettyqt import core, gui, widgets
+from prettyqt import core, gui, custom_widgets
+from prettyqt.qt import QtCore
 
 
-class CursorEdit(widgets.Widget):
+class CursorEdit(custom_widgets.EnumComboBox):
     value_changed = core.Signal(gui.Cursor)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.set_enum_class(QtCore.Qt.CursorShape)
+        self.currentIndexChanged.connect(self._on_value_change)
 
     def _on_value_change(self):
         self._value = self.get_value()
         self.value_changed.emit(self._value)
 
     def get_value(self) -> gui.Cursor:
-        return self._value
+        shape = super().get_value()
+        return gui.Cursor(shape)
 
     def set_value(self, value: gui.Cursor):
-        self._value = value
+        super().set_value(value.shape())
 
     value = core.Property(gui.Cursor, get_value, set_value, user=True)
 
 
 if __name__ == "__main__":
+    from prettyqt import widgets
+
     app = widgets.app()
     widget = CursorEdit(window_title="Test")
     widget.set_value(gui.Cursor())
