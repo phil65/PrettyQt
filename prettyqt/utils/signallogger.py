2e6b779aa1b76de17d5e6ab03fae0d166b0b48e8
phil65 <philipptemminghoff@googlemail.com>
2023-05-22 23:19:34 +0200

    chore: SignalLogger cleanup

--- prettyqt/utils/signallogger.py
+++ prettyqt/utils/signallogger.py
@@ -5,26 +5,11 @@ import logging
 from prettyqt import core
 
 
-class LineSignalLogger(logging.Handler, core.Object):
-    log_line = core.Signal(str)
-
-    def __init__(self):
-        super().__init__()
-        core.Object.__init__(self)
+class SignalLogger(core.Object, logging.Handler):
+    formatted_line = core.Signal(str)
+    log_record = core.Signal(logging.LogRecord)
 
     def emit(self, record):
         msg = self.format(record)
-        self.log_line.emit(msg)
-
-
-class Signals(core.Object):
-    log_record = core.Signal(logging.LogRecord)
-
-
-class RecordSignalLogger(logging.Handler):
-    def __init__(self):
-        super().__init__()
-        self.signals = Signals()
-
-    def emit(self, record):
-        self.signals.log_record.emit(record)
+        self.formatted_line.emit(msg)
+        self.log_record.emit(record)
