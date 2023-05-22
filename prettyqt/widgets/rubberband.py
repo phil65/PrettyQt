1c844910387535515136e1d1c4d53b723d1cb8f0
phil65 <philipptemminghoff@googlemail.com>
2023-05-22 04:04:56 +0200

    feat(RubberBand): add track widget method

--- prettyqt/widgets/rubberband.py
+++ prettyqt/widgets/rubberband.py
@@ -19,13 +19,38 @@ class RubberBandMixin(widgets.WidgetMixin):
         self,
         shape: ShapeStr | QtWidgets.QRubberBand.Shape,
         parent: QtWidgets.QWidget | None = None,
+        **kwargs,
     ):
         shape = SHAPE[shape] if isinstance(shape, str) else shape
-        super().__init__(shape, parent)
+        super().__init__(shape, parent, **kwargs)
 
     def get_shape(self) -> ShapeStr:
         return SHAPE.inverse[self.shape()]
 
+    def track_widget(self, widget: QtWidgets.QWidget):
+        from prettyqt import eventfilters
+
+        event_catcher = eventfilters.EventCatcher(exclude="paint", parent=widget)
+        widget.installEventFilter(event_catcher)
+        event_catcher.caught.connect(self.update_size)
+
+    def update_size(self):
+        self.setGeometry(self.parent().rect())
+
 
 class RubberBand(RubberBandMixin, QtWidgets.QRubberBand):
     pass
+
+
+if __name__ == "__main__":
+    app = widgets.app()
+    splitter = widgets.Splitter()
+    widget = widgets.RadioButton("test")
+    widget2 = widgets.RadioButton("test")
+    splitter.add(widget)
+    splitter.add(widget2)
+    rb = RubberBand("rectangle", widget)
+    splitter.show()
+    rb.track_widget(widget)
+    rb.show()
+    app.main_loop()
