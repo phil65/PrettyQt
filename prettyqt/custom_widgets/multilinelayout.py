4b3f628e91cae3525e9139f9fc5af216d5a853ab
phil65 <philipptemminghoff@googlemail.com>
2023-05-23 21:25:57 +0200

    feat: add MultiLineLayout

---
+++ prettyqt/custom_widgets/multilinelayout.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+from prettyqt import widgets
+from prettyqt.qt import QtWidgets
+
+
+class MultiLineLayout(widgets.BoxLayout):
+    def __init__(self, vertical: bool = True, row_number: int = 10, *args, **kwargs):
+        self._items = []
+        self.row_nb = row_number
+        self.cindex = 0
+        self.layouts = []
+        direction = self.Direction.TopToBottom if vertical else self.Direction.LeftToRight
+        super().__init__(direction, **kwargs)
+
+    def __add__(self, other: QtWidgets.QWidget | QtWidgets.QLayout) -> MultiLineLayout:
+        if not isinstance(other, QtWidgets.QWidget | QtWidgets.QLayout):
+            raise TypeError()
+        self.add(other)
+        return self
+
+    def set_direction(self, direction):
+        super().setDirection(direction)
+        direction = (
+            self.Direction.LeftToRight
+            if self.get_direction() == "top_to_bottom"
+            else self.Direction.TopToBottom
+        )
+        for layout in self.layouts:
+            layout.setDirection(direction)
+        self.repaint()
+
+    # def __del__(self):
+    #     item = self.takeAt(0)
+    #     while item:
+    #         item = self.takeAt(0)
+
+    def addWidget(self, widget):
+        if self.cindex == 0:
+            direction = (
+                self.Direction.LeftToRight
+                if self.get_direction() == "top_to_bottom"
+                else self.Direction.TopToBottom
+            )
+            self.col_layout = widgets.BoxLayout(direction)
+            super().addLayout(self.col_layout)
+            self.layouts.append(self.col_layout)
+        self.col_layout.add(widget)
+        self.cindex = (self.cindex + 1) % self.row_nb
+
+    def addLayout(self, layout: QtWidgets.QLayout):
+        widget = widgets.Widget()
+        widget.setLayout(layout)
+        self.addWidget(widget)
+
+    def addItem(self, item):
+        self._items.append(item)
+
+    def itemAt(self, idx):
+        try:
+            return self._items[idx]
+        except IndexError:
+            pass
+
+    # def takeAt(self, idx):
+    #     layout_idx = idx % len(self.layouts)
+    #     try:
+    #         self.layouts[layout_idx].takeAt(idx)
+    #         for i, layout in enumerate(self.layouts[layout_idx:]):
+    #             prev_layout = self.layouts[layout_idx + i - 1]
+    #             layout.addItem(prev_layout.takeAt(prev_layout.count() - 1))
+    #         return self._items.pop(idx)
+    #     except IndexError:
+    #         pass
+
+    def count(self):
+        return len(self._items)
+
+
+if __name__ == "__main__":
+    app = widgets.app()
+    widget = widgets.Widget()
+    layout = MultiLineLayout(parent=widget)
+    layout.add(widgets.PushButton("Short"))
+    layout.add(widgets.PushButton("Longer"))
+    layout.add(widgets.PushButton("Different text"))
+    layout.add(widgets.PushButton("More text"))
+    layout.add(widgets.PushButton("Even longer button text"))
+    widget.set_layout(layout)
+    widget.show()
+    app.main_loop()
--- prettyqt/custom_widgets/standardiconswidget.py
+++ prettyqt/custom_widgets/standardiconswidget.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from prettyqt import widgets
+from prettyqt.custom_widgets import multilinelayout
 from prettyqt.qt import QtWidgets
 
 
@@ -9,21 +10,14 @@ class StandardIconsWidget(widgets.Widget):
 
     def __init__(self, parent: QtWidgets.QWidget | None = None):
         super().__init__(parent)
-        layout = widgets.HBoxLayout()
-        row_nb = 14
-        cindex = 0
+        layout = multilinelayout.MultiLineLayout(parent=self)
         for k in widgets.style.STANDARD_PIXMAP:
-            if cindex == 0:
-                col_layout = widgets.VBoxLayout()
             icon_layout = widgets.HBoxLayout()
             icon = widgets.Application.get_style_icon(k)
             label = widgets.Label(pixmap=icon.pixmap(32, 32))
             icon_layout.addWidget(label)
             icon_layout.addWidget(widgets.LineEdit(k))
-            col_layout.addLayout(icon_layout)
-            cindex = (cindex + 1) % row_nb
-            if cindex == 0:
-                layout.addLayout(col_layout)
+            layout.addLayout(icon_layout)
         self.set_layout(layout)
         self.set_title("Standard Platform Icons")
         icon = widgets.Application.get_style_icon("titlebar_menu_button")
