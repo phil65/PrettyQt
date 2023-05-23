0aa9c3df0419c7b70726f14c1906e4b95e3ea558
phil65 <philipptemminghoff@googlemail.com>
2023-05-23 18:52:25 +0200

    chore: WidgetDelegate work

--- prettyqt/custom_delegates/widgetdelegate.py
+++ prettyqt/custom_delegates/widgetdelegate.py
@@ -12,7 +12,8 @@ class WidgetDelegate(widgets.StyledItemDelegate):
     ):
         super().__init__(parent)
         self._widget_role = role
-        # self._editors = {}
+        self._editors = {}
+        self._cache_editors = True
 
     def paint(self, painter, option, index):
         value = self._editor_for_index(index)
@@ -32,15 +33,18 @@ class WidgetDelegate(widgets.StyledItemDelegate):
         # using index.data() does not work with PyQt6, it casts widgets to QObjects
         model = index.model()
         editor = model.data(index, self._widget_role)
-        return editor
-        # key = str((index.row(), index.column()))
-        # while (index := index.parent()).isValid():
-        #     key += str((index.row(), index.column()))
-        # if key not in self._editors:
-        #     self._editors[key] = editor
-        #     return editor
-        # else:
-        #     return self._editors[key]
+        if not self._cache_editors:
+            return editor
+        # two reasons for caching:  1) possibly performance. 2) keeping a reference
+        # perhaps it would be nicer to just cache the currently chosen editor, not sure.
+        key = str((index.row(), index.column()))
+        while (index := index.parent()).isValid():
+            key += str((index.row(), index.column()))
+        if key not in self._editors:
+            self._editors[key] = editor
+            return editor
+        else:
+            return self._editors[key]
 
     def createEditor(self, parent, option, index):
         editor = self._editor_for_index(index)
@@ -78,27 +82,41 @@ class WidgetDelegate(widgets.StyledItemDelegate):
 
 
 if __name__ == "__main__":
+    import logging
+
     app = widgets.app()
-    table_widget = widgets.TableWidget(2, 2)
-    # table_widget.set_delegate(StarDelegate(), column=1)
-    table_widget.setEditTriggers(table_widget.EditTrigger.AllEditTriggers)
-    table_widget.set_selection_behavior("rows")
-    table_widget.setHorizontalHeaderLabels(["Title", "Rating"])
-    types = dict(
-        color=widgets.PlainTextEdit(),
-        time=widgets.DateEdit(),
-    )
-    for i, (k, v) in enumerate(types.items()):
-        item_1 = widgets.TableWidgetItem(k)
-        item_2 = widgets.TableWidgetItem()
-        item_2.setData(constants.DISPLAY_ROLE, v)
-        item_2.setData(constants.USER_ROLE, v)
-        table_widget[i, 0] = item_1
-        table_widget[i, 1] = item_2
+    table_widget = widgets.TableView()
+    pb = widgets.PushButton("test", clicked=lambda: logging.info("x"))
+
+    class Model(core.AbstractTableModel):
+        def rowCount(self, index=None):
+            return 1
+
+        def columnCount(self, index=None):
+            return 2
+
+        def data(self, index, role=None):
+            match role, index.column():
+                case constants.DISPLAY_ROLE | constants.USER_ROLE, 0:
+                    # return pb
+                    return widgets.PushButton("test", clicked=lambda: logging.info("x"))
+
+                case constants.DISPLAY_ROLE | constants.USER_ROLE, 1:
+                    return pb
+
+        def flags(self, index):
+            return (
+                super().flags(index)
+                | constants.IS_EDITABLE
+                | constants.IS_ENABLED
+                | constants.IS_SELECTABLE
+            )
 
     delegate = WidgetDelegate(parent=table_widget)
-    table_widget.set_delegate(delegate, column=1)
-
+    table_widget.set_delegate(delegate)
+    model = Model()
+    table_widget.set_model(model)
+    table_widget.set_edit_triggers("all")
     table_widget.resizeColumnsToContents()
     table_widget.resize(500, 300)
     table_widget.show()
