6018aef7e97bf12a1f9f28b0301583bf129315a2
phil65 <philipptemminghoff@googlemail.com>
2023-05-25 02:55:53 +0200

    feat: add FlattenedTreeProxyModel

---
+++ prettyqt/custom_models/flattenedtreeproxymodel.py
@@ -0,0 +1,210 @@
+from __future__ import annotations
+
+import enum
+
+from prettyqt import core
+from prettyqt.qt import QtCore
+
+
+class FlattenedTreeProxyModel(core.AbstractProxyModel):
+    class FlatteningMode(enum.IntEnum):
+        """Flattening modes."""
+
+        Default = 1
+        InternalNodesDisabled = 2
+        LeavesOnly = 4
+
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent, **kwargs)
+        self._source_column = 0
+        self._flattening_mode = FlattenedTreeProxyModel.FlatteningMode.Default
+        self._source_root_index = core.ModelIndex()
+        self._source_key: list[tuple[int, ...]] = []
+        self._source_offset: dict[tuple[int, ...], int] = {}
+
+    def setSourceModel(self, model):
+        with self.reset_model():
+            if (curr_model := self.sourceModel()) is not None:
+                curr_model.dataChanged.disconnect(self._source_data_changed)
+                curr_model.rowsInserted.disconnect(self._source_rows_inserted)
+                curr_model.rowsRemoved.disconnect(self._source_rows_removed)
+                curr_model.rowsMoved.disconnect(self._source_rows_moved)
+
+            super().setSourceModel(model)
+            self._update_row_mapping()
+
+            model.dataChanged.connect(self._source_data_changed)
+            model.rowsInserted.connect(self._source_rows_inserted)
+            model.rowsRemoved.connect(self._source_rows_removed)
+            model.rowsMoved.connect(self._source_rows_moved)
+
+    def setSourceColumn(self, column: int):
+        with self.reset_model():
+            self._source_column = column
+            self._update_row_mapping()
+
+    def sourceColumn(self) -> int:
+        return self._source_column
+
+    def setSourceRootIndex(self, root_index: core.ModelIndex):
+        with self.reset_model():
+            self._source_root_index = root_index
+            self._update_row_mapping()
+
+    def sourceRootIndex(self) -> core.ModelIndex:
+        return self._source_root_index
+
+    def set_flattening_mode(self, mode: FlattenedTreeProxyModel.FlatteningMode):
+        if mode != self._flattening_mode:
+            with self.reset_model():
+                self._flattening_mode = mode
+                self._update_row_mapping()
+
+    def get_flattening_mode(self) -> FlattenedTreeProxyModel.FlatteningMode:
+        return self._flattening_mode
+
+    def mapFromSource(self, source_index: core.ModelIndex) -> core.ModelIndex:
+        if not source_index.isValid():
+            return source_index
+        key = self._get_index_key(source_index)
+        offset = self._source_offset[key]
+        row = offset + source_index.row()
+        return self.index(row, 0)
+
+    def mapToSource(self, index: core.ModelIndex) -> core.ModelIndex:
+        if not index.isValid():
+            return index
+        row = index.row()
+        source_key_path = self._source_key[row]
+        return self._index_from_key(source_key_path)
+
+    def index(
+        self, row: int, column: int = 0, parent: core.ModelIndex | None = None
+    ) -> core.ModelIndex:
+        parent = parent or core.ModelIndex()
+        return (
+            core.ModelIndex()
+            if parent.isValid()
+            else self.createIndex(row, column, object=row)
+        )
+
+    def parent(self, child) -> core.ModelIndex:
+        return core.ModelIndex()
+
+    def rowCount(self, parent=None) -> int:
+        parent = parent or core.ModelIndex()
+        return 0 if parent.isValid() else len(self._source_key)
+
+    def columnCount(self, parent=None) -> int:
+        parent = parent or core.ModelIndex()
+        return 0 if parent.isValid() else 1
+
+    def flags(self, index):
+        flags = super().flags(index)
+        if self._flattening_mode == self.FlatteningMode.InternalNodesDisabled:
+            source_index = self.mapToSource(index)
+            sourceModel = self.sourceModel()
+            if (
+                sourceModel is not None
+                and sourceModel.rowCount(source_index) > 0
+                and flags & QtCore.Qt.ItemFlag.ItemIsEnabled
+            ):
+                # Internal node, enabled in the source model, disable it
+                flags ^= QtCore.Qt.ItemFlag.ItemIsEnabled
+        return flags
+
+    def _get_index_key(self, index: core.ModelIndex):
+        """Return a key for `index` from the source model into the _source_offset map.
+
+        The key is a tuple of row indices on
+        the path from the top if the model to the `index`.
+        """
+        key_path = []
+        parent = index
+        while parent.isValid():
+            key_path.append(parent.row())
+            parent = parent.parent()
+        return tuple(reversed(key_path))
+
+    def _index_from_key(self, key_path: tuple[int, ...]):
+        """Return a source QModelIndex for the given key."""
+        model = self.sourceModel()
+        if model is None:
+            return core.ModelIndex()
+        index = model.index(key_path[0], 0)
+        for row in key_path[1:]:
+            index = model.index(row, 0, index)
+        return index
+
+    def _update_row_mapping(self):
+        source = self.sourceModel()
+
+        source_key: list[tuple[int, ...]] = []
+        source_offset_map: dict[tuple[int, ...], int] = {}
+
+        def create_mapping(model, index: core.ModelIndex, key_path: tuple[int, ...]):
+            rowcount = model.rowCount(index)
+            if rowcount > 0:
+                if self._flattening_mode != self.FlatteningMode.LeavesOnly:
+                    source_offset_map[key_path] = len(source_offset_map)
+                    source_key.append(key_path)
+                for i in range(rowcount):
+                    create_mapping(model, model.index(i, 0, index), key_path + (i,))
+            else:
+                source_offset_map[key_path] = len(source_offset_map)
+                source_key.append(key_path)
+
+        if source is not None:
+            for i in range(source.rowCount()):
+                create_mapping(source, source.index(i, 0), (i,))
+        self._source_key = source_key
+        self._source_offset = source_offset_map
+
+    def _source_data_changed(self, top, bottom):
+        changed_indexes = [top.sibling(i, 0) for i in range(top.row(), bottom.row() + 1)]
+        for ind in changed_indexes:
+            self.dataChanged.emit(ind, ind)
+
+    def _source_rows_inserted(self, parent: core.ModelIndex, start: int, end: int):
+        with self.reset_model():
+            self._update_row_mapping()
+
+    def _source_rows_removed(self, parent: core.ModelIndex, start: int, end: int):
+        with self.reset_model():
+            self._update_row_mapping()
+
+    def _source_rows_moved(
+        self, source_parent, source_start, source_end, dest_parent, dest_row
+    ):
+        with self.reset_model():
+            self._update_row_mapping()
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+    from prettyqt.custom_models import JsonModel
+
+    app = widgets.app()
+    dist = [
+        dict(
+            a=2,
+            b={
+                "a": 4,
+                "b": [1, 2, 3],
+                "jkjkjk": "tekjk",
+                "sggg": "tekjk",
+                "fdfdf": "tekjk",
+                "xxxx": "xxx",
+            },
+        ),
+        6,
+        "jkjk",
+    ]
+    source_model = JsonModel(dist)
+    model = FlattenedTreeProxyModel()
+    model.setSourceModel(source_model)
+    table = widgets.TreeView()
+    table.setRootIsDecorated(True)
+    table.set_model(model)
+    table.show()
+    app.main_loop()
