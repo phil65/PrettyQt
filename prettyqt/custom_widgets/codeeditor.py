fffc03a16d6ae2f0729e4ddb6058ed3a02161e17
phil65 <philipptemminghoff@googlemail.com>
2023-05-30 21:10:14 +0200

    chore: prep

--- prettyqt/custom_widgets/codeeditor.py
+++ prettyqt/custom_widgets/codeeditor.py
@@ -15,15 +15,102 @@ class CodeEditor(widgets.PlainTextEdit):
         self.set_current_line_color(gui.Color(128, 128, 128, 20))
         self.set_syntaxhighlighter(language)
 
+    #     self.delimiter = "    "
+    #     self.completion_state = 0
+    #     self.completing = False
+    #     self.cursorPositionChanged.connect(self.reset_completion)
+
+    # def keyPressEvent(self, event):
+    #     match event.key():
+    #         case QtCore.Qt.Key.Key_Backtab if self.textCursor().hasSelection():
+    #             start_cursor = self.get_textCursor()
+    #             with start_cursor.edit_block():
+    #                 start_pos = start_cursor.selectionStart()
+    #                 start_cursor.setPosition(start_pos)
+    #                 start_cursor.move_position("start_of_line")
+    #                 start_cursor.clearSelection()
+    #                 end_cursor = self.get_text_cursor()
+    #                 end_pos = end_cursor.selectionEnd()
+    #                 end_cursor.setPosition(end_pos)
+    #                 end_cursor.move_position("start_of_line")
+    #                 delimit_len = len(self.delimiter)
+    #                 while start_cursor.anchor() != end_cursor.position():
+    #                     start_cursor.move_position(
+    #                         "next_character", mode="keep", n=delimit_len
+    #                     )
+    #                     if start_cursor.selectedText() == self.delimiter:
+    #                         start_cursor.removeSelectedText()
+    #                     start_cursor.move_position("next_block")
+    #                 start_cursor.move_position(
+    #                     "next_character", mode="keep", n=delimit_len
+    #                 )
+    #                 if start_cursor.selectedText() == self.delimiter:
+    #                     start_cursor.removeSelectedText()
+    #         case QtCore.Qt.Key.Key_Tab if self.textCursor().hasSelection():
+    #             start_cursor = self.get_text_cursor()
+    #             with start_cursor.edit_block():
+    #                 start_pos = start_cursor.selectionStart()
+    #                 start_cursor.setPosition(start_pos)
+    #                 start_cursor.move_position("start_of_line")
+    #                 end_cursor = self.get_text_cursor()
+    #                 end_pos = end_cursor.selectionEnd()
+    #                 end_cursor.setPosition(end_pos)
+    #                 end_cursor.move_position("start_of_line")
+    #                 while start_cursor.position() != end_cursor.position():
+    #                     start_cursor.insertText(self.delimiter)
+    #                     start_cursor.move_position("next_block")
+    #                 start_cursor.insertText(self.delimiter)
+    #         case QtCore.Qt.Key.Key_Escape if self.completion_state > 0:
+    #             self.completion_state = 0
+    #             cursor = self.get_text_cursor()
+    #             with cursor.edit_block():
+    #                 self.selecter.replace_block_at_cursor(self._orig_text)
+    #             self._orig_text == None
+    #         case QtCore.Qt.Key.Key_Tab:
+    #             if self.is_start():
+    #                 self.textCursor().insertText(self.delimiter)
+    #             else:
+    #                 cursor = self.get_text_cursor()
+    #                 with cursor.edit_block():
+    #                     self.completing = True
+    #                     if self.completion_state == 0:
+    #                         self._orig_text = self.textCursor().block().text()
+    #                     if self.completion_state > 0:
+    #                         self.selecter.replace_block_at_cursor(self._orig_text)
+    #                     new_text = self.completer.complete(
+    #                         self._orig_text, self.completion_state
+    #                     )
+    #                     if new_text:
+    #                         if new_text.find("(") > 0:
+    #                             new_text = new_text[0 : new_text.find("(") + 1]
+    #                         self.completion_state += 1
+    #                         self.selecter.replace_block_at_cursor(new_text)
+    #                     else:
+    #                         self.completion_state = 0
+    #                         self.selecter.replace_block_at_cursor(self._orig_text)
+    #                         self._orig_text == None
+    #                 self.completing = False
+    #         case _:
+    #             return super().keyPressEvent(event)
+
+    # def reset_completion(self):
+    #     if not self.completing:
+    #         self.completion_state = 0
+
+    # def is_start(self) -> bool:
+    #     temp_cursor = self.textCursor()
+    #     if temp_cursor.positionInBlock() == 0:
+    #         return True
+    #     start_text = temp_cursor.block().text()[0 : temp_cursor.positionInBlock()]
+    #     delim = set(self.delimiter)
+    #     return set(start_text) - delim == set()
+
     def resizeEvent(self, event):
         super().resizeEvent(event)
 
         cr = self.contentsRect()
         rect = core.Rect(cr.left(), cr.top(), self.line_area_width(), cr.height())
         self.line_area.setGeometry(rect)
-
-    def text(self) -> str:
-        return self.toPlainText()
 
     def line_area_width(self) -> int:
         digits = len(str(self.blockCount()))
