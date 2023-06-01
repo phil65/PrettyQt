54bf975bf436f285fe66b48e599e995018e1ac66
phil65 <philipptemminghoff@googlemail.com>
2023-06-01 18:13:56 +0200

    chore: scripts

---
+++ scripts/download_docs.py
@@ -0,0 +1,30 @@
+import inspect
+import requests
+from bs4 import BeautifulSoup
+import pathlib
+
+from prettyqt.qt import QtCore, QtGui, QtWidgets
+
+module_dict = dict(QtWidgets=QtWidgets, QtGui=QtGui, QtCore=QtCore)
+
+for module_name, module in module_dict.items():
+    clsmembers = inspect.getmembers(module, inspect.isclass)
+    for klass_name, _klass in clsmembers:
+        url = f"https://doc.qt.io/qtforpython-6/PySide6/{module_name}/{klass_name}.html"
+        website = requests.get(url)
+        results = BeautifulSoup(website.content, "html.parser")
+        match = results.find(id="detailed-description")
+        if match is None:
+            continue
+        text = match.get_text()
+        text = text.encode("cp1252", errors="ignore")
+        text = text.decode(errors="ignore")
+        # try:
+        #     print(text)
+        # except:
+        #     pass
+        path = pathlib.Path() / module_name
+        path.mkdir(parents=True, exist_ok=True)
+        filepath = path / f"{klass_name}.txt"
+        filepath.write_text(text)
+        print(f"wrote to {filepath}")
