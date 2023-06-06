3a810d90a8a19e8c3c713305535fcde3e3992814
phil65 <philipptemminghoff@googlemail.com>
2023-06-06 23:39:51 +0200

    chore: testing

--- tests/test_printsupport.py
+++ tests/test_printsupport.py
@@ -13,3 +13,26 @@ def test_printer():
     assert printer.get_pdf_version() == "v1_6"
     with pytest.raises(InvalidParamError):
         printer.set_pdf_version("test")
+
+
+def test_printdialog():
+    dlg = printsupport.PrintDialog()
+    assert dlg is not None
+
+
+def test_pagesetupdialog():
+    dlg = printsupport.PageSetupDialog()
+    assert dlg is not None
+
+
+def test_printpreviewdialog():
+    dlg = printsupport.PrintPreviewDialog()
+    assert dlg is not None
+
+
+def test_printpreviewwidget():
+    widget = printsupport.PrintPreviewWidget()
+    widget.set_view_mode("facing_pages")
+    assert widget.get_view_mode() == "facing_pages"
+    widget.set_zoom_mode("fit_to_width")
+    assert widget.get_zoom_mode() == "fit_to_width"
