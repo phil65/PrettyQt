1ce54f365bd93ee1b4ac10959dd04c9b1cb42c4c
phil65 <philipptemminghoff@googlemail.com>
2023-06-09 20:35:27 +0200

    feat(ScrollArea): make get_visible_widgets work with base QWidgets

--- prettyqt/widgets/scrollarea.py
+++ prettyqt/widgets/scrollarea.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TypeVar
 
-from prettyqt import widgets
+from prettyqt import core, widgets
 from prettyqt.qt import QtWidgets
 
 T = TypeVar("T", bound=QtWidgets.QWidget)
@@ -32,20 +32,18 @@ class ScrollArea(widgets.AbstractScrollAreaMixin, QtWidgets.QScrollArea):
         recursive: bool = True,
     ) -> list[T]:
         widget = self.widget()
-        rect = self.viewport().rect().adjusted(-margin, -margin, margin, margin)
-        return (
-            [
-                w
-                for w in widget.find_children(typ, recursive=recursive)
-                if rect.intersects(w.map_to(self.viewport(), w.rect()))
-            ]
-            if partial_allowed
-            else [
-                w
-                for w in widget.find_children(typ, recursive=recursive)
-                if rect.contains(w.map_to(self.viewport(), w.rect()))
-            ]
-        )
+        viewport = self.viewport()
+        rect = viewport.rect().adjusted(-margin, -margin, margin, margin)
+        found = []
+        for w in widget.find_children(typ, recursive=recursive):
+            top_left = w.mapTo(viewport, w.rect().topLeft())
+            bottom_right = w.mapTo(viewport, w.rect().bottomRight())
+            mapped = core.Rect(top_left, bottom_right)
+            if partial_allowed and rect.intersects(mapped):
+                found.append(w)
+            elif not partial_allowed and rect.contains(mapped):
+                found.append(w)
+        return found
 
     def get_children(self) -> list[QtWidgets.QWidget]:
         return self.widget().layout().get_children()
