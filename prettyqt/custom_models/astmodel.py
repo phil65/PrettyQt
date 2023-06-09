5af5bdc620baa2df1f06af071f3a365d9e7e6ba4
phil65 <philipptemminghoff@googlemail.com>
2023-06-09 23:12:34 +0200

    chore: some astmodel work

--- prettyqt/custom_models/astmodel.py
+++ prettyqt/custom_models/astmodel.py
@@ -13,30 +13,48 @@ logger = logging.getLogger(__name__)
 
 
 class AstModel(custom_models.TreeModel):
+    """Model to display the tree of an AST node."""
+
     HEADER = [
         "Node type",
-        "Line number",
-        "Column offset",
-        "End line number",
-        "End column offset",
+        "Name",
+        "Line range",
+        "Column range",
+        "Code segement",
+        "Docstring",
     ]
 
     def __init__(self, ast_tree, **kwargs):
-        super().__init__(ast_tree.body, **kwargs)
-        self.ast_tree = ast_tree.body
-
-    def set_ast(self, tree: ast.AST | str = ""):
-        with self.reset_model():
-            self.ast_tree = tree
+        super().__init__(None, **kwargs)
+        self.ast_tree = None
+        self.code = ""
+        self.set_ast(ast_tree)
 
     def columnCount(self, parent=None):
-        return len(self.HEADER)
+        return len(self.HEADER) if self.ast_tree is not None else 0
 
-    def parent_node_for_index(self, index):
-        # note: if index data is a node itself, it will get returned.
-        while not hasattr(node := index.data(constants.USER_ROLE), "lineno"):
-            index = index.parent()
-        return node
+    def set_ast(self, ast_tree: ast.AST | str = ""):
+        if isinstance(ast_tree, str):
+            code = ast_tree
+            try:
+                node = ast.parse(ast_tree)
+            except SyntaxError as e:
+                logger.exception(e)
+                return
+        else:
+            code = ast.unparse(ast_tree)
+            node = ast.parse(code)  # makin a circle here to make sure line numbers match.
+        node = ast.fix_missing_locations(node)
+        with self.reset_model():
+            self.ast_tree = node
+            self.set_root_item(node)
+            self.code = code
+
+    # def find_lineno(self, index):
+    #     # not needed, thx to ast.fix_missing_locations
+    #     while not hasattr(node := index.data(constants.USER_ROLE), "lineno"):
+    #         index = index.parent()
+    #     return node.lineno
 
     def headerData(
         self,
@@ -57,15 +75,30 @@ class AstModel(custom_models.TreeModel):
             case constants.DISPLAY_ROLE, 0:
                 return type(node).__name__
             case constants.DISPLAY_ROLE, 1:
+                match node:
+                    case ast.Name(id=name) | ast.arg(arg=name) | ast.Constant(value=name):
+                        return name
+                    case str():
+                        return node
+            case constants.DISPLAY_ROLE, 2:
                 if hasattr(node, "lineno"):
-                    return node.lineno
+                    return f"{node.lineno} - {node.end_lineno}"
+            case constants.DISPLAY_ROLE, 3:
+                if hasattr(node, "col_offset"):
+                    return f"{node.col_offset} - {node.end_col_offset}"
+            case constants.DISPLAY_ROLE, 4:
+                return ast.get_source_segment(self.code, node)
+
+            case constants.DISPLAY_ROLE, 5:
+                try:
+                    return ast.get_docstring(node)
+                except TypeError:
+                    return None
             case constants.USER_ROLE, _:
                 return node
 
     def _fetch_object_children(self, item: treeitem.TreeItem) -> list[treeitem.TreeItem]:
-        if isinstance(item.obj, list):
-            return [treeitem.TreeItem(obj=i) for i in item.obj]
-        return [treeitem.TreeItem(obj=getattr(item.obj, i)) for i in item.obj._fields]
+        return [treeitem.TreeItem(obj=i) for i in ast.iter_child_nodes(item.obj)]
 
     def hasChildren(self, parent: core.ModelIndex | None = None) -> bool:
         parent = parent or core.ModelIndex()
@@ -74,33 +107,53 @@ class AstModel(custom_models.TreeModel):
         item = self.data_by_index(parent)
         if self._show_root and item == self._root_item:
             return True
-        match item.obj:
-            case list():
-                return len(item.obj)
-            case ast.AST():
-                return len(item.obj._fields) > 0
-            case ast.Name():
-                return 0
-        return 0
+        if item.obj is None:
+            return False
+        return len(item.obj._fields) > 0
+
+    def rename_variable(
+        self,
+        old: str,
+        new: str,
+        root_tree: ast.AST | None = None,
+        ignore: list[str] | None = None,
+        scope: list[str] | None = None,
+    ):
+        if scope is None:
+            scope = ["main"]
+        if ignore is None:
+            ignore = []
+        if root_tree is None:
+            root_tree = self.ast_tree
+        for i in ast.iter_fields(root_tree):
+            if not isinstance(a := getattr(root_tree, i), list):
+                if a == old and not {*scope} & {*ignore}:
+                    setattr(root_tree, i, new)
+            n = a if isinstance(a, list) else [a]
+            s = [root_tree.name] if type(root_tree).__name__.endswith("Def") else scope
+            for j in n:
+                if isinstance(j, ast.AST):
+                    self.rename_variable(j, old, new, ignore, s)
+
+    def get_variable_names(self):
+        current_names = set()
+        for node in ast.walk(self.ast_tree):
+            match node:
+                case ast.Name(id=name) | ast.arg(arg=name):
+                    current_names.add(name)
+        return current_names
 
 
 if __name__ == "__main__":
     from prettyqt import widgets
     from prettyqt.custom_widgets import filtercontainer
+    import pathlib
 
     app = widgets.app()
     view = widgets.TreeView()
     view.setRootIsDecorated(True)
-    tree = ast.parse(
-        """\
-for a in b:
-    if a > 5:
-        break
-    else:
-        continue
-
-"""
-    )
+    code = pathlib.Path(__file__).read_text()
+    tree = ast.parse(code)
 
     model = AstModel(tree, show_root=True, parent=view)
     view.set_model(model)
