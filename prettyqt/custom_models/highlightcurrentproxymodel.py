dc5a3aeeb5e859a8fca89c27b8f1afd57bd6beab
phil65 <philipptemminghoff@googlemail.com>
2023-06-10 05:04:15 +0200

    feat(HighlightCurrentProxyModel): add "row" as mode option

--- prettyqt/custom_models/highlightcurrentproxymodel.py
+++ prettyqt/custom_models/highlightcurrentproxymodel.py
@@ -1,17 +1,28 @@
 from __future__ import annotations
 
+from typing import Literal
+
 from prettyqt import constants, core
 from prettyqt.qt import QtGui
 
 
 class HighlightCurrentProxyModel(core.IdentityProxyModel):
+    """Highlights all cells which have same data as current index in given role."""
+
     ID = "highlight_current"
 
-    def __init__(self, *args, role=constants.DISPLAY_ROLE, mode="column", **kwargs):
+    def __init__(
+        self,
+        *args,
+        role=constants.DISPLAY_ROLE,
+        mode: Literal["column", "row", "all"] = "column",
+        **kwargs,
+    ):
         self._mode = mode
-        self._current_value = None
-        self._data_role = constants.DISPLAY_ROLE
-        self._column = None
+        self._current_value = ...  # Sentinel value
+        self._data_role = role
+        self._current_column = None
+        self._current_row = None
         super().__init__(*args, **kwargs)
         parent: widgets.AbstractItemView = self.parent()  # type: ignore
         parent.model_changed.connect(self._on_model_change)
@@ -24,13 +35,21 @@ class HighlightCurrentProxyModel(core.IdentityProxyModel):
     def _on_current_change(self, new, old):
         with self.change_layout():
             self._current_value = new.data(constants.DISPLAY_ROLE)
-            self._column = new.column()
+            self._current_column = new.column()
+            self._current_row = new.row()
 
     def data(self, index, role=constants.DISPLAY_ROLE):
         if (
             role == constants.BACKGROUND_ROLE
             and index.data(self._data_role) == self._current_value
-            and (index.column() == self._column or self._mode == "all")
+            and (
+                (self._mode == "column" and index.column() == self._current_column)
+                or (
+                    self._mode == "row"
+                    and (index.row() == self._current_row)
+                    or self._mode == "all"
+                )
+            )
         ):
             return QtGui.QColor("red")
         return super().data(index, role)
