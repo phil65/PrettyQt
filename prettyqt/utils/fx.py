77795b6deb62bef4cae6d00d5c03b8182b024a17
phil65 <philipptemminghoff@googlemail.com>
2023-06-12 00:10:09 +0200

    chore: rework fx stuff

--- prettyqt/utils/fx.py
+++ prettyqt/utils/fx.py
@@ -1,42 +1,93 @@
 from __future__ import annotations
 
 import logging
-from typing import Literal
 
 from prettyqt import core, widgets
-from prettyqt.utils import colors, datatypes
+from prettyqt.utils import colors, datatypes, helpers
 
 logger = logging.getLogger(__name__)
+
+
+class AnimWrapper:
+    def __init__(self, animation, fx):
+        self._animation = animation
+        self.fx = fx
+
+    def animate(
+        self, start, end, easing="in_out_sine", duration: int = 1000, delay: int = 0
+    ):
+        self._animation.set_start_value(start)
+        self._animation.set_end_value(end)
+        self._animation.set_easing(easing)
+        self._animation.set_duration(duration)
+        self.fx.run(self._animation, delay)
+
+    def animate_on_event(
+        self,
+        event: core.event.EventStr,
+        start,
+        end,
+        easing="in_out_sine",
+        duration: int = 1000,
+        delay: int = 0,
+    ):
+        self._animation.setStartValue(start)
+        self._animation.setEndValue(end)
+        self._animation.setDuration(duration)
+        self._animation.set_easing(easing)
+
+        def on_event(event):
+            self._animation.start()
+            return False
+
+        obj = self._animation.targetObject()
+        obj.add_callback_for_event(on_event, include=event)
 
 
 class Fx:
     def __init__(self, widget):
         self._widget = widget
+        self._wrapper = None
 
-    def set_graphics_effect(
+    def __getitem__(self, value: str):
+        value = helpers.to_lower_camel(value)
+        anim = core.PropertyAnimation()
+        anim.apply_to(self._widget.__getattr__(value))
+        # keep a reference
+        self._wrapper = AnimWrapper(anim, self)
+        return self._wrapper
+
+    def set_colorize_effect(
         self,
-        effect: widgets.QGraphicsEffect
-        | Literal["drop_shadow", "blur", "opacity", "colorize"],
-        radius: int = 10,
-        opacity: float = 0.7,
         strength: float = 0.5,
         color: datatypes.ColorType = "blue",
-    ) -> widgets.QGraphicsEffect:
-        match effect:
-            case "drop_shadow":
-                effect = widgets.GraphicsDropShadowEffect(self._widget)
-                effect.setBlurRadius(radius)
-                effect.setColor(colors.get_color(color))
-            case "blur":
-                effect = widgets.GraphicsBlurEffect(self._widget)
-                effect.setBlurRadius(radius)
-            case "opacity":
-                effect = widgets.GraphicsOpacityEffect(self._widget)
-                effect.setOpacity(opacity)
-            case "colorize":
-                effect = widgets.GraphicsColorizeEffect(self._widget)
-                effect.setColor(colors.get_color(color))
-                effect.setStrength(strength)
+    ) -> widgets.GraphicsColorizeEffect:
+        effect = widgets.GraphicsColorizeEffect(self._widget)
+        effect.setColor(colors.get_color(color))
+        effect.setStrength(strength)
+        self._widget.setGraphicsEffect(effect)
+        return effect
+
+    def set_opacity_effect(self, opacity: float = 0.7) -> widgets.GraphicsOpacityEffect:
+        effect = widgets.GraphicsOpacityEffect(self._widget)
+        effect.setOpacity(opacity)
+        self._widget.setGraphicsEffect(effect)
+        return effect
+
+    def set_blur_effect(self, radius: int = 10) -> widgets.GraphicsBlurEffect:
+        effect = widgets.GraphicsBlurEffect(self._widget)
+        effect.setBlurRadius(radius)
+        self._widget.setGraphicsEffect(effect)
+        return effect
+
+    def set_drop_shadow_effect(
+        self,
+        radius: int = 10,
+        color: datatypes.ColorType = "blue",
+    ) -> widgets.GraphicsDropShadowEffect:
+        effect = widgets.GraphicsDropShadowEffect(self._widget)
+        effect.setBlurRadius(radius)
+        effect.setColor(colors.get_color(color))
         self._widget.setGraphicsEffect(effect)
         return effect
 
@@ -67,6 +118,25 @@ class Fx:
         anim.setEndValue(0.0)
         anim.setDuration(duration)
         anim.apply_to(self._widget.windowOpacity)
+        self.run(anim, delay)
+        return anim
+
+    def zoom(
+        self,
+        duration: int = 1000,
+        start: float = 1.0,
+        end: float = 1.0,
+        easing: core.easingcurve.TypeStr = "in_out_sine",
+        anchor: str = "center",
+        delay: int = 0,
+    ):
+        from prettyqt import custom_animations
+
+        anim = custom_animations.ZoomAnimation(parent=self._widget, anchor=anchor)
+        anim.set_easing(easing)
+        anim.set_start_value(start)
+        anim.set_end_value(end)
+        anim.set_duration(duration)
         self.run(anim, delay)
         return anim
 
@@ -108,49 +178,6 @@ class Fx:
         anim.apply_to(self._widget.windowOpacity)
         self.run(anim, delay)
         return anim
-
-    def play_property_animation(
-        self,
-        property_name: str,
-        start_value,
-        end_value,
-        easing="in_out_sine",
-        duration: int = 1000,
-        delay: int = 0,
-    ) -> core.QPropertyAnimation:
-        anim = core.PropertyAnimation(parent=self._widget)
-        anim.setTargetObject(self._widget)
-        anim.set_property_name(property_name)
-        anim.setStartValue(start_value)
-        anim.setEndValue(end_value)
-        anim.setDuration(duration)
-        anim.set_easing(easing)
-        self.run(anim, delay)
-        return anim
-
-    def animate_on_event(
-        self,
-        event: core.event.EventStr,
-        property_name: str,
-        start_value,
-        end_value,
-        easing="in_out_sine",
-        duration: int = 1000,
-        delay: int = 0,
-    ):
-        anim = core.PropertyAnimation(parent=self._widget)
-        anim.setTargetObject(self._widget)
-        anim.set_property_name(property_name)
-        anim.setStartValue(start_value)
-        anim.setEndValue(end_value)
-        anim.setDuration(duration)
-        anim.set_easing(easing)
-
-        def on_event(event):
-            anim.start()
-            return False
-
-        self._widget.add_callback_for_event(on_event, include=event)
 
     def run(
         self, animation: core.QPropertyAnimation, delay: int = 0, single_shot: bool = True
