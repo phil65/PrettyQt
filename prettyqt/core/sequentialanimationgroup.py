f9e4deb26b368d81f8d88e58c7135f06b362a8da
phil65 <philipptemminghoff@googlemail.com>
2023-06-12 03:03:49 +0200

    feat(SequentialAnimationGroup): add reverse / reversed / append_reversed methods

--- prettyqt/core/sequentialanimationgroup.py
+++ prettyqt/core/sequentialanimationgroup.py
@@ -7,4 +7,24 @@ from prettyqt.qt import QtCore
 class SequentialAnimationGroup(
     core.AnimationGroupMixin, QtCore.QSequentialAnimationGroup
 ):
-    pass
+    def reverse(self):
+        for anim in reversed(list(self)):
+            old_start = anim.startValue()
+            old_end = anim.endValue()
+            anim.setStartValue(old_end)
+            anim.setEndValue(old_start)
+
+    def reversed(self) -> SequentialAnimationGroup:
+        new = core.MetaObject(self.metaObject()).copy(self)
+        for anim in reversed(list(self)):
+            animation = core.MetaObject(anim.get_metaobject()).copy(anim)
+            animation = animation.reversed()
+            new.addAnimation(animation)
+        return new
+
+    def append_reversed(self) -> core.SequentialAnimationGroup:
+        revers = self.reversed()
+        animation = core.SequentialAnimationGroup()
+        animation.addAnimation(self)
+        animation.addAnimation(revers)
+        return animation
