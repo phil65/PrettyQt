132df344c52741c103c000f224b5989cdac85fa6
phil65 <philipptemminghoff@googlemail.com>
2023-06-13 03:00:51 +0200

    chore: some love for SelectedWordHighlighter

--- prettyqt/syntaxhighlighters/selectedwordhighlighter.py
+++ prettyqt/syntaxhighlighters/selectedwordhighlighter.py
@@ -1,53 +1,78 @@
 from __future__ import annotations
 
+from typing import Literal
 import re
 
 from prettyqt import core, gui
 from prettyqt.qt import QtGui
 
+ModeStr = Literal["word", "all"]
+
+PAT = re.compile("\\s+")
+
 
 class SelectedWordHighlighter(gui.SyntaxHighlighter):
     def __init__(self, parent: QtGui.QTextDocument | None = None):
         super().__init__(parent)
-        self.selection_term = ""
+        self._selection_term = ""
+        self._mode = "word"
         self._highlight_format = gui.TextCharFormat()
         self._highlight_format.setBackground(gui.Color(255, 210, 120))
         self._highlight_format.setFontWeight(QtGui.QFont.Weight.Bold)
         self._highlight_pattern = None
         self._widget = parent.parent()
-        self._widget.selectionChanged.connect(self.filter_selection_for_single_word)
+        self._widget.selectionChanged.connect(self._filter_selection_for_single_word)
 
     def set_selection_term(self, term: str):
-        if term == self.selection_term:
+        if term == self._selection_term:
             return
-        if term:
-            term = f"\\b{term}\\b"
-            if term == self.selection_term:
-                return
-        self.selection_term = term
-        self._highlight_pattern = core.RegularExpression(self.selection_term)
+        self._selection_term = term
+        self._update()
+
+    def _update(self):
+        match self._mode:
+            case "all":
+                search_pattern = self._selection_term
+            case "word":
+                search_pattern = f"\\b{self._selection_term}\\b"
+            case _:
+                raise ValueError(self._mode)
+        self._highlight_pattern = core.RegularExpression(search_pattern)
         self.rehighlight()
 
-    def highlightBlock(self, text):
-        if len(self.selection_term) > 1:
-            self.apply_highlight(text)
+    def get_selection_term(self) -> str:
+        return self._selection_term
 
-    def apply_highlight(self, text):
+    def set_selection_mode(self, mode: ModeStr):
+        if mode == self._mode:
+            return
+        self._mode = mode
+        self._update()
+
+    def get_selection_mode(self) -> ModeStr:
+        return self._mode
+
+    def highlightBlock(self, text):
+        if len(self._selection_term) > 1:
+            self._apply_highlight(text)
+
+    def _apply_highlight(self, text):
         for m in self._highlight_pattern.finditer(text):
             length = m.span()[1] - m.span()[0]
             self.setFormat(m.span()[0], length, self._highlight_format)
 
-    def filter_selection_for_single_word(self):
+    def _filter_selection_for_single_word(self):
         tc = self._widget.selecter.get_text_cursor()
         current_selection = tc.selectedText()
-        pat = re.compile("\\s+")
-        items = pat.split(current_selection)  # SkipEmptyParts
-        if not items:
+        if not PAT.split(current_selection):  # SkipEmptyParts
             self.set_selection_term("")
             return
         tc.select_text("start_of_word", "end_of_word")
         term = current_selection if current_selection == tc.selectedText() else ""
         self.set_selection_term(term)
+
+    selectionTerm = core.Property(str, get_selection_term, set_selection_term)
+    selectionMode = core.Property(str, get_selection_mode, set_selection_mode)
 
 
 if __name__ == "__main__":
