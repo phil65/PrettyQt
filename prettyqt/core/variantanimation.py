b85103b392faeacf9c5e2edede740f0c45b49e59
phil65 <philipptemminghoff@googlemail.com>
2023-06-14 05:16:34 +0200

    chore: comments

--- prettyqt/core/variantanimation.py
+++ prettyqt/core/variantanimation.py
@@ -39,14 +39,17 @@ class VariantAnimationMixin(core.AbstractAnimationMixin):
         self.setEndValue(end)
 
     def reverse(self):
+        """True reverse instead of just setting direction."""
         self.setKeyValues(list(reversed(self.keyValues())))
 
     def reversed(self) -> VariantAnimation:
+        """Return a reversed copy of the animation."""
         new = self.get_metaobject().copy(self)
         new.reverse()
         return new
 
     def append_reversed(self) -> Self:
+        """Append the reversed animation, effectively doubling the duration."""
         self.setDuration(self.duration() * 2)
         first_part = [(k / 2, v) for k, v in self.keyValues()]
         second_part = [(1 - (k / 2), v) for k, v in self.keyValues()]
