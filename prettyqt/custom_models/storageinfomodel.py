ccfd52327386820812a66a2c70822850aefe0959
phil65 <philipptemminghoff@googlemail.com>
2023-06-14 21:28:14 +0200

    chore: prep

--- prettyqt/custom_models/storageinfomodel.py
+++ prettyqt/custom_models/storageinfomodel.py
@@ -3,6 +3,112 @@ from __future__ import annotations
 from prettyqt import core, custom_models
 
 
+# class RootPathColumn(custom_models.ColumnItem):
+#     name="Root path"
+#     doc="Root path"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return str(item.get_root_path())
+
+
+# class VolumeNameColumn(custom_models.ColumnItem):
+#     name="Volume name"
+#     doc="Volume name"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return item.name()
+
+
+# class DeviceColumn(custom_models.ColumnItem):
+#     name="Device"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return item.get_device()
+
+
+# class FileSystemColumn(custom_models.ColumnItem):
+#     name="File system"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return item.get_file_system_type()
+
+
+# class TotalColumn(custom_models.ColumnItem):
+#     name = "Total"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return core.Locale().get_formatted_data_size(item.bytesTotal())
+
+
+# class FreeColumn(custom_models.ColumnItem):
+#     name = "Free"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return core.Locale().get_formatted_data_size(item.bytesFree())
+
+
+# class AvailableColumn(custom_models.ColumnItem):
+#     name = "Available"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.DISPLAY_ROLE:
+#                 return core.Locale().get_formatted_data_size(item.bytesAvailable())
+
+
+# class ReadyColumn(custom_models.ColumnItem):
+#     name = "Is ready"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.CHECKSTATE_ROLE:
+#                 return item.isReady()
+
+
+# class ReadOnlyColumn(custom_models.ColumnItem):
+#     name = "Read-only"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.CHECKSTATE_ROLE:
+#                 return item.isReadOnly()
+
+
+# class ValidColumn(custom_models.ColumnItem):
+#     name = "Valid"
+
+#     def get_data(self, item, role):
+#         match role:
+#             case constants.CHECKSTATE_ROLE:
+#                 return item.isValid()
+
+
+# COLUMNS = [
+#     RootPathColumn,
+#     VolumeNameColumn,
+#     DeviceColumn,
+#     FileSystemColumn,
+#     TotalColumn,
+#     FreeColumn,
+#     AvailableColumn,
+#     ReadyColumn,
+#     ReadOnlyColumn,
+#     ValidColumn,
+# ]
+
+
 loc = core.Locale()
 
 COL_ROOT_PATH = custom_models.ColumnItem(
