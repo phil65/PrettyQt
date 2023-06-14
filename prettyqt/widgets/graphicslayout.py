c3e8d136cc2ef02176e5bb63fb1beafbf16bc192
phil65 <philipptemminghoff@googlemail.com>
2023-06-14 08:47:38 +0200

    feat(GraphicsLayout): add slicing support

--- prettyqt/widgets/graphicslayout.py
+++ prettyqt/widgets/graphicslayout.py
@@ -4,12 +4,24 @@ from collections.abc import Iterator
 
 from prettyqt import widgets
 from prettyqt.qt import QtWidgets
+from prettyqt.utils import listdelegators
 
 
 class GraphicsLayoutMixin(widgets.GraphicsLayoutItemMixin):
-    def __getitem__(self, index: int) -> QtWidgets.QGraphicsItem:
-        layoutitem = self.itemAt(index)
-        return layoutitem.graphicsItem()
+    def __getitem__(
+        self, index: int | slice
+    ) -> widgets.QGraphicsItem | listdelegators.BaseListDelegator[widgets.QGraphicsItem]:
+        match index:
+            case int():
+                layoutitem = self.itemAt(index)
+                return layoutitem.graphicsItem()
+            case slice():
+                stop = index.stop or self.count()
+                rng = range(index.start or 0, stop, index.step or 1)
+                items = [self.itemAt(i).graphicsItem() for i in rng]
+                return listdelegators.BaseListDelegator(items)
+            case _:
+                raise TypeError(index)
 
     def __setitem__(self, index: int, value: QtWidgets.QGraphicsItem):
         layoutitem = self.itemAt(index)
@@ -28,8 +40,9 @@ class GraphicsLayoutMixin(widgets.GraphicsLayoutItemMixin):
         # for PySide2
         return self.count()
 
-    def get_children(self) -> list[QtWidgets.QGraphicsItem]:
-        return list(self)
+    def get_children(self) -> listdelegators.BaseListDelegator[QtWidgets.QGraphicsItem]:
+        items = [self.itemAt(i).graphicsItem() for i in range(self.count())]
+        return listdelegators.BaseListDelegator(items)
 
     def set_margin(self, margin: int):
         self.setContentsMargins(margin, margin, margin, margin)
