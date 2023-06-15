1b195ed62abde8460b9cc4cfd01a2a8976312922
phil65 <philipptemminghoff@googlemail.com>
2023-06-15 14:55:50 +0200

    feat: add DisplayMode setting to FlattenedTreeProxyModel

--- prettyqt/custom_models/flattenedtreeproxymodel.py
+++ prettyqt/custom_models/flattenedtreeproxymodel.py
@@ -2,25 +2,39 @@ from __future__ import annotations
 
 import enum
 
-from prettyqt import core
+from prettyqt import constants, core
 from prettyqt.qt import QtCore
+
+
+class FlatteningMode(enum.IntEnum):
+    """Flattening modes."""
+
+    Default = 1
+    InternalNodesDisabled = 2
+    LeavesOnly = 4
+
+
+class DisplayMode(enum.IntEnum):
+    """Display modes."""
+
+    Path = 1
+    Title = 2
 
 
 class FlattenedTreeProxyModel(core.AbstractProxyModel):
     ID = "flatten_tree"
 
-    @core.Enum
-    class FlatteningMode(enum.IntEnum):
-        """Flattening modes."""
+    FlatteningMode = FlatteningMode
+    core.Enum(FlatteningMode)
 
-        Default = 1
-        InternalNodesDisabled = 2
-        LeavesOnly = 4
+    DisplayMode = DisplayMode
+    core.Enum(DisplayMode)
 
     def __init__(self, parent=None, **kwargs):
         super().__init__(parent, **kwargs)
         self._source_column = 0
-        self._flattening_mode = FlattenedTreeProxyModel.FlatteningMode.Default
+        self._flattening_mode = FlatteningMode.Default
+        self._display_mode = DisplayMode.Path
         self._source_root_index = core.ModelIndex()
         self._source_key: list[tuple[int, ...]] = []
         self._source_offset: dict[tuple[int, ...], int] = {}
@@ -65,6 +79,14 @@ class FlattenedTreeProxyModel(core.AbstractProxyModel):
     def get_flattening_mode(self) -> FlattenedTreeProxyModel.FlatteningMode:
         return self._flattening_mode
 
+    def set_display_mode(self, mode: FlattenedTreeProxyModel.DisplayMode):
+        if mode != self._display_mode:
+            with self.reset_model():
+                self._display_mode = mode
+
+    def get_display_mode(self) -> FlattenedTreeProxyModel.DisplayMode:
+        return self._display_mode
+
     def mapFromSource(self, source_index: core.ModelIndex) -> core.ModelIndex:
         if not source_index.isValid():
             return source_index
@@ -87,7 +109,7 @@ class FlattenedTreeProxyModel(core.AbstractProxyModel):
         return (
             core.ModelIndex()
             if parent.isValid()
-            else self.createIndex(row, column, row)  # object=row)
+            else self.createIndex(row, column, parent)  # object=row)
         )
 
     def parent(self, child=None) -> core.ModelIndex:
@@ -112,29 +134,34 @@ class FlattenedTreeProxyModel(core.AbstractProxyModel):
             flags ^= QtCore.Qt.ItemFlag.ItemIsEnabled
         return flags
 
+    def data(self, index, role=constants.DISPLAY_ROLE):
+        if role == constants.DISPLAY_ROLE and self._display_mode == DisplayMode.Path:
+            index = self.mapToSource(index)
+            model = self.sourceModel()
+            path = model.get_breadcrumbs_path(index)
+            return " / ".join(str(i) for i in path)
+        return super().data(index, role)
+
     def _update_row_mapping(self):
         source = self.sourceModel()
 
-        source_key: list[tuple[int, ...]] = []
-        source_offset_map: dict[tuple[int, ...], int] = {}
+        self._source_key = []
+        self._source_offset = {}
 
         def create_mapping(model, index: core.ModelIndex, key_path: tuple[int, ...]):
-            rowcount = model.rowCount(index)
-            if rowcount > 0:
+            if (rowcount := model.rowCount(index)) > 0:
                 if self._flattening_mode != self.FlatteningMode.LeavesOnly:
-                    source_offset_map[key_path] = len(source_offset_map)
-                    source_key.append(key_path)
+                    self._source_offset[key_path] = len(self._source_offset)
+                    self._source_key.append(key_path)
                 for i in range(rowcount):
                     create_mapping(model, model.index(i, 0, index), key_path + (i,))
             else:
-                source_offset_map[key_path] = len(source_offset_map)
-                source_key.append(key_path)
+                self._source_offset[key_path] = len(self._source_offset)
+                self._source_key.append(key_path)
 
         if source is not None:
             for i in range(source.rowCount()):
                 create_mapping(source, source.index(i, 0), (i,))
-        self._source_key = source_key
-        self._source_offset = source_offset_map
 
     def _source_data_changed(self, top, bottom):
         changed_indexes = [top.sibling(i, 0) for i in range(top.row(), bottom.row() + 1)]
@@ -159,6 +186,12 @@ class FlattenedTreeProxyModel(core.AbstractProxyModel):
         FlatteningMode,
         get_flattening_mode,
         set_flattening_mode,
+    )
+
+    displayMode = core.Property(
+        DisplayMode,
+        get_display_mode,
+        set_display_mode,
     )
 
 
