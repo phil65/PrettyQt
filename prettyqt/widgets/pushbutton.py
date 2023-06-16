8a305da7e4dbc865ea30322e1e877211d1e6e6a3
phil65 <philipptemminghoff@googlemail.com>
2023-06-16 11:12:14 +0200

    feat(PushButton): add set_action method

--- prettyqt/widgets/pushbutton.py
+++ prettyqt/widgets/pushbutton.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from prettyqt import core, widgets
+from prettyqt import core, gui, widgets
 from prettyqt.qt import QtWidgets
 
 
@@ -10,6 +10,28 @@ class PushButtonMixin(widgets.AbstractButtonMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.toggled.connect(self.value_changed)
+        self._action = None
+
+    def set_action(self, action: gui.QAction):
+        if self._action == action:
+            return
+        if self._action:
+            self._action.changed.disconnect(self._update_button_for_action)
+            self.clicked.disconnect(self._action.trigger)
+        self._action = action
+        self.clicked.connect(action.trigger)
+        action.changed.connect(self._update_button_for_action)
+        self._update_button_for_action()
+
+    @core.Slot()
+    def _update_button_for_action(self):
+        self.setText(self._action.text())
+        self.setIcon(self._action.icon())
+        self.setStatusTip(self._action.statusTip())
+        self.setToolTip(self._action.toolTip())
+        self.setEnabled(self._action.isEnabled())
+        self.setCheckable(self._action.isCheckable())
+        self.setChecked(self._action.isChecked())
 
 
 class PushButton(PushButtonMixin, QtWidgets.QPushButton):
