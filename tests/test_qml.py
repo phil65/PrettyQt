a5fd817d19051e2da32e9a2a5ca6d0c0524d4e6b
phil65 <philipptemminghoff@googlemail.com>
2023-06-19 21:59:45 +0200

    chore: test work

--- tests/test_qml.py
+++ tests/test_qml.py
@@ -1,9 +1,11 @@
 """Tests for `prettyqt` package."""
 
 import pathlib
+import sys
 
 import pytest
 
+import prettyqt
 from prettyqt import core, qml
 from prettyqt.utils import InvalidParamError
 
@@ -35,6 +37,10 @@ def test_qmlengine():
     engine.get_import_paths()
 
 
+@pytest.mark.skipif(
+    sys.platform == "linux" and prettyqt.qt.API.startswith("pyside"),
+    reason="Segmentation fault",
+)
 def test_qmlapplicationengine(qtlog):
     with qtlog.disabled():
         engine = qml.QmlApplicationEngine()
