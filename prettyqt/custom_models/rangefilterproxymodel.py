04ae4255f9a89d2241bec78b613bf77a2bbee788
phil65 <philipptemminghoff@googlemail.com>
2023-06-21 03:51:36 +0200

    chore(RangeFilterProxyModel): separate props for min/max value

--- prettyqt/custom_models/rangefilterproxymodel.py
+++ prettyqt/custom_models/rangefilterproxymodel.py
@@ -16,8 +16,6 @@ class RangeFilterProxyModel(core.SortFilterProxyModel):
         return get_repr(self, self.get_range)
 
     def filterAcceptsRow(self, source_row: int, parent: core.ModelIndex) -> bool:
-        if self._min_value is None:
-            return True
         column = self.filterKeyColumn()
         role = self.filterRole()
         source_model = self.sourceModel()
@@ -27,28 +25,33 @@ class RangeFilterProxyModel(core.SortFilterProxyModel):
             return False
         return self._max_value is None or value <= self._max_value
 
-    def set_min_value(self, value):
+    def set_min_value(self, value: float | int | None):
         self._min_value = value
         self.invalidateRowsFilter()
 
-    def get_min_value(self):
+    def get_min_value(self) -> float | int:
+        if self._min_value is None:
+            return -float("inf")
         return self._min_value
 
-    def set_max_value(self, value):
+    def set_max_value(self, value: float | int):
         self._max_value = value
         self.invalidateRowsFilter()
 
-    def get_max_value(self):
+    def get_max_value(self) -> float | int:
+        if self._max_value is None:
+            return float("inf")
         return self._max_value
 
-    def set_range(self, range_: tuple[int, int]):
-        self._min_value, self._max_value = range_
+    def set_range(self, rng: tuple[float | int | None, float | int | None]):
+        self._min_value, self._max_value = rng
         self.invalidateRowsFilter()
 
-    def get_range(self) -> tuple[int, int]:
+    def get_range(self) -> tuple[float | int | None, float | int | None]:
         return (self._min_value, self._max_value)
 
-    value_range = core.Property(object, get_range, set_range, user=True)
+    min_value = core.Property(float, get_min_value, set_min_value)
+    max_value = core.Property(float, get_max_value, set_max_value)
 
 
 if __name__ == "__main__":
