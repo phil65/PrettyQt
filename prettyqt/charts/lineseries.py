677d33217361dea3dc8011ade7bb523a465e280f
phil65 <philipptemminghoff@googlemail.com>
2023-06-25 23:06:32 +0200

    chore: cleanup

--- prettyqt/charts/lineseries.py
+++ prettyqt/charts/lineseries.py
@@ -1,24 +1,12 @@
 from __future__ import annotations
 
-from prettyqt import charts, core
+from prettyqt import charts
 from prettyqt.qt import QtCharts, QtCore
 
 
 class LineSeries(charts.XYSeriesMixin, QtCharts.QLineSeries):
     """QLineSeries with some custom properties."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._process_name = ""
-
-    def get_process_name(self):
-        return self._process_name
-
-    def set_process_name(self, value):
-        self._process_name = value
-
-    process_name = core.Property(str, get_process_name, set_process_name)
-
+    pass
 
 if __name__ == "__main__":
     line = LineSeries()
