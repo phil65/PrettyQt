d16143f30c4cc1133546ec695081317b84d0fe86
phil65 <philipptemminghoff@googlemail.com>
2023-06-26 09:11:29 +0200

    feat(DateTimeAxis): add some datetime-related methods

--- prettyqt/charts/datetimeaxis.py
+++ prettyqt/charts/datetimeaxis.py
@@ -4,11 +4,24 @@ import datetime
 
 from prettyqt import charts
 from prettyqt.qt import QtCharts
+from prettyqt.utils import datatypes
 
 
 class DateTimeAxis(charts.AbstractAxisMixin, QtCharts.QDateTimeAxis):
+
+    def set_min(self, minimum: datatypes.DateTimeType):
+        minimum = datatypes.to_datetime(minimum)
+        self.setMin(minimum)
+
     def get_min(self) -> datetime.datetime:
         return self.min().toPython()
 
+    def set_max(self, maximum: datatypes.DateTimeType):
+        maximum = datatypes.to_datetime(maximum)
+        self.setMax(maximum)
+
     def get_max(self) -> datetime.datetime:
         return self.max().toPython()
+
+    def set_range(self, minimum: datatypes.DateTimeType, maximum: datatypes.DateTimeType):
+        self.setRange(datatypes.to_datetime(minimum), datatypes.to_datetime(maximum))
