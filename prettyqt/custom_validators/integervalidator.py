fbf35158458e9ec390a999b96cafe2c6047d9986
phil65 <philipptemminghoff@googlemail.com>
2023-06-29 10:08:42 +0200

    feat: Properties for IntegerValidator / TextLengthValidator

--- prettyqt/custom_validators/integervalidator.py
+++ prettyqt/custom_validators/integervalidator.py
@@ -1,33 +1,60 @@
 from __future__ import annotations
 
-from prettyqt import gui
-from prettyqt.qt import QtCore
+import sys
+
+from prettyqt import core, gui
 
 
 class IntegerValidator(gui.Validator):
+    """Validator to check for integer values.
+
+    In contrast to IntValidator provided by Qt, this validator is not bound to
+    a restricted value range.
+    """
+
     ID = "integer"
 
     def __init__(
         self,
         bottom: int | None = None,
         top: int | None = None,
-        parent: QtCore.QObject | None = None,
+        parent: core.QObject | None = None,
     ):
         super().__init__(parent)
-        self.range: tuple[int | None, int | None] = (bottom, top)
+        self._bottom = -sys.maxsize if bottom is None else bottom
+        self._top = sys.maxsize if top is None else top
 
     def set_range(self, lower: int | None, upper: int | None):
-        self.range = (lower, upper)
+        self.set_top(upper)
+        self.set_bottom(lower)
+
+    def get_range(self) -> tuple[int, int]:
+        return (self._bottom, self._top)
+
+    def set_bottom(self, bottom: int | None):
+        self._bottom = -sys.maxsize if bottom is None else bottom
+
+    def get_bottom(self) -> int:
+        return self._bottom
+
+    def set_top(self, top: int | None):
+        self._top = sys.maxsize if top is None else top
+
+    def get_top(self) -> int:
+        return self._top
 
     def validate(self, text: str, pos: int):
         if not text.lstrip("-"):
             return self.State.Intermediate, text, len(text)
         if text.lstrip("-").isnumeric():
-            left = float("-inf") if self.range[0] is None else self.range[0]
-            right = float("inf") if self.range[1] is None else self.range[1]
+            left = float("-inf") if self._bottom is None else self._bottom
+            right = float("inf") if self._top is None else self._top
             if left <= int(text) <= right:
                 return self.State.Acceptable, text, len(text)
         return self.State.Invalid, text, len(text)
+
+    bottom = core.Property(int, get_bottom, set_bottom)
+    top = core.Property(int, get_top, set_top)
 
 
 if __name__ == "__main__":
--- prettyqt/custom_validators/textlengthvalidator.py
+++ prettyqt/custom_validators/textlengthvalidator.py
@@ -1,33 +1,51 @@
 from __future__ import annotations
+import sys
 
-from prettyqt import gui
-from prettyqt.qt import QtCore
+from prettyqt import core, gui
 
 
 class TextLengthValidator(gui.Validator):
+    """Validator which checks whether given text has a specific length."""
     ID = "text_length"
 
     def __init__(
         self,
         minimum: int | None = None,
         maximum: int | None = None,
-        parent: QtCore.QObject | None = None,
+        parent: core.QObject | None = None,
     ):
         super().__init__(parent)
-        self.range: tuple[int | None, int | None] = (minimum, maximum)
+        self._minimum = 0 if minimum is None else minimum
+        self._maximum = sys.maxsize if maximum is None else maximum
 
-    def set_range(self, lower: int | None, upper: int | None):
-        self.range = (lower, upper)
+    def set_range(self, minimum: int | None, maximum: int | None):
+        self._minimum = 0 if minimum is None else minimum
+        self._maximum = sys.maxsize if maximum is None else maximum
+
+    def get_range(self) -> tuple[int | None, int | None]:
+        return (self._minimum, self._maximum)
+
+    def set_minimum(self, minimum: int | None):
+        self._minimum = 0 if minimum is None else minimum
+
+    def get_minimum(self) -> int:
+        return self._minimum
+
+    def set_maximum(self, maximum: int | None):
+        self._maximum = sys.maxsize if maximum is None else maximum
+
+    def get_maximum(self) -> int:
+        return self._maximum
 
     def validate(self, text: str, pos: int):
-        left = 0 if self.range[0] is None else self.range[0]
-        right = float("inf") if self.range[1] is None else self.range[1]
-
-        if left <= len(text) <= right:
+        if self._minimum <= len(text) <= self._maximum:
             return self.State.Acceptable, text, len(text)
-        if len(text) <= right:
+        if len(text) <= self._maximum:
             return self.State.Intermediate, text, len(text)
         return self.State.Invalid, text, len(text)
+
+    minimum = core.Property(int, get_minimum, set_minimum)
+    maximum = core.Property(int, get_maximum, set_maximum)
 
 
 if __name__ == "__main__":
