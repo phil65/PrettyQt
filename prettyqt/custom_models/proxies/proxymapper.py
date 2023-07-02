06046a34b8e252af0ed06136e745255c9fb50b0a
phil65 <philipptemminghoff@googlemail.com>
2023-07-02 17:44:20 +0200

    feat(ProxyMapper): allow more than 2 models for mapping

--- prettyqt/custom_models/proxies/proxymapper.py
+++ prettyqt/custom_models/proxies/proxymapper.py
@@ -28,22 +28,21 @@ class ProxyMapper(core.Object):
 
     def __init__(
         self,
-        proxy_1: core.QAbstractItemModel,
-        proxy_2: core.QAbstractItemModel,
+        *proxies: core.QAbstractItemModel,
         **kwargs,
     ):
         super().__init__(**kwargs)
-        proxychain_1 = self.get_proxy_chain(proxy_1)
-        proxychain_2 = self.get_proxy_chain(proxy_2)
-        common_list = [c for c in proxychain_1 if c in proxychain_2]
+        chains = [self.get_proxy_chain(proxy) for proxy in proxies]
+        common_list = [
+            element
+            for element in chains[0]
+            if all(element in sublist for sublist in chains[1:])
+        ]
         if not common_list:
             raise RuntimeError("No common source model")
         common_source = common_list[0]
         logger.debug(f"Common source: {common_source}")
-        self._proxychain_1 = proxychain_1[: proxychain_1.index(common_source)]
-        self._proxychain_2 = proxychain_2[: proxychain_2.index(common_source)]
-        logger.debug(f"Chain 1:{self._proxychain_1}")
-        logger.debug(f"Chain 2:{self._proxychain_2}")
+        self._chains = [chain[: chain.index(common_source)] for chain in chains]
 
     @staticmethod
     def get_proxy_chain(model) -> list[core.QAbstractItemModel]:
@@ -53,42 +52,22 @@ class ProxyMapper(core.Object):
             models.append(model)
         return models
 
-    def map_index_from_one_to_two(self, index: core.ModelIndex) -> core.ModelIndex:
-        for model in self._proxychain_1:
+    def map_index(self, from_: int, to: int, index: core.ModelIndex) -> core.ModelIndex:
+        for model in self._chains[from_]:
             logger.debug(f"mapping from {model!r} to {model.sourceModel()!r}")
             index = model.mapToSource(index)
-        for model in reversed(self._proxychain_2):
+        for model in reversed(self._chains[to]):
             logger.debug(f"mapping from {model.sourceModel()!r} to {model!r}")
             index = model.mapFromSource(index)
         return index
 
-    def map_index_from_two_to_one(self, index: core.ModelIndex) -> core.ModelIndex:
-        for model in self._proxychain_2:
-            logger.debug(f"mapping from {model!r} to {model.sourceModel()!r}")
-            index = model.mapToSource(index)
-        for model in reversed(self._proxychain_1):
-            logger.debug(f"mapping from {model.sourceModel()!r} to {model!r}")
-            index = model.mapFromSource(index)
-        return index
-
-    def map_selection_from_one_to_two(
-        self, selection: core.QItemSelection
+    def map_selection(
+        self, from_: int, to: int, selection: core.QItemSelection
     ) -> core.QItemSelection:
-        for model in self._proxychain_1:
+        for model in self._chains[from_]:
             logger.debug(f"mapping from {model!r} to {model.sourceModel()!r}")
             selection = model.mapSelectionToSource(selection)
-        for model in reversed(self._proxychain_2):
-            logger.debug(f"mapping from {model.sourceModel()!r} to {model!r}")
-            selection = model.mapSelectionFromSource(selection)
-        return selection
-
-    def map_selection_from_two_to_one(
-        self, selection: core.QItemSelection
-    ) -> core.QItemSelection:
-        for model in self._proxychain_2:
-            logger.debug(f"mapping from {model!r} to {model.sourceModel()!r}")
-            selection = model.mapSelectionToSource(selection)
-        for model in reversed(self._proxychain_1):
+        for model in reversed(self._chains[to]):
             logger.debug(f"mapping from {model.sourceModel()!r} to {model!r}")
             selection = model.mapSelectionFromSource(selection)
         return selection
@@ -125,4 +104,5 @@ if __name__ == "__main__":
     proxy_2_2.setSourceModel(proxy_2_1)
     with app.debug_mode():
         mapper = ProxyMapper(proxy_1_2, proxy_2_2)
-        index = mapper.map_index_from_one_to_two(proxy_1_2.index(0, 0))
+        index = mapper.map_index(0, 1, proxy_1_2.index(0, 0))
+    print(index)
