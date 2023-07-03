d4a6f3f50ccdce9672fa1c7b18bc6108ae41b638
phil65 <philipptemminghoff@googlemail.com>
2023-07-03 15:03:17 +0200

    chore: cleanup

--- prettyqt/custom_models/proxies/linkedselectionmodel.py
+++ prettyqt/custom_models/proxies/linkedselectionmodel.py
@@ -11,24 +11,27 @@ logger = logging.getLogger(__name__)
 class LinkedSelectionModel(core.ItemSelectionModel):
     def __init__(
         self,
-        *widgets,
+        *itemviews,
         **kwargs,
     ):
         super().__init__(**kwargs)
-        self._widgets = widgets
-        self._models = [w.model() for w in widgets]
+        self._itemviews = itemviews
+        self._models = [w.model() for w in itemviews]
         self._mapper = custom_models.ProxyMapper(*self._models)
-        for w in widgets:
+        for w in itemviews:
             w.selectionModel().currentChanged.connect(self._on_current_change)
 
     def _on_current_change(self, new: core.ModelIndex, _):
         source = new.model()
+        if source is None:
+            logger.warning("No model connected to index")
+            return
         source_index = self._models.index(source)
         targets = list(range(len(self._models)))
         targets.remove(source_index)
         for target in targets:
-            target_index = self._mapper.map_index(source_index, target, new)
-            self._widgets[target].setCurrentIndex(target_index)
+            mapped_idx = self._mapper.map_index(from_=source_index, to=target, index=new)
+            self._itemviews[target].setCurrentIndex(mapped_idx)
 
 
 if __name__ == "__main__":
