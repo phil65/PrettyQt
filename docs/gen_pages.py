048af1bb909c6469f588c33b1847908a050d3b2f
phil65 <philipptemminghoff@googlemail.com>
2023-07-07 08:25:54 +0200

    chore: sitegenerator work

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -15,11 +15,13 @@ BASE_URL = "https://doc.qt.io/qtforpython-6/PySide6/"
 def write_file_for_klass(klass, parts, file):
     ident = ".".join(parts)
     file.write(f"::: prettyqt.{ident}.{klass.__name__}\n")
-    print(ident)
+    # print(ident)
     if issubclass(klass, itemmodels.SliceIdentityProxyModel):
         print("here")
-        file.write("!!! Info\n")
-        file.write("    ")
+        file.write("!!! note\n")
+        file.write(
+            "This is a [slice proxy](SliceIdentityProxyModel.md) and can be selectively applied to a model.\n\n"
+        )
     if issubclass(klass, core.QObject):
         table = get_prop_table_for_klass(klass)
         file.write(table)
@@ -27,7 +29,7 @@ def write_file_for_klass(klass, parts, file):
         file.write(table)
         if parts[-1] in ["core", "gui", "widgets"]:
             url = f"{BASE_URL}Qt{parts[-1].capitalize()}/Q{klass.__name__}.html"
-            file.write(url)
+            file.write(f"Qt Base Class: [Q{klass.__name__}]({url})")
     if hasattr(klass, "ID") and issubclass(klass, gui.Validator):
         file.write(f"\n\nValidator ID: **{klass.ID}**\n\n")
     if hasattr(klass, "ID") and issubclass(klass, widgets.AbstractItemDelegateMixin):
@@ -36,6 +38,11 @@ def write_file_for_klass(klass, parts, file):
 
 def get_prop_table_for_klass(klass: type[core.QObject]):
     metaobject = core.MetaObject(klass.staticMetaObject)
+    user_prop_name = (
+        user_prop.get_name()
+        if (user_prop := metaobject.get_user_property()) is not None
+        else None
+    )
     props_without_super = metaobject.get_properties(include_super=False)
     prop_names_without_super = [p.get_name() for p in props_without_super]
     props_with_super = metaobject.get_properties(include_super=True)
@@ -49,22 +56,23 @@ def get_prop_table_for_klass(klass: type[core.QObject]):
         lines.extend(
             (
                 f"\n### Class Properties:\n",
-                "|Qt Property|Type|Description|\n|-----------|----|-----------|",
+                "|Qt Property|Type|User property|\n|-----------|----|-----------|",
             )
         )
         lines.extend(
-            f"|`{prop.get_name()}`|{prop.get_meta_type()}||"
+            f"|`{prop.get_name()}`|{prop.get_meta_type()}|{'x' if prop.get_name() == user_prop_name else ''}|"
             for prop in props_without_super
         )
     if super_props:
         lines.extend(
             (
                 f"\n### Inherited Properties:\n",
-                "|Qt Property|Type|Description|\n|-----------|----|-----------|",
+                "|Qt Property|Type|User property|\n|-----------|----|-----------|",
             )
         )
         lines.extend(
-            f"|`{prop.get_name()}`|{prop.get_meta_type()}||" for prop in super_props
+            f"|`{prop.get_name()}`|{prop.get_meta_type()}|{'x' if prop.get_name() == user_prop_name else ''}|"
+            for prop in super_props
         )
     return "\n\n" + "\n".join(lines) + "\n\n"
 
@@ -73,11 +81,10 @@ def get_ancestor_table_for_klass(klass: type[core.QObject]):
     subclasses = klass.__subclasses__()
     if not subclasses:
         return ""
-    lines = []
-    lines.append("|Ancestor|Description|")
-    lines.append("|--------|-----------|")
-    for subklass in subclasses:
-        lines.append(f"|`{subklass.__name__}`|{subklass.__module__}|")
+    lines = ["|Ancestor|Description|", "|--------|-----------|"]
+    lines.extend(
+        f"|`{subklass.__name__}`|{subklass.__module__}|" for subklass in subclasses
+    )
     return "\n\n" + "\n".join(lines) + "\n\n"
 
 
@@ -88,12 +95,15 @@ def write_files_for_module(module_path, doc_path, parts):
         fd.write(f"::: prettyqt.{ident}")
     try:
         module = importlib.import_module(module_path)
+        klass_names = [i[0] for i in inspect.getmembers(module, inspect.isclass)]
         klasses = [
             i[1]
             for i in inspect.getmembers(module, inspect.isclass)
-            if not i[0].endswith("Mixin") and not i[0].startswith("Q")
+            if issubclass(
+                i[1], core.ObjectMixin
+            )  # not i[0].endswith("Mixin") and not i[0].startswith("Q")  # f"Q{i[0]}" in klass_names
         ]
-    except (AttributeError, ModuleNotFoundError, ImportError) as e:
+    except (AttributeError, ImportError) as e:
         klasses = []
     mkdocs_gen_files.set_edit_path(full_doc_path.with_name("index.md"), module_path)
     for klass in klasses:
