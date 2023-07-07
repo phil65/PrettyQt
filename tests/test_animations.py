de96c79c13324c930ff76e79c622aeaafad9b945
phil65 <philipptemminghoff@googlemail.com>
2023-07-07 17:43:39 +0200

    chore: test fix

--- tests/test_animations.py
+++ tests/test_animations.py
@@ -5,8 +5,8 @@ from prettyqt import animations, widgets
 
 def test_slideanimation(qtbot):
     widget = widgets.Widget()
+    qtbot.addWidget(widget)
     anim = animations.SlideAnimation(parent=widget)
     anim.set_start_value((20, 20))
     anim.set_end_value((20, 20))
-    widget = widgets.Widget()
     anim.apply_to(widget)
