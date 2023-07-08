973e42065ac6624fcd31226c11f5c643f6df528f
phil65 <philipptemminghoff@googlemail.com>
2023-07-09 01:48:12 +0200

    chore: move stuff from AbsTableModel to AbsItemModel

--- prettyqt/core/abstractitemmodel.py
+++ prettyqt/core/abstractitemmodel.py
@@ -400,6 +400,59 @@ class AbstractItemModelMixin(core.ObjectMixin):
     def to_checkstate(value: bool):
         return constants.CheckState.Checked if value else constants.CheckState.Unchecked
 
+    def get_header_index_for_value(
+        self,
+        label,
+        orientation: constants.Orientation
+        | constants.OrientationStr = constants.HORIZONTAL,
+        role: constants.ItemDataRole = constants.DISPLAY_ROLE,
+    ) -> int | None:
+        o = constants.ORIENTATION.get_enum_value(orientation)
+        count = self.columnCount() if o == constants.HORIZONTAL else self.rowCount()
+        return next(
+            (i for i in range(count) if self.headerData(i, o, role) == label), None
+        )
+
+    def get_table_data(
+        self,
+        include_index: bool = False,
+        role: constants.ItemDataRole = constants.DISPLAY_ROLE,
+        x_range: slice | int | None = None,
+        y_range: slice | int | None = None,
+    ):
+        match x_range:
+            case None:
+                colrange = range(self.columnCount())
+            case slice():
+                stop = x_range.stop or self.columnCount()
+                colrange = range(x_range.start or 0, stop, x_range.step or 1)
+            case int():
+                colrange = range(x_range, x_range + 1)
+
+        match y_range:
+            case None:
+                rowrange = range(self.rowCount())
+            case slice():
+                stop = y_range.stop or self.rowCount()
+                rowrange = range(y_range.start or 0, stop, y_range.step or 1)
+            case int():
+                rowrange = range(y_range, y_range + 1)
+
+        data = [[self.index(i, j).data(role) for j in colrange] for i in rowrange]
+        h_header = [self.headerData(i, constants.HORIZONTAL) for i in colrange]
+        v_header = (
+            [self.headerData(i, constants.VERTICAL) for i in rowrange]
+            if include_index
+            else None
+        )
+        return data, h_header, v_header
+
+    def to_dataframe(self, **kwargs):
+        import pandas as pd
+
+        data, h_header, v_header = self.get_table_data(**kwargs)
+        return pd.DataFrame(data=data, columns=h_header, index=v_header)
+
 
 class AbstractItemModel(AbstractItemModelMixin, core.QAbstractItemModel):
     pass
--- prettyqt/core/abstracttablemodel.py
+++ prettyqt/core/abstracttablemodel.py
@@ -11,46 +11,6 @@ class AbstractTableModelMixin(core.AbstractItemModelMixin):
         | constants.NO_CHILDREN
     )
 
-    def get_table_data(
-        self,
-        include_index: bool = False,
-        role: constants.ItemDataRole = constants.DISPLAY_ROLE,
-        x_range: slice | int | None = None,
-        y_range: slice | int | None = None,
-    ):
-        match x_range:
-            case None:
-                colrange = range(self.columnCount())
-            case slice():
-                stop = x_range.stop or self.columnCount()
-                colrange = range(x_range.start or 0, stop, x_range.step or 1)
-            case int():
-                colrange = range(x_range, x_range + 1)
-
-        match y_range:
-            case None:
-                rowrange = range(self.rowCount())
-            case slice():
-                stop = y_range.stop or self.rowCount()
-                rowrange = range(y_range.start or 0, stop, y_range.step or 1)
-            case int():
-                rowrange = range(y_range, y_range + 1)
-
-        data = [[self.index(i, j).data(role) for j in colrange] for i in rowrange]
-        h_header = [self.headerData(i, constants.HORIZONTAL) for i in colrange]
-        v_header = (
-            [self.headerData(i, constants.VERTICAL) for i in rowrange]
-            if include_index
-            else None
-        )
-        return data, h_header, v_header
-
-    def to_dataframe(self, **kwargs):
-        import pandas as pd
-
-        data, h_header, v_header = self.get_table_data(**kwargs)
-        return pd.DataFrame(data=data, columns=h_header, index=v_header)
-
 
 class AbstractTableModel(AbstractTableModelMixin, core.QAbstractTableModel):
     pass
