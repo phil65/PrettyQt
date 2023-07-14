4306221b4cc54cd98079dbc1945a4b4707085ef5
phil65 <philipptemminghoff@googlemail.com>
2023-07-14 08:42:11 +0200

    chore: some work for Lists

--- prettyqt/utils/markdownizer/list.py
+++ prettyqt/utils/markdownizer/list.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 
-from prettyqt.utils import markdownizer
+from prettyqt.utils import get_repr, markdownizer
 
 
 logger = logging.getLogger(__name__)
@@ -16,9 +16,26 @@ class List(markdownizer.BaseSection):
     def __str__(self):
         return self.to_markdown()
 
+    def __repr__(self):
+        return get_repr(self, self.listitems)
+
+    def __len__(self):
+        return len(self.listitems)
+
     def _to_markdown(self):
         lines = [f"  - {i}" for i in self.listitems]
         return "\n" + "\n".join(lines) + "\n"
+
+    def to_html(self, shorten_after: int | None = None, make_link: bool = False):
+        if not self.listitems:
+            return ""
+        item_str = "".join(
+            f"<li>{markdownizer.linked(i)}</li>" if make_link else f"<li>{i}</li>"
+            for i in self.listitems[:shorten_after]
+        )
+        if shorten_after and len(self.listitems) > shorten_after:
+            item_str += "<li>...</li>"
+        return f"<ul>{item_str}</ul>"
 
 
 if __name__ == "__main__":
