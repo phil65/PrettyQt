328b202c17290c622ed429b3e71b72ace97ca157
phil65 <philipptemminghoff@googlemail.com>
2023-07-25 00:02:10 +0200

    chore: prep

--- prettyqt/core/property.py
+++ prettyqt/core/property.py
@@ -6,6 +6,20 @@ from prettyqt.qt import QtCore
 class Property(QtCore.Property):
     """Template class that enables automatic property bindings."""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.doc = kwargs.get("doc")
+
+    @classmethod
+    def get_doc_dict(cls, klass: type):
+        import inspect
+
+        result = {}
+        for name, member in inspect.getmembers(klass):
+            if isinstance(member, cls):
+                result[name] = member.doc
+        return result
+
 
 if __name__ == "__main__":
     prop = Property(int)
