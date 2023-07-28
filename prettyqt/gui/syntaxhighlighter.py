269765c6d44bfa87b7923c66bab7345713bdbc9c
phil65 <philipptemminghoff@googlemail.com>
2023-07-28 05:50:14 +0200

    fix: SyntaxHighlighter fix

--- prettyqt/gui/syntaxhighlighter.py
+++ prettyqt/gui/syntaxhighlighter.py
@@ -4,6 +4,9 @@ from prettyqt import core, gui
 
 
 class SyntaxHighlighterMixin(core.ObjectMixin):
+    def __init__(self, parent: core.QObject | None = None):
+        super().__init__(parent)  # type: ignore
+
     def get_current_block(self) -> gui.TextBlock:
         return gui.TextBlock(self.currentBlock())
 
