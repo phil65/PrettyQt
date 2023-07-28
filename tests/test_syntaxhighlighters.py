e931513e939bb74052e394de29e584e5ef78259a
phil65 <philipptemminghoff@googlemail.com>
2023-07-28 07:49:02 +0200

    chore: testing

--- tests/test_syntaxhighlighters.py
+++ tests/test_syntaxhighlighters.py
@@ -2,35 +2,38 @@
 
 import re
 
-from prettyqt import gui, syntaxhighlighters
+import pytest
+
+from prettyqt import gui, qt, syntaxhighlighters
 
 
+@pytest.mark.skipif(qt.API == "pyside6", reason="X11 connection break")
 def test_jsonhighlighter():
-    doc = gui.TextDocument()
+    doc = gui.QTextDocument()
     highlighter = syntaxhighlighters.JsonHighlighter(doc)
     highlighter.highlightBlock('{"a": "b"}')
 
 
 def test_pythonhighlighter():
-    doc = gui.TextDocument()
+    doc = gui.QTextDocument()
     highlighter = syntaxhighlighters.PythonHighlighter(doc)
     highlighter.highlightBlock("def test(): pass")
 
 
 def test_yamlhighlighter():
-    doc = gui.TextDocument()
+    doc = gui.QTextDocument()
     highlighter = syntaxhighlighters.YamlHighlighter(doc)
     highlighter.highlightBlock("---\ntest:\n  - hallo")
 
 
 def test_markdownhighlighter():
-    doc = gui.TextDocument()
+    doc = gui.QTextDocument()
     highlighter = syntaxhighlighters.MarkdownHighlighter(doc)
     highlighter.highlightBlock("### Headline")
 
 
 def test_regexmatchhighlighter():
-    doc = gui.TextDocument()
+    doc = gui.QTextDocument()
     highlighter = syntaxhighlighters.RegexMatchHighlighter(doc)
     text = "a123"
     prog = re.compile("[0-9]")
