8087807468c1ce4380f9cfbc990022fe644db619
phil65 <philipptemminghoff@googlemail.com>
2023-08-02 13:49:41 +0200

    fix(MkPrettyQtClassPage): switch to new processor system

--- prettyqt/prettyqtmarkdown/mkprettyqtclasspage.py
+++ prettyqt/prettyqtmarkdown/mkprettyqtclasspage.py
@@ -4,6 +4,7 @@ import logging
 
 import mknodes
 
+from mknodes.templatenodes import processors
 from mknodes.utils import helpers
 
 from prettyqt import core, gui, itemmodels, prettyqtmarkdown, widgets
@@ -20,50 +21,84 @@ logger = logging.getLogger(__name__)
 app = widgets.app()
 
 
+class QtParentPageProcessor(processors.PageProcessor):
+    ID = "qt_signature"
+
+    def append_block(self, page: mknodes.MkPage):
+        qt_parent = classhelpers.get_qt_parent_class(self.item)
+        page += f"Qt Base Class: {helpers.link_for_class(qt_parent)}"
+        page += f"Signature: `{qt_parent.__doc__}`"
+
+    def get_default_header(self, page: mknodes.MkPage):
+        return ""
+
+    def check_if_apply(self, page: mknodes.MkPage):
+        return bool(classhelpers.get_qt_parent_class(self.item))
+
+
+class QtScreenshotPageProcessor(processors.PageProcessor):
+    ID = "qt_signature"
+
+    def append_block(self, page: mknodes.MkPage):
+        if widget := self.item.setup_example():
+            page += mknodes.MkCode.for_object(self.item.setup_example)
+            page += prettyqtmarkdown.MkWidgetScreenShot(widget)
+
+    def get_default_header(self, page: mknodes.MkPage):
+        return "🖼 Screenshot"
+
+    def check_if_apply(self, page: mknodes.MkPage):
+        return (
+            "setup_example" in self.item.__dict__
+            and "Abstract" not in self.item.__name__
+            and not self.item.__name__.endswith("Mixin")
+        )
+
+
+class QtPageProcessor(processors.PageProcessor):
+    ID = "qt"
+
+    def append_block(self, page: mknodes.MkPage):
+        if issubclass(self.item, itemmodels.SliceIdentityProxyModel):
+            page.add_admonition(SLICE_PROXY_INFO)
+        if issubclass(self.item, core.AbstractItemModelMixin) and self.item.IS_RECURSIVE:
+            page.add_admonition(RECURSIVE_MODEL_INFO, typ="warning")
+        if (
+            issubclass(self.item, core.AbstractItemModelMixin)
+            and self.item.DELEGATE_DEFAULT is not None
+        ):
+            msg = f"Recommended delegate: {self.item.DELEGATE_DEFAULT!r}"
+            page.add_admonition(msg)
+        if issubclass(self.item, core.AbstractItemModelMixin) and hasattr(
+            self.item, "SUPPORTS"
+        ):
+            page.add_admonition(f"Supported data type: `{self.item.SUPPORTS}`")
+        if issubclass(self.item, core.QObject):
+            header = "⌗ Property table"
+            table = prettyqtmarkdown.MkPropertyTable(self.item, header=header)
+            page += table
+        if hasattr(self.item, "ID") and issubclass(self.item, gui.Validator):
+            page += f"\n\nValidator ID: **{self.item.ID}**\n\n"
+        if hasattr(self.item, "ID") and issubclass(
+            self.item, widgets.AbstractItemDelegateMixin
+        ):
+            page += f"\n\nDelegate ID: **{self.item.ID}**\n\n"
+
+    def get_default_header(self, page: mknodes.MkPage):
+        return ""
+
+
 class MkPrettyQtClassPage(mknodes.MkClassPage):
     """A ClassPage specifically for Qt-based classes."""
 
-    def _build(self):
-        if qt_parent := classhelpers.get_qt_parent_class(self.klass):
-            self.append(f"Qt Base Class: {helpers.link_for_class(qt_parent)}")
-            self.append(f"Signature: `{qt_parent.__doc__}`")
-        super()._build()
-        if issubclass(self.klass, itemmodels.SliceIdentityProxyModel):
-            self.add_admonition(SLICE_PROXY_INFO)
-        if (
-            issubclass(self.klass, core.AbstractItemModelMixin)
-            and self.klass.IS_RECURSIVE
-        ):
-            self.add_admonition(RECURSIVE_MODEL_INFO, typ="warning")
-        if (
-            issubclass(self.klass, core.AbstractItemModelMixin)
-            and self.klass.DELEGATE_DEFAULT is not None
-        ):
-            msg = f"Recommended delegate: {self.klass.DELEGATE_DEFAULT!r}"
-            self.add_admonition(msg)
-        if issubclass(self.klass, core.AbstractItemModelMixin) and hasattr(
-            self.klass, "SUPPORTS"
-        ):
-            self.add_admonition(f"Supported data type: `{self.klass.SUPPORTS}`")
-        if issubclass(self.klass, core.QObject):
-            header = "⌗ Property table"
-            table = prettyqtmarkdown.MkPropertyTable(self.klass, header=header)
-            self.append(table)
-        if hasattr(self.klass, "ID") and issubclass(self.klass, gui.Validator):
-            self.append(f"\n\nValidator ID: **{self.klass.ID}**\n\n")
-        if hasattr(self.klass, "ID") and issubclass(
-            self.klass, widgets.AbstractItemDelegateMixin
-        ):
-            self.append(f"\n\nDelegate ID: **{self.klass.ID}**\n\n")
-        if (
-            "setup_example" in self.klass.__dict__
-            and "Abstract" not in self.klass.__name__
-            and not self.klass.__name__.endswith("Mixin")
-        ):
-            if widget := self.klass.setup_example():
-                self += mknodes.MkCode.for_object(self.klass.setup_example)
-                header = "🖼 Screenshot"
-                self += prettyqtmarkdown.MkWidgetScreenShot(widget, header=header)
+    def get_processors(self):
+        processors = super().get_processors()
+        return [
+            QtParentPageProcessor(self.klass),
+            *processors,
+            QtPageProcessor(self.klass),
+            QtScreenshotPageProcessor(self.klass),
+        ]
 
 
 if __name__ == "__main__":
