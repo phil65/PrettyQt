a55aeedafae418a9bf77333b8cb18e632239dfcb
phil65 <phil65@users.noreply.github.com>
2023-09-04 18:09:27 +0000

    deploy: 2751c86e117eddd255aafd7b99a46911054d94a8

--- gen_pages.py
+++ gen_pages.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 from prettyqt.docs import build_root, build_index
 
+import mknodes
 
-root_nav = build_root.build_root()
-page = root_nav.add_index_page("Home", hide_nav=True)
-build_index.build_index(page)
-root_nav.write()
+
+def build(project: mknodes.Project):
+    root_nav = build_root.build_root(project)
+    page = root_nav.add_index_page("Home", hide_nav=True)
+    build_index.build_index(page)
+
 
 # from prettyqt import prettyqtmarkdown, widgets
 # app = widgets.app()
