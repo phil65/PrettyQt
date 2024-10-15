7714ad37bf3156f323b9b6ec440bb9be1a89e5b6
phil65 <philipptemminghoff@googlemail.com>
2024-10-15 22:35:30 +0200

    build: poetry -> uv

--- duties.py
+++ duties.py
@@ -5,21 +5,18 @@ import os
 from duty import duty
 
 
-ENV_PREFIX = "poetry run "
-
-
 @duty(capture=False)
 def build(ctx, *args: str):
     """Build a MkNodes page."""
     args_str = " " + " ".join(args) if args else ""
-    ctx.run(f"{ENV_PREFIX}mknodes build{args_str}")
+    ctx.run(f"uv run mknodes build{args_str}")
 
 
 @duty(capture=False)
 def serve(ctx, *args: str):
     """Serve a MkNodes page."""
     args_str = " " + " ".join(args) if args else ""
-    ctx.run(f"{ENV_PREFIX}mknodes serve{args_str}")
+    ctx.run(f"uv run mknodes serve{args_str}")
 
 
 @duty(capture=False)
@@ -35,7 +32,7 @@ def test_pyside6(ctx, *args: str):
     os.environ["QT_API"] = "pyside6"
     args = ("--cov-report=xml", *args)
     args_str = " " + " ".join(args)
-    ctx.run(f"{ENV_PREFIX}pytest{args_str}")
+    ctx.run(f"uv run pytest{args_str}")
 
 
 @duty(capture=False)
@@ -44,7 +41,7 @@ def test_pyqt6(ctx, *args: str):
     os.environ["QT_API"] = "pyqt6"
     args = ("--cov-report=xml", *args)
     args_str = " " + " ".join(args)
-    ctx.run(f"{ENV_PREFIX}pytest{args_str}")
+    ctx.run(f"uv run pytest{args_str}")
 
 
 @duty(capture=False)
@@ -56,27 +53,30 @@ def clean(ctx):
 @duty(capture=False)
 def update(ctx, *args: str):
     """Update all environment packages using pip directly."""
-    ctx.run("poetry update")
-    ctx.run("poetry install --all-extras")
+    ctx.run("uv sync --all-extras")
 
 
 @duty(capture=False)
 def lint(ctx):
-    """Update all environment packages using pip directly."""
-    ctx.run(f"{ENV_PREFIX}lint")
+    """Lint and fix the code."""
+    ctx.run("uv run ruff check --fix .")
+    ctx.run("uv run ruff format .")
+    ctx.run("uv run mypy prettyqt/")
 
 
 @duty(capture=False)
 def lint_check(ctx):
-    """Update all environment packages using pip directly."""
-    ctx.run(f"{ENV_PREFIX}lint-check")
+    """Lint the code."""
+    ctx.run("uv run ruff check .")
+    ctx.run("uv run ruff format --check .")
+    ctx.run("uv run mypy prettyqt/")
 
 
 @duty(capture=False)
 def profile(ctx, *args: str):
     """Run generating the docs using pyinstrument."""
     args_str = " " + " ".join(args) if args else ""
-    ctx.run(f"{ENV_PREFIX}pyinstrument mknodes/manual/root.py{args_str}")
+    ctx.run(f"uv run pyinstrument mknodes/manual/root.py{args_str}")
 
 
 @duty(capture=False)
